4e24e236dc7cfb4c83c512b9c2e034ffc593a30d
Neitmann, Thomas {MDBB~Basel} <thomas.neitmann@roche.com>
2021-02-26 12:33:12 +0100

    Create function `derive_var_pchg()`

--- R/derive_var_chg.R
+++ R/derive_var_chg.R
@@ -4,3 +4,10 @@ derive_var_chg <- function(bds_dataset) {
   bds_dataset %>%
     mutate(CHG = AVAL - BASE)
 }
+
+derive_var_pchg <- function(bds_dataset) {
+  assert_has_variables(bds_dataset, c("BASE", "CHG"))
+
+  bds_dataset %>%
+    mutate(PCHG = CHG / BASE)
+}
--- tests/testthat/test-derive_var_chg.R
+++ tests/testthat/test-derive_var_chg.R
@@ -37,7 +37,7 @@ test_that("`CHG` is calculated as `AVAL - BASE`", {
   expect_equal(derive_var_chg(input)$CHG, expected_output$CHG)
 })
 
-test_that("`CHG` is calculated as `AVAL - BASE`", {
+test_that("`PCHG` is calculated as `CHG / BASE`", {
   input <- tibble::tribble(
     ~STUDYID, ~USUBJID, ~PARAMCD,  ~AVAL, ~ABLFL, ~BASETYPE, ~BASE,  ~CHG,
     "TEST01", "PAT01",  "PARAM01", 10.12, "Y",    "LAST",     10.12,  0,
@@ -55,22 +55,22 @@ test_that("`CHG` is calculated as `AVAL - BASE`", {
     "TEST01", "PAT02",  "PARAM02",  5.35, "",     "LAST",      8.9,  -3.55
   )
 
-  expected_ouput <- tibble::tribble(
-    ~STUDYID, ~USUBJID, ~PARAMCD,  ~AVAL, ~ABLFL, ~BASETYPE, ~BASE,  ~CHG,
-    "TEST01", "PAT01",  "PARAM01", 10.12, "Y",    "LAST",     10.12,  0,
-    "TEST01", "PAT01",  "PARAM01",  9.7,  "",     "LAST",     10.12, -0.42,
-    "TEST01", "PAT01",  "PARAM01", 15.01, "",     "LAST",     10.12,  4.89,
-    "TEST01", "PAT01",  "PARAM02",  8.35, "Y",    "LAST",      8.35,  0,
-    "TEST01", "PAT01",  "PARAM02", NA,    "",     "LAST",      8.35, NA,
-    "TEST01", "PAT01",  "PARAM02",  8.35, "",     "LAST",      8.35,  0,
+  expected_output <- tibble::tribble(
+    ~STUDYID, ~USUBJID, ~PARAMCD,  ~AVAL, ~ABLFL, ~BASETYPE, ~BASE,  ~CHG,  ~PCHG,
+    "TEST01", "PAT01",  "PARAM01", 10.12, "Y",    "LAST",     10.12,  0,     0,
+    "TEST01", "PAT01",  "PARAM01",  9.7,  "",     "LAST",     10.12, -0.42, -0.04150198,
+    "TEST01", "PAT01",  "PARAM01", 15.01, "",     "LAST",     10.12,  4.89,  0.4832016,
+    "TEST01", "PAT01",  "PARAM02",  8.35, "Y",    "LAST",      8.35,  0,     0,
+    "TEST01", "PAT01",  "PARAM02", NA,    "",     "LAST",      8.35, NA,    NA,
+    "TEST01", "PAT01",  "PARAM02",  8.35, "",     "LAST",      8.35,  0,     0,
 
-    "TEST01", "PAT02",  "PARAM01", 29,    "Y",    "LAST",     29,     0,
-    "TEST01", "PAT02",  "PARAM01", 19.7,  "",     "LAST",     29,    -9.3,
-    "TEST01", "PAT02",  "PARAM01", 18.01, "",     "LAST",     29,   -10.99,
-    "TEST01", "PAT02",  "PARAM02",  8.9,  "Y",    "LAST",      8.9,   0,
-    "TEST01", "PAT02",  "PARAM02",  9,    "",     "LAST",      8.9,   0.1,
-    "TEST01", "PAT02",  "PARAM02",  5.35, "",     "LAST",      8.9,  -3.55
+    "TEST01", "PAT02",  "PARAM01", 29,    "Y",    "LAST",     29,     0,     0,
+    "TEST01", "PAT02",  "PARAM01", 19.7,  "",     "LAST",     29,    -9.3,  -0.3206897,
+    "TEST01", "PAT02",  "PARAM01", 18.01, "",     "LAST",     29,   -10.99, -0.3789655,
+    "TEST01", "PAT02",  "PARAM02",  8.9,  "Y",    "LAST",      8.9,   0,     0,
+    "TEST01", "PAT02",  "PARAM02",  9,    "",     "LAST",      8.9,   0.1,   0.01123596,
+    "TEST01", "PAT02",  "PARAM02",  5.35, "",     "LAST",      8.9,  -3.55, -0.3988764
   )
 
-  expect_equal(derive_var_chg(input)$PCHG, expected_output$PCHG)
+  expect_equal(derive_var_pchg(input)$PCHG, expected_output$PCHG, tolerance = 1e-7)
 })
