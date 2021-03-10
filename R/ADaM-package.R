58078f74644cb6893e1982319a0743ee7589c211
Bundfuss, Stefan {MDBB~Basel} <stefan.bundfuss@roche.com>
2021-03-10 16:06:14 +0100

    #44_duration: update NAMESPACE

--- R/ADaM-package.R
+++ R/ADaM-package.R
@@ -1,4 +1,7 @@
+#' @keywords internal
+#' @importFrom assertthat assert_that
 #' @importFrom dplyr mutate if_else
-#' @importFrom lubridate floor_date time_length %--%
+#' @importFrom lubridate floor_date time_length %--% ymd ymd_hms is.instant
 #' @importFrom magrittr %>%
 #' @importFrom rlang enquo
+"_PACKAGE"
