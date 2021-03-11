2cdba924cdca2d9a90e5f0800e262d5403e12fc4
Neitmann, Thomas {MDBB~Basel} <thomas.neitmann@roche.com>
2021-03-11 14:13:18 +0100

    Move `derive_var_pchg()` to its own file

--- R/derive_var_pchg.R
+++ R/derive_var_pchg.R
@@ -1,17 +1,18 @@
-#' Derive Change from Baseline
+#' Derive Percent Change from Baseline
 #'
-#' Derive change from baseline (`CHG`) in a BDS dataset
+#' Derive percent change from baseline (`PCHG`) in a BDS dataset
 #'
 #' @param bds_dataset `data.frame`. Required variables are `AVAL` and
 #' `BASE`.
 #'
 #' @details
-#' Change from baseline is calculated by subtracting the baseline value
-#' from the analysis value.
+#' Percent change from baseline is calculated by dividing change from
+#' baseline by the absolute value of the baseline value and
+#' multiplying the result by `100`.
 #'
 #' @author Thomas Neitmann
 #'
-#' @return The input dataset with an additional column named `CHG`
+#' @return The input dataset with an additional column named `PCHG`
 #' @export
 #'
 #' @examples
@@ -23,11 +24,11 @@
 #'   "P02",    "WEIGHT", 75.3,  "Y",    75.3,
 #'   "P02",    "WEIGHT", 76,    "",     75.3
 #' )
-#' derive_var_chg(advs)
+#' derive_var_pchg(advs)
 #'
-derive_var_chg <- function(bds_dataset) {
+derive_var_pchg <- function(bds_dataset) {
   assert_has_variables(bds_dataset, c("AVAL", "BASE"))
 
   bds_dataset %>%
-    mutate(CHG = AVAL - BASE)
+    mutate(PCHG = if_else(BASE == 0, NA_real_, (AVAL - BASE) / abs(BASE) * 100))
 }
