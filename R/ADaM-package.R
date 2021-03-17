3956248d8f58fc6d49853de9c4e77f433368217c
Neitmann, Thomas {MDBB~Basel} <thomas.neitmann@roche.com>
2021-03-17 14:42:40 +0100

    Create `warn_if_vars_exist()` function

--- R/ADaM-package.R
+++ R/ADaM-package.R
@@ -1,7 +1,7 @@
 #' @keywords internal
 #' @importFrom dplyr filter mutate left_join rename select if_else
 #' @importFrom magrittr %>%
-#' @importFrom rlang := abort enquo syms
+#' @importFrom rlang := abort enquo syms warn
 #' @importFrom utils capture.output
 #' @importFrom assertthat assert_that
 #' @importFrom lubridate floor_date time_length %--% ymd ymd_hms is.instant
---
+++ tests/testthat/test-warnings.R
@@ -0,0 +1,22 @@
+context("test-warnings")
+
+test_that("a warning is issued when a variable to be derived already exists in the input dataset", {
+  data(dm)
+
+  expect_warning(
+    warn_if_vars_exist(dm, "AGE"),
+    "Variable `AGE` already exists in the dataset"
+  )
+  expect_warning(
+    warn_if_vars_exist(dm, c("AGE", "AGEU", "ARM")),
+    "Variables `AGE`, `AGEU` and `ARM` already exist in the dataset"
+  )
+  expect_warning(
+    warn_if_vars_exist(dm, c("AAGE", "AGEU", "ARM")),
+    "Variables `AGEU` and `ARM` already exist in the dataset"
+  )
+  expect_warning(
+    warn_if_vars_exist(dm, "AAGE"),
+    NA
+  )
+})
