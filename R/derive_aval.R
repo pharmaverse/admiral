cdbf23b2e3644c2d52bbe40cfae3154840da81f9
neitmant <thomas.neitmann@roche.com>
2021-04-12 16:28:31 +0200

    Add documentation

--- R/derive_aval.R
+++ R/derive_aval.R
@@ -1,3 +1,21 @@
+#' Derive `AVALC`, `AVAL` and `AVALU`
+#'
+#' @param dataset The input dataset
+#'
+#' @author Thomas Neitmann
+#'
+#' @details
+#' The SDTM variables `--STRES`, `--STRESN` and `--STRESU` are mapped to the
+#' ADaM variables `AVALC`, `AVAL` and `AVALU`, respectively.
+#'
+#' @export
+#'
+#' @examples
+#' library(dplyr)
+#' data(vs)
+#' vs %>%
+#'   derive_aval() %>%
+#'   select(USUBJID, PARAMCD, VSSTRES, AVALC, VSSTRESN, AVAL, VSSTRESU, AVALU)
 derive_aval <- function(dataset) {
   select_col <- function(pattern) grep(pattern, colnames(dataset), value = TRUE)
 
