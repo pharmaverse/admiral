14f3368e8d6068b7a66ae10ccb892b9169237627
neitmant <thomas.neitmann@roche.com>
2021-04-22 12:09:16 +0200

    Add documentation

--- R/derive_reference_ranges.R
+++ R/derive_reference_ranges.R
@@ -1,3 +1,43 @@
+#' Derive Reference Ranges and Range Indicator
+#'
+#' @param dataset The input dataset
+#' @param meta_ref_ranges A dataset containing the references ranges. Required
+#'        variables are `by_var`, `ANRLO`, `ANRHI`. `A1LO` and `A1HI` are optional.
+#' @param by_var A `character` vector of variables by which to join `dataset` and
+#'        `meta_ref_ranges`. By default, `"PARAMCD"`.
+#'
+#' @details
+#' `ANRIND` is set to
+#' - `"NORMAL"` if `AVAL` is greater or equal `ANRLO` and less than
+#'   or equal `ANRHI`
+#' - `"LOW"` if `AVAL` is less than `ANRLO` and either `A1LO` is missing or `AVAL`
+#'   is greater than or equal `A1LO`
+#' - `"HIGH"` if `AVAL` is greater than `ANRHI` and either `A1HI` is missing or `AVAL`
+#'   is less than or equal `A1HI`
+#' - `"LOW LOW"` if `AVAL` is less than `A1LO`
+#' - `"HIGH HIGH"` if `AVAL` is greater than `A1HI`
+#'
+#' @return The input dataset with additional columns `ANRLO`, `ANRHI`, `ANRIND`
+#'         and---if present in `meta_ref_ranges`---`A1LO` as well as `A1HI`
+#' @export
+#'
+#' @examples
+#' library(dplyr)
+#' data(vs)
+#' ref_ranges <- tibble::tribble(
+#'   ~PARAMCD, ~ANRLO, ~ANRHI, ~A1LO, ~A1HI,
+#'   "DIABP",  60,      80,    40,     90,
+#'   "PULSE",  60,     100,    40,    110
+#' )
+#'
+#' vs %>%
+#'   mutate(
+#'     PARAMCD = VSTESTCD,
+#'     AVAL = VSSTRESN
+#'   ) %>%
+#'   filter(PARAMCD %in% c("PULSE", "DIABP")) %>%
+#'   derive_reference_ranges(ref_ranges) %>%
+#'   select(USUBJID, PARAMCD, AVAL, ANRLO:ANRIND)
 derive_reference_ranges <- function(dataset,
                                     meta_ref_ranges,
                                     by_var = "PARAMCD") {
