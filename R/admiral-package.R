6858ae7ca048b8188b85d07aeb60f50670be8fb6
neitmant <thomas.neitmann@roche.com>
2021-04-22 12:18:36 +0200

    Update docs and NAMESPACE

--- R/admiral-package.R
+++ R/admiral-package.R
@@ -1,7 +1,7 @@
 #' @keywords internal
-#' @importFrom dplyr arrange case_when desc ends_with filter group_by mutate n
-#'             left_join rename row_number select slice starts_with if_else transmute
-#'             ungroup
+#' @importFrom dplyr anti_join arrange case_when desc ends_with filter group_by if_else
+#'             mutate n left_join pull rename row_number select slice starts_with
+#'             transmute ungroup
 #' @importFrom magrittr %>%
 #' @importFrom rlang := abort arg_match as_string enquo exprs inform sym syms warn
 #' @importFrom utils capture.output
