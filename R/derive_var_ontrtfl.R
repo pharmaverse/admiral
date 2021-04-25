b517630086aee77ca985cf918d6163d128139f8a
Alice Ehmann <alice.x.ehmann@gsk.com>
2021-04-25 13:20:36 -0400

    ONTRTFL: Updated per comments, collapse into 1 function (derive_var_ontrtfl.R)

--- R/derive_var_ontrtfl.R
+++ R/derive_var_ontrtfl.R
@@ -5,7 +5,7 @@
 #'
 #' @param dataset `data.frame`.
 #'
-#' @param start_date The assessment date (e.g. `ADT` for the date of a VS test)
+#' @param date The assessment date (e.g. `ADT` for the date of a VS test)
 #'   Required; A date or date-time object column is expected
 #'
 #' @param ref_start_date The lower bound of the on-treatment period
@@ -13,45 +13,39 @@
 #'
 #' @param ref_end_date The upper bound of the on-treatment period
 #'   A date or date-time object column is expected.
-#'   Optional; This can be null everything after `ref_start_date` is considered
-#'   on-treatment.
+#'   Optional; This can be null and everything after `ref_start_date` will be
+#'   considered on-treatment.
+#'   Default is NULL.
 #'
-#' @param ref_end_window A window to add to the upper bound `ref_end_date`.
+#' @param new_var Variable name to store the on-treatment flag
+#'   Optional; default: ONTRTFL
+#'
+#' @param ref_end_window A window to add to the upper bound `ref_end_date`
+#'   measured in days
 #'   (e.g. 7 if 7 days should be added to the upper bound)
 #'   Optional; default is 0
-#'   `ref_end_window` and `ref_end_window_units` should be used together
 #'
-#' @param ref_end_window_units The units of `ref_end_window`
-#'   (e.g. 7 days would have `ref_end_window_units` set to days)
-#'   Optional; default is "days", valid values: days, weeks, years
-#'   `ref_end_window` and `ref_end_window_units` should be used together
-#'
-#' @param timepoint_var Timepoint variable used to exclude observations
-#'   that occur on the `ref_start_date`.
-#'   Optional; default is NA
-#'   `timepoint_var` and `start_timepoint_pre_value` should be used together
-#'
-#' @param start_timepoint_pre_value If a baseline timepoint is used to
-#'   differentiate observations taken on the day of treatment, those to be
-#'   considered not on-treatment should be listed.
-#'   Optional; default is NA
-#'   Valid values can be specified as "PRE" or c("PRE", "BEFORE")
-#'   `timepoint_var` and `start_timepoint_pre_value` should be used together
+#' @param filter_pre_timepoint An expression to filter observtions as not
+#' on-treatment when `date` = `ref_start_date`. For example, if
+#' observations where `VSTPT = PRE` should not be considered on-treatment when
+#' `date = ref_start_date`, `filter_pre_timepoint` should be used
+#' to denote when the on-treatment flag should be set to null.
+#' Optional; default = NULL
 #'
 #' @details
 #' On-Treatment is calculated by determining whether the assessment date or
 #' start/stop dates fall between 2 dates. The following logic is used to
 #' assign on-treatment = Y:
-#'   1. `start_date` is missing and `ref_start_date`is non-missing
-#'   2. No timepoint is provided (`timepoint_var`) and both `start_date` and
-#'      `ref_start_date` are non-missing and `start_date` = `ref_start_date`
-#'   3. A timepoint is provided (`timepoint_var`) and both `start_date` and
-#'      `ref_start_date` are non-missing and `start_date` = `ref_start_date` and
-#'      `timepoint_var` value is not in `start_timepoint_pre_value` list
-#'   4. `ref_end_date` is not provided in the function call and `ref_start_date` <
-#'      `start_date`
-#'   5. `ref_end_date` is provided in the function call and `ref_start_date` <
-#'      `start_date` <= `ref_end_date` + `ref_end_window`.
+#'   1. `date` is missing and `ref_start_date`is non-missing
+#'   2. No timepoint filter is provided (`filter_pre_timepoint`) and both
+#'      `date` and `ref_start_date` are non-missing and `date` =
+#'      `ref_start_date`
+#'   3. A timepoint is provided (`filter_pre_timepoint`) and both `date`
+#'      and `ref_start_date` are non-missing and `date = ref_start_date`
+#'      and the filter provided in `filter_pre_timepoint` is not true.
+#'   4. `ref_end_date` is not provided and `ref_start_date < date`
+#'   5. `ref_end_date` is provided and `ref_start_date < date` <=
+#'      `ref_end_date + ref_end_window`.
 #'
 #' Any date imputations needed should be done prior to calling this function.
 #'
@@ -63,63 +57,90 @@
 #' @export
 #'
 #' @examples
+#' library(lubridate)
 #' advs <- tibble::tribble(
 #'   ~USUBJID, ~ADT, ~TRTSDT, ~TRTEDT, ~ONTRTFL
-#'   "P01",    lubridate::ymd("2020-02-24"), lubridate::ymd("2020-01-01"),
-#'             lubridate::ymd("2020-03-01"), "Y",
-#'   "P02",    lubridate::ymd("2020-01-01"), lubridate::ymd("2020-01-01"),
-#'             lubridate::ymd("2020-03-01"), "Y",
-#'   "P03",    lubridate::ymd("2019-12-31"), lubridate::ymd("2020-01-01"),
-#'             lubridate::ymd("2020-03-01"), "N")
+#'   "P01",    ymd("2020-02-24"), ymd("2020-01-01"), ymd("2020-03-01"), "Y",
+#'   "P02",    ymd("2020-01-01"), ymd("2020-01-01"), ymd("2020-03-01"), "Y",
+#'   "P03",    ymd("2019-12-31"), ymd("2020-01-01"), ymd("2020-03-01"), "N")
 #' )
-#' #' derive_var_ontrtfl(advs, ADT, TRTSDT, TRTEDT)
+#' #' derive_var_ontrtfl(advs, ADT, TRTSDT, ref_end_date = TRTEDT)
 #'
 #' advs <- tibble::tribble(
 #'   ~USUBJID, ~ADT, ~TRTSDT, ~TRTEDT, ~ONTRTFL
-#'   "P01",    lubridate::ymd("2020-07-01"), lubridate::ymd("2020-01-01"),
-#'             lubridate::ymd("2020-03-01"), "N",
-#'   "P02",    lubridate::ymd("2020-04-30"), lubridate::ymd("2020-01-01"),
-#'             lubridate::ymd("2020-03-01"), "Y",
-#'   "P03",    lubridate::ymd("2020-03-15"), lubridate::ymd("2020-01-01"),
-#'             lubridate::ymd("2020-03-01"), "Y")
+#'   "P01",    ymd("2020-07-01"), ymd("2020-01-01"), ymd("2020-03-01"), "N",
+#'   "P02",    ymd("2020-04-30"), ymd("2020-01-01"), ymd("2020-03-01"), "Y",
+#'   "P03",    ymd("2020-03-15"), ymd("2020-01-01"), ymd("2020-03-01"), "Y")
 #' )
-#' derive_var_ontrtfl(advs, ADT, TRTSDT, TRTEDT, ref_end_window=60,
-#'   ref_end_window_units="days")
+#' derive_var_ontrtfl(advs, ADT, TRTSDT, ref_end_date = TRTEDT,
+#'   ref_end_window=60)
 #'
 #' advs <- tibble::tribble(
 #' ~USUBJID, ~ADTM, ~TRTSDTM, ~TRTEDTM, ~TPT, ~ONTRTFL,
-#' "P01",    lubridate::ymd("2020-01-02T12:00"),
-#'           lubridate::ymd_hm("2020-01-01T12:00"),
-#'           lubridate::ymd_hm("2020-03-01T12:00"), "", "Y",
-#' "P02",    lubridate::ymd("2020-01-01"),
-#'           lubridate::ymd_hm("2020-01-01T12:00"),
-#'           lubridate::ymd_hm("2020-03-01T12:00"), "PRE", NA,
-#' "P03",    lubridate::ymd("2019-12-31"),
-#'           lubridate::ymd_hm("2020-01-01T12:00"),
-#'           lubridate::ymd_hm("2020-03-01T12:00"), "", NA
+#' "P01",    ymd("2020-01-02T12:00"), ymd_hm("2020-01-01T12:00"),
+#'           ymd_hm("2020-03-01T12:00"), "", "Y",
+#' "P02",    ymd("2020-01-01"), ymd_hm("2020-01-01T12:00"),
+#'           ymd_hm("2020-03-01T12:00"), "PRE", NA,
+#' "P03",    ymd("2019-12-31"), ymd_hm("2020-01-01T12:00"),
+#'           ymd_hm("2020-03-01T12:00"), "", NA
 #' )
-#' derive_var_ontrtfl(advs, ADTM, TRTSDTM, TRTEDTM, timepoint_var=TPT,
-#'   start_timepoint_pre_value="PRE")
+#' derive_var_ontrtfl(advs, ADTM, TRTSDTM, ref_end_date = TRTEDTM,
+#' filter_pre_timepoint = rlang::exprs(TPT == "PRE"))
 #'
 derive_var_ontrtfl <- function(dataset,
-                               start_date,
+                               date,
                                ref_start_date,
-                               ref_end_date,
+                               ref_end_date = NULL,
+                               new_var = ONTRTFL,
                                ref_end_window = 0,
-                               ref_end_window_units = "days",
-                               timepoint_var = NA,
-                               start_timepoint_pre_value = NA) {
+                               filter_pre_timepoint = NULL) {
 
-   derive_ontrt(
+   warn_if_vars_exist(
       dataset,
-      new_var = ONTRTFL,
-      start_date = !!enquo(start_date),
-      ref_start_date = !!enquo(ref_start_date),
-      ref_end_present = c(deparse(substitute(ref_end_date))),
-      ref_end_date = !!enquo(ref_end_date),
-      ref_end_window = ref_end_window,
-      ref_end_window_units = ref_end_window_units,
-      timepoint_var = !!enquo(timepoint_var),
-      start_timepoint_pre_value = start_timepoint_pre_value
+      c(deparse(substitute(new_var)))
    )
+
+   new_var <- enquo(new_var)
+   date <- enquo(date)
+   ref_start_date <- enquo(ref_start_date)
+   ref_end_date <- enquo(ref_end_date)
+
+   if (quo_text(ref_end_date) == "NULL") {
+      dataset <- dataset %>%
+         mutate(TEMP_REF_END_DATE = NA)
+   }else {
+      dataset <- dataset %>%
+         mutate(TEMP_REF_END_DATE = !!ref_end_date + days(x = ref_end_window))
+   }
+
+   #Derive On-Treatment flag
+   #Scenario 1: No treatment end date is passed
+   #Scenario 2: Treatment end date is passed, window added above
+
+   dataset <- dataset %>%
+      mutate(!!new_var :=
+                case_when(is.na(!!date) & !is.na(!!ref_start_date) ~ "Y",
+                          !is.na(!!date) & !is.na(!!ref_start_date) &
+                             !!ref_start_date == !!date ~ "Y"))
+
+   if (!missing(filter_pre_timepoint)) {
+      dataset <- dataset  %>%
+         mutate(!!new_var := if_else(!!!filter_pre_timepoint, NA_character_, !!new_var))
+   }
+
+   if (quo_text(ref_end_date) == "NULL") {
+      dataset <- dataset %>%
+         mutate(!!new_var := if_else(!is.na(!!ref_start_date) &
+                                       !!ref_start_date < !!date,
+                                    "Y", !!new_var))
+   } else {
+      dataset <- dataset %>%
+         mutate(!!new_var :=
+                   if_else(!is.na(!!ref_start_date) & !!ref_start_date < !!date &
+                             !is.na(TEMP_REF_END_DATE) &
+                             !!date <= TEMP_REF_END_DATE, "Y", !!new_var))
+   }
+
+   dataset <- select(dataset, -TEMP_REF_END_DATE)
+
 }
