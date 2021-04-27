b13a6e131821873b809e8f8b320e59f13f24cc1c
neitmant <thomas.neitmann@roche.com>
2021-04-27 14:54:08 +0200

    Add argument checks

--- R/derive_var_basetype.R
+++ R/derive_var_basetype.R
@@ -1,4 +1,13 @@
 derive_var_basetype <- function(dataset, basetypes) {
+  assert_that(
+    is.data.frame(dataset),
+    is_named_exprs(basetypes)
+  )
+  assert_has_variables(
+    dataset,
+    unique(map_chr(basetypes, all.vars))
+  )
+
   subsets <- map2(names(basetypes), basetypes, function(label, condition) {
     dataset %>%
       filter(!!condition) %>%
