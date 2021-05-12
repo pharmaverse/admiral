250ca74bb77674e495d33130963a060fc7cbdc3b
Ondrej <ondrej.slama@roche.com>
2021-05-12 09:38:28 +0200

    fixes and DOMAIN column updates

--- R/derive_joined_vars.R
+++ R/derive_joined_vars.R
@@ -59,13 +59,12 @@
 #'   derive_vars_dtm(new_vars_prefix = "AST", dtc = AESTDTC) %>%
 #'   derive_joined_vars(
 #'     dataset_join = ex,
-#'     new_vars = exprs(LDOSEDTM = compute_dtm(dtc = EXENDTC,
-#'                                             date_imputation = NULL,
-#'                                             time_imputation = NULL)),
-#'     pre_filter = exprs(is_valid_dose()),
-#'     by_vars = exprs(USUBJID),
+#'     new_vars = exprs(LDOSEDTM = convert_dtc_to_dtm(dtc = EXENDTC)),
+#'     pre_filter = exprs((EXDOSE > 0 | (EXDOSE == 0 & str_detect(EXTRT, "PLACEBO"))) &
+#'                          nchar(EXENDTC) >= 10),
+#'     by_vars = exprs(STUDYID, USUBJID),
 #'     order = exprs(EXENDTC),
-#'     post_filter = exprs(ASTDTM <= c(dtc = EXENDTC))
+#'     post_filter = exprs(ASTDTM <= convert_dtc_to_dt(dtc = EXENDTC))
 #'   )
 #'
 #' data(time_def)
@@ -89,29 +88,52 @@ derive_joined_vars <- function(dataset,
   assert_has_variables(dataset_join, map_chr(by_vars, as_string))
   stopifnot(is.list(new_vars) && vapply(new_vars, rlang::is_expression, logical(1)))
 
-  cols_dataset <- colnames(dataset)
-
   if (!is.null(pre_filter)) {
     dataset_join <- filter(dataset_join, !!!pre_filter)
   }
 
-  dataset_res <- dataset %>%
-    left_join(dataset_join, by = map_chr(by_vars, as_string)) %>%
-    mutate(!!!new_vars)
+  # check if there are no overlapping columns,
+  # otherwise the join and derivation does not need to work properly
+  inter <- setdiff(intersect(colnames(dataset), colnames(dataset_join)), "DOMAIN")
+  join_by <- unname(map_chr(by_vars, as_string))
 
+  # cannot join by DOMAIN as these values are generally different
+  if ("DOMAIN" %in% join_by) {
+    stop("Cannot join by DOMAIN, please remove it from by_vars.")
+  }
+
+  # check overlap
+  if (length(inter) > length(join_by) || !all(sort(inter) == sort(join_by))) {
+    stop(paste("There are variables with similar names that are not used for joining:",
+               paste0(paste(setdiff(inter, join_by), collapse = ", "), "."),
+               "Please either rename them or add them to 'by_vars' argument."))
+  }
+
+  # save input dataset columns
+  cols_dataset <- setdiff(colnames(dataset), "DOMAIN")
+
+  # perform join and remove DOMAIN columns
+  dataset_res <- dataset %>%
+    left_join(dataset_join, by = join_by) %>%
+    mutate(!!!new_vars) %>%
+    mutate(DOMAIN.x = NULL, DOMAIN.y = NULL)
+
+  # apply post_filter
   if (!is.null(post_filter)) {
     dataset_res <- filter(dataset_res, !!!post_filter)
   }
 
+  # apply ordering
   if (!is.null(order)) {
     dataset_res <- dataset_res %>%
-      group_by(!!!by_vars) %>%
+      group_by(!!!syms(join_by)) %>%
       arrange(!!!order, .by_group = TRUE)
   }
 
+  # return only derived columns from the joined dataset
   if (is.null(names(new_vars)) || all(names(new_vars) == "")) {
     names(new_vars) <- unname(map_chr(new_vars, as_string))
   }
 
-  return(select(dataset_res, c(cols_dataset, names(new_vars))))
+  return(select(ungroup(dataset_res), c(cols_dataset, names(new_vars))))
 }
