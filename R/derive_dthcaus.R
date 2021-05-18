27ce8b736fc739b904fcaf2910b16747fd5eb0f3
Shimeng Huang <shimeng.huang@roche.com>
2021-05-18 15:21:13 -0400

    Updated documentation.

--- R/derive_dthcaus.R
+++ R/derive_dthcaus.R
@@ -6,11 +6,7 @@
 #'
 #' @param sources A list of sources.
 #'
-#' @param by_vars Grouping variables.
-#'
-#'   Default: `exprs(USUBJID)`
-#'
-#' @keywords Roche adsl
+#' @keywords adsl
 #'
 #' @author Shimeng Huang
 #'
@@ -106,53 +102,3 @@ derive_dthcaus <- function(dataset, sources) {
 
   left_join(dataset, dataset_add, by = "USUBJID")
 }
-
-# derive_dthcaus <- function(dataset, dataset_ae, dataset_ds,
-#                            filter_ae = exprs(AEOUT == "FATAL"),
-#                            filter_ds = exprs(DSDECOD == "DEATH", grepl("DEATH DUE TO", DSTERM)),
-#                            dthcaus_ae = expr(AEDECOD),
-#                            dthcaus_ds = expr(DSTERM)) {
-#
-#   # extract from AE
-#   dataset_ae <- filter_extreme(dataset_ae %>%
-#                                  filter(!!!filter_ae),
-#                                order = exprs(AEDTHDTC),
-#                                by_vars = exprs(USUBJID),
-#                                mode = "first")
-#
-#   if (typeof(dthcaus_ae) == "symbol") {
-#     dataset_ae <- dataset_ae %>%
-#       transmute(USUBJID, DTHDOM = "AE", DTHCAUS = !!dthcaus_ae, temp_source_nr = 1)
-#   } else if (typeof(dthcaus_ae) == "character") {
-#     dataset_ae <- dataset_ae %>%
-#       transmute(USUBJID, DTHDOM = "AE", DTHCAUS = dthcaus_ae, temp_source_nr = 1)
-#   } else {
-#     stop("Type of `dthcaus_ae` must be either symbol or character.")
-#   }
-#
-#   # extract from DS
-#   dataset_ds <- filter_extreme(dataset_ds %>%
-#                                  filter(!!!filter_ds),
-#                                order = exprs(DSSTDTC),
-#                                by_vars = exprs(USUBJID),
-#                                mode = "first")
-#
-#   if (typeof(dthcaus_ds) == "symbol") {
-#     dataset_ds <- dataset_ds %>%
-#       transmute(USUBJID, DTHDOM = "DS", DTHCAUS = !!dthcaus_ds, temp_source_nr = 2)
-#   } else if (typeof(dthcaus_ds) == "character") {
-#     dataset_ds <- dataset_ds %>%
-#       transmute(USUBJID, DTHDOM = "DS", DTHCAUS = dthcaus_ds, temp_source_nr = 2)
-#   } else {
-#     stop("Type of `dthcaus_ds` must be either symbol or character.")
-#   }
-#
-#   # combine together and take AE one if exists
-#   dataset_add <- bind_rows(dataset_ae, dataset_ds) %>%
-#     filter_extreme(by_vars = exprs(USUBJID),
-#                    order = exprs(temp_source_nr),
-#                    mode = "first") %>%
-#     select(-starts_with("temp_"))
-#
-#   left_join(dataset, dataset_add, by = "USUBJID")
-# }
