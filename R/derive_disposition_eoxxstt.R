598616c42c1a0dbf8042ff84a775a0a0db0de298
kabis <samia.kabi@roche.com>
2021-05-20 14:47:35 +0200

    92_disposition_status update after review

---
+++ R/derive_disposition_eoxxstt.R
@@ -0,0 +1,127 @@
+#' Derive a disposition status at a specific timepoint
+#'
+#' Derive a disposition status from the the relevant records in the disposition domain.
+#'
+#' @param dataset Input dataset.
+#'
+#' @param dataset_ds Dataset containing the disposition information (e.g.: ds).
+#'
+#' The variable specified in the status_var parameter must be in dataset_ds.
+#'
+#' @param new_var Name of the disposition date variable.
+#'
+#' a variable name is expected (e.g. EOSSTT).
+#'
+#' @param status_var The variable used to derive the disposition status.
+#'
+#'   A character vector is expected (e.g. DSDECOD).
+#'
+#' @param format_new_var The format used to derive the status.
+#'
+#' Default: format_eoxxstt_default defined as:
+#' format_eoxxstt_default<-function(x){
+#' case_when (
+#'   x %in% c("COMPLETED")~"COMPLETED",
+#'   !(x %in% c("COMPLETED")) & ! is.na(x)~"DISCONTINUED",
+#'   TRUE ~ "ONGOING"
+#' )
+#' }
+#' where x is the status_var.
+#'
+#' @param filter_ds Filter condition for the disposition data.
+#'
+#'   Filter used to select the relevant disposition data.
+#'
+#'   Permitted Values: logical expression.
+#'
+#' @return the input dataset with the disposition status (new_var) added.
+#'
+#' @keywords adsl
+#'
+#' @author Samia Kabi
+#'
+#' @export
+#'
+#' @examples
+#' library(dplyr)
+#' data("dm")
+#' data("ds")
+#'
+#' # Default derivation: EOSSTT =
+#' #- COMPLETED when status_var = COMPLETED
+#' #- DISCONTINUED when status_var != COMPLETED
+#' #- ONGOING otherwise
+#'
+#' derive_disposition_eoxxstt(
+#'   dataset = dm,
+#'   dataset_ds = ds,
+#'   new_var = EOSSTT,
+#'   status_var = DSDECOD,
+#'   filter_ds = expr(DSCAT == "DISPOSITION EVENT")
+#' )
+#'
+#' # Specific derivation: EOSSTT =
+#' #- COMPLETED when status_var = COMPLETED
+#' #- DISCONTINUED DUE TO AE when status_var = ADVERSE EVENT
+#' #- DISCONTINUED NOT DUE TO AE when status_var != ADVERSE EVENT nor COMPLETED nor missing
+#' #- ONGOING otherwise
+#'
+#' format_eoxxstt1 <- function(x) {
+#'   case_when(
+#'     x == "COMPLETED" ~ "COMPLETED",
+#'     x == "ADVERSE EVENT" ~ "DISCONTINUED DUE TO AE",
+#'     x %!in% c("ADVERSE EVENT", "COMPLETED") & !is.na(x) ~ "DISCONTINUED NOT DUE TO AE",
+#'     TRUE ~ "ONGOING"
+#'   )
+#' }
+#'
+#' derive_disposition_eoxxstt(
+#'   dataset = dm,
+#'   dataset_ds = ds,
+#'   new_var = EOSSTT,
+#'   status_var = DSDECOD,
+#'   format_new_var = format_eoxxstt1,
+#'   filter_ds = expr(DSCAT == "DISPOSITION EVENT")
+#' )
+derive_disposition_eoxxstt <- function(dataset,
+                                       dataset_ds,
+                                       new_var,
+                                       status_var,
+                                       format_new_var = format_eoxxstt_default,
+                                       filter_ds) {
+  # Checks
+  warn_if_vars_exist(dataset, deparse(substitute(new_var)))
+  assert_that(is.data.frame(dataset_ds))
+  assert_has_variables(dataset_ds, deparse(substitute(status_var)))
+
+  # Process the disposition data
+  ds_subset <- dataset_ds %>%
+    filter(!!filter_ds) %>%
+    select(STUDYID, USUBJID, !!enquo(status_var))
+
+  # Expect 1 record per subject in the subsetted DS - issue a warning otherwise
+  has_unique_records(
+    dataset = ds_subset,
+    by_vars = "USUBJID",
+    message_type = "warning",
+    message = "The filter used for DS results in several records per patient - please check"
+  )
+  # Add the status variable and derive the new dispo status in the input dataset
+  dataset %>%
+    left_join(ds_subset, by = c("STUDYID", "USUBJID")) %>%
+    mutate(!!enquo(new_var) := format_new_var(!!enquo(status_var))) %>%
+    select(-!!enquo(status_var))
+}
+
+#' Default format for the disposition status
+#'
+#' Define a function to map the disposition status.
+#'
+#' @param x the disposition variable used for the mapping (e.g. `DSDECOD`).
+format_eoxxstt_default <- function(x) {
+  case_when(
+    x == "COMPLETED" ~ "COMPLETED",
+    x != "COMPLETED" & !is.na(x) ~ "DISCONTINUED",
+    TRUE ~ "ONGOING"
+  )
+}
--- inst/example_scripts/ad_adsl.R
+++ inst/example_scripts/ad_adsl.R
@@ -9,13 +9,56 @@ library(dplyr)
 library(lubridate)
 library(admiral)
 
-# read in predecessor variables from dm
 data("dm")
 data("ex")
 data("ds")
 
-# derive treatment variables (TRT01P, TRT01A)
+
+# User defined functions
+# Grouping
+format_agegr1 <- function(x) {
+  case_when(
+    !is.na(x) & x < 18 ~ "< 18",
+    x >= 18 & x < 65 ~ "18 - 65",
+    x >= 65 ~ ">= 65"
+  )
+}
+format_racegr1 <- function(x) {
+  case_when(
+    !is.na(x) & x == "WHITE" ~ "White",
+    !is.na(x) & x != "WHITE" ~ "Non-white",
+    TRUE ~ "Missing"
+  )
+}
+format_region1 <- function(x) {
+  case_when(
+    x %in% c("CAN", "USA") ~ "NA",
+    !is.na(x) ~ "RoW",
+    TRUE ~ "Missing"
+  )
+}
+format_lddthgr1 <- function(x) {
+  case_when(
+    !is.na(x) & x <= 30 ~ "<= 30",
+    x > 30 ~ "> 30",
+    TRUE ~ NA_character_
+  )
+}
+
+# EOSSTT mapping
+format_eoxxstt <- function(x) {
+  case_when(
+    x %in% c("COMPLETED") ~ "COMPLETED",
+    !(x %in% c("COMPLETED", "SCREEN FAILURE")) & !is.na(x) ~ "DISCONTINUED",
+    x %in% c("SCREEN FAILURE") ~ NA_character_,
+    TRUE ~ "ONGOING"
+  )
+}
+# end of user defined functions
+
+
 adsl <- dm %>%
+  # derive treatment variables (TRT01P, TRT01A)
   mutate(
     TRT01P = ARMCD, TRT01A = ARMCD
   ) %>%
@@ -31,13 +74,35 @@ adsl <- dm %>%
   # derive treatment duration (TRTDURD)
   derive_var_trtdurd() %>%
 
-  # Disposition date - option 1
+  # Disposition dates, status
+  # Screen fail date
   derive_disposition_dt(
     dataset_ds = ds,
-    new_var = EOSDT1,
+    new_var = SCRFDT,
     dtc = DSSTDTC,
+    filter = expr(DSCAT == "DISPOSITION EVENT" & DSDECOD == "SCREEN FAILURE")
+  ) %>%
+
+  # EOS date
+  derive_disposition_dt(
+    dataset_ds = ds,
+    new_var = EOSDT,
+    dtc = DSSTDTC,
+    filter = expr(DSCAT == "DISPOSITION EVENT" & DSDECOD != "SCREEN FAILURE")
+  ) %>%
+
+  # EOS status
+  derive_disposition_eoxxstt(
+    dataset_ds = ds,
+    new_var = EOSSTT,
+    status_var = DSDECOD,
+    format_new_var = format_eoxxstt,
     filter = expr(DSCAT == "DISPOSITION EVENT")
   ) %>%
+
+  # study specific variables
+
+  # Last retrieval date
   derive_disposition_dt(
     dataset_ds = ds,
     new_var = FRVDT,
@@ -45,16 +110,38 @@ adsl <- dm %>%
     filter = expr(DSCAT == "OTHER EVENT" & DSDECOD == "FINAL RETRIEVAL VISIT")
   ) %>%
 
-  # Option 2
-  # derive study completion/discontinuation variables
-  derive_merged_vars(
-    dataset_add = ds,
-    filter_add = exprs(DSCAT == "DISPOSITION EVENT"),
-    new_vars = exprs(
-      EOSDT = convert_dtc_to_dt(impute_dtc(DSSTDTC, date_imputation = "FIRST")),
-      EOSSTT = if_else(DSDECOD == "COMPLETED", "COMPLETED", "DISCONTINUED"),
-      DCSREAS = if_else(DSDECOD != "COMPLETED", DSDECOD, "")
-    )
+  # Death date - impute partial date to first day/month
+  derive_vars_dt(
+    new_vars_prefix = "DTH",
+    dtc = DTHDTC,
+    date_imputation = "FIRST"
+  ) %>%
+
+  # Relative Day of Death
+  derive_duration(
+    new_var = DTHADY,
+    start_date = TRTSDT,
+    end_date = DTHDT
+  ) %>%
+  # Elapsed Days from Last Dose to Death
+  derive_duration(
+    new_var = LDDTHELD,
+    start_date = TRTEDT,
+    end_date = DTHDT,
+    add_one = FALSE
+  ) %>%
+
+  # Groupings, populations and others variables
+  mutate(
+    AGEGR1 = format_agegr1(AGE),
+    RACEGR1 = format_racegr1(RACE),
+    REGION1 = format_region1(COUNTRY),
+    LDDTHGR1 = format_lddthgr1(LDDTHELD),
+    FASFL = if_else(is.na(SCRFDT), "Y", "N"),
+    SAFFL = if_else(!is.na(TRTSDTM), "Y", "N"),
+    DTH30FL = if_else(LDDTHGR1 == "<= 30", "Y", NA_character_),
+    DTHA30FL = if_else(LDDTHGR1 == "> 30", "Y", NA_character_),
+    DTHB30FL = if_else(DTHDT <= TRTSDT + 30, "Y", NA_character_)
   )
 
 save(adsl, file = "data/adsl.rda", compress = TRUE)
