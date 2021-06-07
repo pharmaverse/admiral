42995ceab4ae03ab6560770f27ea757cd59ae894
Vignesh T <vignesh.x.thanikachalam@gsk.com>
2021-06-07 23:05:02 +0530

    Updated NAMESPACE

--- R/admiral-package.R
+++ R/admiral-package.R
@@ -9,11 +9,11 @@
 #'             is_call eval_bare caller_env friendly_type new_formula f_lhs
 #'             f_rhs expr_interp as_function quo call_name is_formula quo_squash
 #'             is_bare_formula is_quosure call2 set_names enexpr quo_get_expr
-#'             quo_is_symbol
+#'             quo_is_symbol is_character eval_tidy
 #' @importFrom utils capture.output
 #' @importFrom purrr map map2 map_chr map_lgl reduce walk keep map_if transpose
+#'             flatten
 #' @importFrom stringr str_c str_detect str_remove str_trim str_to_upper str_glue
-#'
 #' @importFrom assertthat assert_that is.number on_failure<-
 #' @importFrom lubridate ceiling_date days duration floor_date time_length %--%
 #'             ymd ymd_hms is.instant
