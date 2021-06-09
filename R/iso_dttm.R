ebc8cf8a23ee8bff27980dde0bb78d405b5a5008
neitmant <thomas.neitmann@roche.com>
2021-06-09 14:54:16 +0200

    Document S4 methods

--- R/iso_dttm.R
+++ R/iso_dttm.R
@@ -12,24 +12,33 @@ print.iso_dttm <- function(x, ...) {
 
 setOldClass("iso_dttm")
 
+#' Add Methods
+#'
+#' @param e1 First object
+#' @param e2 Second object
+#'
+#' @rdname add
 setMethod(
   "+",
   signature(e1 = "iso_dttm", e2 = "Period"),
   function(e1, e2) as_iso_dttm(as.POSIXct(e1) + e2)
 )
 
+#' @rdname add
 setMethod(
   "+",
   signature(e1 = "Period", e2 = "iso_dttm"),
   function(e1, e2) as_iso_dttm(e1 + as.POSIXct(e2))
 )
 
+#' @rdname add
 setMethod(
   "+",
   signature(e1 = "iso_dttm", e2 = "Duration"),
   function(e1, e2) as_iso_dttm(as.POSIXct(e1) + e2)
 )
 
+#' @rdname add
 setMethod(
   "+",
   signature(e1 = "Duration", e2 = "iso_dttm"),
