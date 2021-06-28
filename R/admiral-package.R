1f24931194415ad00f71a19f8796126aaa0a2baf
aehmann-gsk <82041175+aehmann-gsk@users.noreply.github.com>
2021-06-28 05:30:35 -0400

    Create supplemental domain join function
    * Initial copy
    
    * Draft version of suppqual
    
    * Add tidyr package into renv.lock
    
    * Fully baked suppqual function
    
    * Included tidyr into DESCRIPTION and updated NAMESPACE
    
    * Initial test script for derive_suppqual_vars
    
    * Modified NAMESPACE used in derive_suppqual_vars()
    
    * Renamed file derive_suppquall_vars.R -> derive_suppqual_vars.R
    
    * Included additional unit tests for derive_suppqual_vars()
    
    Co-authored-by: Vignesh Thanikachalam <vignesh.x.thanikachalam@gsk.com>
    Co-authored-by: neitmant <thomas.neitmann@roche.com>

--- R/admiral-package.R
+++ R/admiral-package.R
@@ -2,7 +2,7 @@
 #' @importFrom dplyr anti_join arrange bind_rows case_when desc ends_with full_join filter group_by
 #'             if_else inner_join mutate mutate_at n left_join pull rename row_number select slice
 #'             starts_with transmute ungroup vars n_distinct union bind_rows distinct
-#'             summarise_at summarise
+#'             summarise_at summarise coalesce bind_cols na_if tibble
 #' @importFrom magrittr %>%
 #' @importFrom rlang := abort arg_match as_string enquo expr exprs inform
 #'             quo_is_null sym syms warn is_symbol is_quosures is_integerish
@@ -13,10 +13,11 @@
 #'             .data
 #' @importFrom utils capture.output
 #' @importFrom purrr map map2 map_chr map_lgl reduce walk keep map_if transpose
-#'             flatten every
+#'             flatten every modify_at modify_if reduce compose
 #' @importFrom stringr str_c str_detect str_extract str_remove str_remove_all str_trim
 #'             str_to_upper str_glue
 #' @importFrom assertthat assert_that is.number on_failure<-
 #' @importFrom lubridate ceiling_date date days duration floor_date is.Date is.instant time_length %--%
 #'             ymd ymd_hms
+#' @importFrom tidyr spread
 "_PACKAGE"
