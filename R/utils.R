dd5fa516925e2445bae8992e67f03ed0ebb0e455
neitmant <thomas.neitmann@roche.com>
2021-07-15 10:25:45 +0200

    Move join functions to separate file

--- R/utils.R
+++ R/utils.R
@@ -152,20 +152,6 @@ extract_vars <- function(quosures) {
   structure(vars, class = "quosures")
 }
 
-inner_join <- function(x, y, by = NULL, copy = FALSE, suffix = c(".x", ".y"), ...) {
-  suppress_warning(
-    dplyr::inner_join(x, y, by = by, copy = copy, suffix = suffix, ...),
-    "^Column `.+` has different attributes on LHS and RHS of join$"
-  )
-}
-
-left_join <- function(x, y, by = NULL, copy = FALSE, suffix = c(".x", ".y"), ...) {
-  suppress_warning(
-    dplyr::left_join(x, y, by = by, copy = copy, suffix = suffix, ...),
-    "^Column `.+` has different attributes on LHS and RHS of join$"
-  )
-}
-
 #' Concatenate One or More Quosure(s)
 #'
 #' @param ... One or more objects of class `quosure` or `quosures`
