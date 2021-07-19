281abdb2d389b6f686a9734977593cd01597e9a5
neitmant <thomas.neitmann@roche.com>
2021-07-19 11:04:30 +0200

    Format code

--- tests/testthat/test-derive_summary_records.R
+++ tests/testthat/test-derive_summary_records.R
@@ -16,17 +16,20 @@ test_that("creates a new record for each group and new data frame retains groupi
 })
 
 test_that("unique records within `by_vars` are retained, not set to <NA>", {
-  input <- tibble(x = rep(1:2, each = 2),
-               y = rep_len("a", 4),
-               z = letters[1:4],
-               a = sample(1:100, 4))
-  actual_output <- input %>%
-    derive_summary_records(
-      by_vars = vars(x),
-      fns = list(a ~ sum)
-    )
-  expect_equal(actual_output$y, rep_len("a", 6))
-  expect_equal(actual_output$z, c("a", "b", NA, "c", "d", NA))
+  input <- tibble(
+    x = rep(1:2, each = 2),
+    y = rep_len("a", 4),
+    z = letters[1:4],
+    a = sample(1:100, 4)
+  )
+  output <- derive_summary_records(
+    input,
+    by_vars = vars(x),
+    fns = list(a ~ sum)
+  )
+
+  expect_equal(output$y, rep_len("a", 6))
+  expect_equal(output$z, c("a", "b", NA, "c", "d", NA))
 })
 
 
@@ -37,9 +40,12 @@ test_that("LHS of `fns` accepts multiple variable", {
     by_vars = vars(x),
     fns = list(vars(y, z) ~ mean)
   )
-  expected_output <- tibble(x = rep(1:2, each = 4),
-               y = c(9:10, 9.5, NA, 11:12, 11.5, NA),
-               z = c(101:102, NA, 101.5, 103:104, NA, 103.5))
+  expected_output <- tibble(
+    x = rep(1:2, each = 4),
+    y = c(9:10, 9.5, NA, 11:12, 11.5, NA),
+    z = c(101:102, NA, 101.5, 103:104, NA, 103.5)
+  )
+
   expect_dfs_equal(actual_output, expected_output, keys = c("x", "y", "z"))
 })
 
@@ -50,10 +56,13 @@ test_that("`fns` accepts single forumula without wrapping into list", {
     by_vars = vars(x),
     fns = y ~ mean
   )
-  expected_output <- tibble(x = rep(1:2, each = 3),
-               y = c(9:10, 9.5, 11:12, 11.5),
-               z = c(101:102, NA, 103:104, NA))
-  expect_equal(actual_output, expected_output, keys = c("x", "y", "z"))
+  expected_output <- tibble(
+    x = rep(1:2, each = 3),
+    y = c(9:10, 9.5, 11:12, 11.5),
+    z = c(101:102, NA, 103:104, NA)
+  )
+
+  expect_dfs_equal(actual_output, expected_output, keys = c("x", "y", "z"))
 })
 
 test_that("`fns` as inlined", {
@@ -63,10 +72,13 @@ test_that("`fns` as inlined", {
     by_vars = vars(x),
     fns = list(y ~ mean(., na.rm = TRUE))
   )
-  expected_output <- tibble(x = rep(1:2, each = 3),
-               y = c(9:10, 9.5, 11:12, 11.5),
-               z = c(101:102, NA, 103:104, NA))
-  expect_equal(actual_output, expected_output, keys = c("x", "y", "z"))
+  expected_output <- tibble(
+    x = rep(1:2, each = 3),
+    y = c(9:10, 9.5, 11:12, 11.5),
+    z = c(101:102, NA, 103:104, NA)
+  )
+
+  expect_dfs_equal(actual_output, expected_output, keys = c("x", "y", "z"))
 })
 
 test_that("set new value to a derived record", {
@@ -77,10 +89,13 @@ test_that("set new value to a derived record", {
     fns = list(y ~ mean),
     set_values_to = vars(z = "MEAN")
   )
-  expected_output <- tibble(x = rep(1:2, each = 3),
-               y = c(9:10, 9.5, 11:12, 11.5),
-               z = c(NA, NA, "MEAN", NA, NA, "MEAN"))
-  expect_equal(actual_output, expected_output, keys = c("x", "y", "z"))
+  expected_output <- tibble(
+    x = rep(1:2, each = 3),
+    y = c(9:10, 9.5, 11:12, 11.5),
+    z = c(NA, NA, "MEAN", NA, NA, "MEAN")
+  )
+
+  expect_dfs_equal(actual_output, expected_output, keys = c("x", "y", "z"))
 })
 
 test_that("drop a value from derived record", {
@@ -92,23 +107,27 @@ test_that("drop a value from derived record", {
     set_values_to = vars(d = "MEAN"),
     drop_values_from = vars(z)
   )
-  expected_output <- tibble(x = rep(1:2, each = 3),
-               y = c(9:10, 9.5, 11:12, 11.5),
-               z = c(1, 1, NA, 1, 1, NA),
-               d = c(NA, NA, "MEAN", NA, NA, "MEAN"))
-  expect_equal(actual_output, expected_output, keys = c("x", "y", "z"))
+  expected_output <- tibble(
+    x = rep(1:2, each = 3),
+    y = c(9:10, 9.5, 11:12, 11.5),
+    z = c(1, 1, NA, 1, 1, NA),
+    d = c(NA, NA, "MEAN", NA, NA, "MEAN")
+  )
+
+  expect_dfs_equal(actual_output, expected_output, keys = c("x", "y", "z"))
 })
 
 test_that("check `set_values_to` mapping", {
   input <- tibble(x = rep(1:4, each = 4), y = rep(1:2, each = 8), z = runif(16))
   input <- input %>% group_by(x, y)
-  actual_output <- input %>%
-    derive_summary_records(
-      by_vars = vars(x, y),
-      fns = list(z ~ mean, z ~ sum),
-      set_values_to = vars(d = c("MEAN", "SUM"))
-    )
+  actual_output <- derive_summary_records(
+    input,
+    by_vars = vars(x, y),
+    fns = list(z ~ mean, z ~ sum),
+    set_values_to = vars(d = c("MEAN", "SUM"))
+  )
   tf <- rep(c(rep_len(NA, 4), "MEAN", "SUM"), 4)
+
   expect_equal(actual_output$d, tf)
 })
 
@@ -123,7 +142,6 @@ test_that("Filter record within `by_vars`", {
     set_values_to = vars(d = "MEAN"),
     drop_values_from = vars(z)
   )
-
   expected_output <- tibble(
     x = c(rep(1, 2), rep(2, 4)),
     y = c(9:13, 12),
@@ -131,7 +149,7 @@ test_that("Filter record within `by_vars`", {
     d = c(rep(NA, 5), "MEAN")
   )
 
-  expect_equal(actual_output, expected_output, keys = c("x", "y", "z"))
+  expect_dfs_equal(actual_output, expected_output, keys = c("x", "y", "z"))
 
   actual_output <- derive_summary_records(
     input,
@@ -141,7 +159,6 @@ test_that("Filter record within `by_vars`", {
     set_values_to = vars(d = "MEAN"),
     drop_values_from = vars(z)
   )
-
   expected_output <- tibble(
     x = c(rep(1, 3), rep(2, 4)),
     y = c(9:10, 9.5, 11:13, 12.5),
@@ -149,7 +166,7 @@ test_that("Filter record within `by_vars`", {
     d = c(rep(NA, 2), "MEAN", rep(NA, 3), "MEAN")
   )
 
-  expect_equal(actual_output, expected_output, keys = c("x", "y", "z"))
+  expect_dfs_equal(actual_output, expected_output, keys = c("x", "y", "z"))
 })
 
 # Errors ---
