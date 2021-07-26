1dd9614a743485519649f5256dafd7652d97577e
neitmant <thomas.neitmann@roche.com>
2021-07-26 09:38:43 +0200

    Add test case

--- tests/testthat/test-call_derivation.R
+++ tests/testthat/test-call_derivation.R
@@ -38,3 +38,37 @@ test_that("call_derivation works", {
 
   expect_dfs_equal(expected_output, actual_output, keys = c("USUBJID", "VSTESTCD", "VISIT", "DTYPE", "VSSEQ"))
 })
+
+test_that("call_derivation works", {
+  input <- ae[sample(1:nrow(ae), 1000), ] %>%
+    left_join(adsl, by = "USUBJID")
+
+  expected_output <- input %>%
+    derive_vars_dt(
+      new_vars_prefix = "AST",
+      dtc = AESTDTC,
+      date_imputation = "first",
+      min_dates = TRTSDT,
+      max_dates = TRTEDT
+    ) %>%
+    derive_vars_dt(
+      new_vars_prefix = "AEN",
+      dtc = AEENDTC,
+      date_imputation = "last",
+      min_dates = list(TRTSDT),
+      max_date = list(TRTEDT)
+    )
+
+  actual_output <- call_derivation(
+    dataset = input,
+    derivation = derive_vars_dt,
+    variable_params = list(
+      params(dtc = AESTDTC, date_imputation = "first", new_vars_prefix = "AST"),
+      params(dtc = AEENDTC, date_imputation = "last", new_vars_prefix = "AEN")
+    ),
+    min_dates = list(TRTSDT),
+    max_date = list(TRTEDT)
+  )
+
+  expect_dfs_equal(expected_output, actual_output, keys = c("USUBJID", "AESEQ"))
+})
