76584c2543ca4b8f506592e7d4f5f317604797a0
Ondrej <ondrej.slama@roche.com>
2021-07-27 12:40:58 +0200

    derive_aage tests corrected

--- tests/testthat/test-derive_aage.R
+++ tests/testthat/test-derive_aage.R
@@ -15,12 +15,15 @@ test_that("duration and unit variable are added", {
 test_that("derive_agegr_fda works as expected", {
 
   input <- tibble::tibble(AGE = c(10, 18, 19, 50, 64, 65, 80))
-  expected_output <- mutate(input,
-                            AGEGR_EXP = factor(
-                              c(NA, NA, "19-64", "19-64", "19-64", ">=65", ">=65"),
-                              levels = c("19-64", ">=65", NA_character_),
-                              exclude = NULL
-                            ))
+
+  expected_output <- mutate(
+    input,
+    AGEGR_EXP = factor(
+      c(NA, NA, "19-64", "19-64", "19-64", ">=65", ">=65"),
+      levels = c("19-64", ">=65", NA_character_),
+      exclude = NULL
+    )
+  )
 
   expect_dfs_equal(derive_agegr_fda(input, AGE, AGEGR_EXP), expected_output, keys = "AGE")
 
@@ -28,33 +31,41 @@ test_that("derive_agegr_fda works as expected", {
 
 test_that("derive_agegr_ema works as expected", {
 
-  input <- tibble::tibble(AGE = c(10, 18, 19, 50, 64, 65, 80))
-  expected_output <- mutate(input,
-                            AGEGR_EXP = factor(
-                              c(NA, NA, "19-64", "19-64", "19-64", ">=65", ">=65"),
-                              levels = c("19-64", ">=65", NA_character_),
-                              exclude = NULL
-                            ))
+  input <- tibble::tibble(AGE = c(10, 18, 19, 50, 64, 65, 80, 85))
 
-  expect_dfs_equal(derive_agegr_fda(input, AGE, AGEGR_EXP), expected_output, keys = "AGE")
+  expected_output <- mutate(
+    input,
+    AGEGR_EXP = factor(
+      c(NA, "18-64", "18-64", "18-64", "18-64", "65-84", "65-84", ">=85"),
+      levels = c("18-64", "65-84", ">=85", NA_character_),
+      exclude = NULL
+    )
+  )
+
+  expect_dfs_equal(derive_agegr_ema(input, AGE, AGEGR_EXP), expected_output, keys = "AGE")
 
 })
 
 test_that("derive_agegr_ema - pediatric version - works as expected", {
 
   input <- tibble::tibble(AGE = c(1, 2, 11, 12, 17, 18))
-  expected_output <- mutate(input,
-                            AGEGR_EXP = factor(
-                              c("0-1 (Newborns / Infants / Toddlers)", "2-11 (Children)",
-                                "2-11 (Children)", "12-17 (Adolescents)", "12-17 (Adolescents)",
-                                NA),
-                              levels = c("0-1 (Newborns / Infants / Toddlers)", "2-11 (Children)",
-                                         "12-17 (Adolescents)", NA_character_),
-                              exclude = NULL
-                            ))
 
-  expect_dfs_equal(derive_agegr_ema(input, AGE, AGEGR_EXP, adults = FALSE),
-                   expected_output,
-                   keys = "AGE")
+  expected_output <- mutate(
+    input,
+    AGEGR_EXP = factor(
+      c("0-1 (Newborns / Infants / Toddlers)", "2-11 (Children)",
+        "2-11 (Children)", "12-17 (Adolescents)", "12-17 (Adolescents)",
+        NA),
+      levels = c("0-1 (Newborns / Infants / Toddlers)", "2-11 (Children)",
+                 "12-17 (Adolescents)", NA_character_),
+      exclude = NULL
+    )
+  )
+
+  expect_dfs_equal(
+    derive_agegr_ema(input, AGE, AGEGR_EXP, adults = FALSE),
+    expected_output,
+    keys = "AGE"
+  )
 
 })
