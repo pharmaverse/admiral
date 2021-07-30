f0f19794152960a599ab98974301a1f2766e73e8
neitmant <thomas.neitmann@roche.com>
2021-07-30 14:07:17 +0200

    Document functions

--- R/call_derivations.R
+++ R/call_derivations.R
@@ -1,3 +1,59 @@
+#' Call a Single Derivation Multiple Times
+#'
+#' Call a single derivation multiple times with some parameters being fixed across
+#' iterations and others varying.
+#'
+#' @param dataset The input dataset
+#' @param derivation The derivation function to call
+#' @param variable_params A `list` of arguments that are different across iterations.
+#'   Each set of arguments must be created using [`params()`].
+#' @param ... Any number of arguments that are fixed across iterations
+#'
+#' @author Thomas Neitmann, Stefan Bundfuss
+#'
+#' @return
+#' The input dataset with additional records/variables added depending on
+#' which `derivation` has been used.
+#'
+#' @export
+#' @seealso params
+#'
+#' @examples
+#' library(dplyr, warn.conflicts = FLASE)
+#' data(ae)
+#' data(adsl)
+#'
+#' input <- ae[sample(1:nrow(ae), 1000), ] %>%
+#'   left_join(adsl, by = "USUBJID")
+#'
+#' ## Call the same derivation twice in a row
+#' expected_output <- input %>%
+#'   derive_vars_dt(
+#'     new_vars_prefix = "AST",
+#'     dtc = AESTDTC,
+#'     date_imputation = "first",
+#'     min_dates = list(TRTSDT),
+#'     max_dates = list(TRTEDT)
+#'   ) %>%
+#'   derive_vars_dt(
+#'     new_vars_prefix = "AEN",
+#'     dtc = AEENDTC,
+#'     date_imputation = "last",
+#'     min_dates = list(TRTSDT),
+#'     max_dates = list(TRTEDT)
+#'   )
+#'
+#' ## Call the same derivation in one go
+#' actual_output <- call_derivation(
+#'   dataset = input,
+#'   derivation = derive_vars_dt,
+#'   variable_params = list(
+#'     params(dtc = AESTDTC, date_imputation = "first", new_vars_prefix = "AST"),
+#'     params(dtc = AEENDTC, date_imputation = "last", new_vars_prefix = "AEN")
+#'   ),
+#'   min_dates = list(TRTSDT),
+#'   max_dates = list(TRTEDT)
+#' )
 call_derivation <- function(dataset, derivation, variable_params, ...) {
   assert_data_frame(dataset)
   assert_s3_class(derivation, "function")
@@ -20,6 +76,21 @@ call_derivation <- function(dataset, derivation, variable_params, ...) {
   dataset
 }
 
+#' Create a Set of Parameters
+#'
+#' Create a set of variable parameters to be used in an iteration of [`call_derivation()`]
+#'
+#' @param ... One or more named arguments
+#'
+#' @author Thomas Neitmann
+#' @return An object of class `params`
+#' @export
+#'
+#' @examples
+#' params(
+#'   fns = list(VSSTRESN ~ mean(., na.rm = TRUE)),
+#'   set_values_to = vars(DTYPE = "AVERAGE")
+#' )
 params <- function(...) {
   args <- eval(substitute(alist(...)))
   params <- names(args)
