9e00ab9fa223e7d0f7e22489058621591c5d371f
neitmant <thomas.neitmann@roche.com>
2021-08-03 10:02:34 +0200

    Indent properly

--- R/derive_adeg_params.R
+++ R/derive_adeg_params.R
@@ -58,16 +58,16 @@
 #'
 #' @examples
 #' adeg <- tibble::tribble(
-#' ~USUBJID,      ~PARAMCD, ~PARAM,        ~AVAL, ~AVALU,      ~VISIT,
-#' "01-701-1015", "HR",     "Heart Rate",  70.14, "beats/min", "BASELINE",
-#' "01-701-1015", "QT",     "QT Duration", 370,   "msec",      "WEEK 2",
-#' "01-701-1015", "HR",     "Heart Rate",  62.66, "beats/min", "WEEK 1",
-#' "01-701-1015", "RR",     "RR Duration", 710,   "msec",      "WEEK 2",
-#' "01-701-1028", "HR",     "Heart Rate",  85.45, "beats/min", "BASELINE",
-#' "01-701-1028", "QT",     "QT Duration", 480,   "msec",      "WEEK 2",
-#' "01-701-1028", "QT",     "QT Duration", 350,   "msec",      "WEEK 3",
-#' "01-701-1028", "HR",     "Heart Rate",  56.54, "beats/min", "WEEK 3",
-#' "01-701-1028", "RR",     "RR Duration", 842,   "msec",      "WEEK 2",
+#'   ~USUBJID,      ~PARAMCD, ~PARAM,        ~AVAL, ~AVALU,      ~VISIT,
+#'   "01-701-1015", "HR",     "Heart Rate",  70.14, "beats/min", "BASELINE",
+#'   "01-701-1015", "QT",     "QT Duration", 370,   "msec",      "WEEK 2",
+#'   "01-701-1015", "HR",     "Heart Rate",  62.66, "beats/min", "WEEK 1",
+#'   "01-701-1015", "RR",     "RR Duration", 710,   "msec",      "WEEK 2",
+#'   "01-701-1028", "HR",     "Heart Rate",  85.45, "beats/min", "BASELINE",
+#'   "01-701-1028", "QT",     "QT Duration", 480,   "msec",      "WEEK 2",
+#'   "01-701-1028", "QT",     "QT Duration", 350,   "msec",      "WEEK 3",
+#'   "01-701-1028", "HR",     "Heart Rate",  56.54, "beats/min", "WEEK 3",
+#'   "01-701-1028", "RR",     "RR Duration", 842,   "msec",      "WEEK 2",
 #' )
 #' derive_param_qtcb(
 #'   adeg,
@@ -149,16 +149,16 @@ derive_param_qtcb <- function(dataset,
 #'
 #' @examples
 #' adeg <- tibble::tribble(
-#' ~USUBJID,      ~PARAMCD, ~PARAM,        ~AVAL, ~AVALU,      ~VISIT,
-#' "01-701-1015", "HR",     "Heart Rate",  70.14, "beats/min", "BASELINE",
-#' "01-701-1015", "QT",     "QT Duration", 370,   "msec",      "WEEK 2",
-#' "01-701-1015", "HR",     "Heart Rate",  62.66, "beats/min", "WEEK 1",
-#' "01-701-1015", "RR",     "RR Duration", 710,   "msec",      "WEEK 2",
-#' "01-701-1028", "HR",     "Heart Rate",  85.45, "beats/min", "BASELINE",
-#' "01-701-1028", "QT",     "QT Duration", 480,   "msec",      "WEEK 2",
-#' "01-701-1028", "QT",     "QT Duration", 350,   "msec",      "WEEK 3",
-#' "01-701-1028", "HR",     "Heart Rate",  56.54, "beats/min", "WEEK 3",
-#' "01-701-1028", "RR",     "RR Duration", 842,   "msec",      "WEEK 2",
+#'   ~USUBJID,      ~PARAMCD, ~PARAM,        ~AVAL, ~AVALU,      ~VISIT,
+#'   "01-701-1015", "HR",     "Heart Rate",  70.14, "beats/min", "BASELINE",
+#'   "01-701-1015", "QT",     "QT Duration", 370,   "msec",      "WEEK 2",
+#'   "01-701-1015", "HR",     "Heart Rate",  62.66, "beats/min", "WEEK 1",
+#'   "01-701-1015", "RR",     "RR Duration", 710,   "msec",      "WEEK 2",
+#'   "01-701-1028", "HR",     "Heart Rate",  85.45, "beats/min", "BASELINE",
+#'   "01-701-1028", "QT",     "QT Duration", 480,   "msec",      "WEEK 2",
+#'   "01-701-1028", "QT",     "QT Duration", 350,   "msec",      "WEEK 3",
+#'   "01-701-1028", "HR",     "Heart Rate",  56.54, "beats/min", "WEEK 3",
+#'   "01-701-1028", "RR",     "RR Duration", 842,   "msec",      "WEEK 2",
 #' )
 #' derive_param_qtcf(
 #'   adeg,
@@ -240,16 +240,16 @@ derive_param_qtcf <- function(dataset,
 #'
 #' @examples
 #' adeg <- tibble::tribble(
-#' ~USUBJID,      ~PARAMCD, ~PARAM,        ~AVAL, ~AVALU,      ~VISIT,
-#' "01-701-1015", "HR",     "Heart Rate",  70.14, "beats/min", "BASELINE",
-#' "01-701-1015", "QT",     "QT Duration", 370,   "msec",      "WEEK 2",
-#' "01-701-1015", "HR",     "Heart Rate",  62.66, "beats/min", "WEEK 1",
-#' "01-701-1015", "RR",     "RR Duration", 710,   "msec",      "WEEK 2",
-#' "01-701-1028", "HR",     "Heart Rate",  85.45, "beats/min", "BASELINE",
-#' "01-701-1028", "QT",     "QT Duration", 480,   "msec",      "WEEK 2",
-#' "01-701-1028", "QT",     "QT Duration", 350,   "msec",      "WEEK 3",
-#' "01-701-1028", "HR",     "Heart Rate",  56.54, "beats/min", "WEEK 3",
-#' "01-701-1028", "RR",     "RR Duration", 842,   "msec",      "WEEK 2",
+#'   ~USUBJID,      ~PARAMCD, ~PARAM,        ~AVAL, ~AVALU,      ~VISIT,
+#'   "01-701-1015", "HR",     "Heart Rate",  70.14, "beats/min", "BASELINE",
+#'   "01-701-1015", "QT",     "QT Duration", 370,   "msec",      "WEEK 2",
+#'   "01-701-1015", "HR",     "Heart Rate",  62.66, "beats/min", "WEEK 1",
+#'   "01-701-1015", "RR",     "RR Duration", 710,   "msec",      "WEEK 2",
+#'   "01-701-1028", "HR",     "Heart Rate",  85.45, "beats/min", "BASELINE",
+#'   "01-701-1028", "QT",     "QT Duration", 480,   "msec",      "WEEK 2",
+#'   "01-701-1028", "QT",     "QT Duration", 350,   "msec",      "WEEK 3",
+#'   "01-701-1028", "HR",     "Heart Rate",  56.54, "beats/min", "WEEK 3",
+#'   "01-701-1028", "RR",     "RR Duration", 842,   "msec",      "WEEK 2",
 #' )
 #' derive_param_qtlc(
 #'   adeg,
