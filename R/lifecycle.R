59a76a5ab0aafd90097a2c4b76ae37532de43986
neitmant <thomas.neitmann@roche.com>
2021-09-01 14:44:49 +0200

    Implement fall back for `deprecate_warn()`

--- R/lifecycle.R
+++ R/lifecycle.R
@@ -1,14 +1,22 @@
+# Re-implemented functions from {lifecycle} package for internal use in case
+# the package is not available
 
-# Re-implemented functions from "lifecycle" package
-
-deprecate_warn <- function(...) {
+deprecate_warn <- function(when, what, with, ...) {
   if (requireNamespace("lifecycle", quietly = TRUE)) {
-    lifecycle::deprecate_warn(...)
+    lifecycle::deprecate_warn(when, what, with, ...)
   } else {
-    invisible()
+    msg <- sprintf(
+      "`%s` is deprecated as of admiral %s.\nPlease use `%s` instead.",
+      what, when, with
+    )
+    warn(msg)
   }
 }
 
-deprecated <- function() rlang::missing_arg()
+deprecated <- function() {
+  rlang::missing_arg()
+}
 
-is_present <- function(arg) !rlang::is_missing(rlang::maybe_missing(arg))
+is_present <- function(arg) {
+  !rlang::is_missing(rlang::maybe_missing(arg))
+}
