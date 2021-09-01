fb654090f8d80629423e4bdcd3693209fb2f030c
Ondrej <ondrej.slama@roche.com>
2021-09-01 17:10:53 +0200

    bug fix

--- R/zzz.R
+++ R/zzz.R
@@ -11,4 +11,13 @@ deprecate_warn <- function(...) {
 
 deprecated <- function() rlang::missing_arg()
 
-is_present <- function(arg) !rlang::is_missing(rlang::maybe_missing(arg))
+is_present <- function(arg, quoted = FALSE) {
+  stopifnot(rlang::is_scalar_logical(quoted))
+  if (quoted) {
+    arg <- rlang::quo_get_expr(arg)
+    if (identical(arg, quote(deprecated()))) {
+      return(FALSE)
+    }
+  }
+  !rlang::is_missing(rlang::maybe_missing(arg))
+}
