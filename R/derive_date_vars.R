8ce67f1c638e7d6eff67a73358db90fe3b243f0a
Hamza RAHAL <rahal.hmza@yahoo.fr>
2021-10-14 13:38:34 +0100

    Add a call of assert_vars() for both min_dates and max_dates to check that must be a list of variable names
    Add optional = TRUE to assert_vars() call
    
    Replace min_dates = list(...) with min_dates = vars(...)

--- R/derive_date_vars.R
+++ R/derive_date_vars.R
@@ -46,7 +46,7 @@
 #' ```
 #' impute_dtc(
 #'   "2020-11",
-#'   min_dates = list(ymd_hms("2020-12-06T12:12:12"),
+#'   min_dates = vars(ymd_hms("2020-12-06T12:12:12"),
 #'                    ymd_hms("2020-11-11T11:11:11")),
 #'   date_imputation = "first"
 #' )
@@ -134,7 +134,7 @@
 #' # minimum dates
 #' impute_dtc(
 #'   "2020-12",
-#'   min_dates = list(
+#'   min_dates = vars(
 #'     ymd_hms("2020-12-06T12:12:12"),
 #'     ymd_hms("2020-11-11T11:11:11")
 #'   ),
