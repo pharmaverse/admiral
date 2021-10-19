828dcd5aa1b9154d91a7dcbc212736cf23f9a16f
Ben Straub <ben.x.straub@gsk.com>
2021-10-19 13:57:10 -0400

    #385 Updated rox header for derive_last_dose_date to use inheritParams.  Associated test file checks trace_vars now.

--- R/derive_last_dose_date.R
+++ R/derive_last_dose_date.R
@@ -1,58 +1,22 @@
-#' Derive Last Dose Date(-time)
+#' Derive Last Dose Date-Time
 #'
-#' @param dataset Input dataset.
-#' @param dataset_ex Input EX dataset.
-#' @param filter_ex Filtering condition applied to EX dataset.
-#' For example, it can be used to filter for valid dose.
-#' Defaults to NULL.
-#' @param by_vars Variables to join by (created by `dplyr::vars`).
-#' @param dose_start The dose start date variable.
-#' @param dose_end The dose end date variable.
-#' @param analysis_date The analysis date variable.
-#' @param dataset_seq_var The sequence variable
-#' (this together with `by_vars` creates the keys of `dataset`).
+#' Displays the start date or start datetime from the exposure dataset relative to an adverse event
+#'
+#' @inheritParams get_last_dose
 #' @param new_var The output variable.
-#' @param output_datetime Logical. Should only date or date-time variable be returned?
-#' Defaults to `TRUE` (i.e. date-time variable).
-#' @param check_dates_only Logical.
-#' An assumption that start and end dates of treatment match is checked.
-#' By default (`FALSE`), the date as well as the time component is checked.
-#' If set to `TRUE`, then only the date component of those variables is checked.
-#' @param traceability_vars A named list returned by [`vars()`] listing the traceability variables,
-#' e.g. `vars(LDOSEDOM = "EX", LDOSESEQ = EXSEQ)`.
-#' The left-hand side (names of the list elements) gives the names of the traceability variables
-#' in the returned dataset.
-#' The right-hand side (values of the list elements) gives the values of the traceability variables
-#' in the returned dataset.
-#' These can be either strings or symbols referring to existing variables.
+#' @param output_datetime  Display `new_var` as datetime or as date
 #'
-#' @details All date (date-time) variables can be characters in standard ISO format or
-#' of date / date-time class.
-#' For ISO format, see [`impute_dtc`] - parameter `dtc` for further details.
-#' Date-time imputations are done as follows:
-#' * `dose_end`: no date imputation, time imputation to `00:00:00` if time is missing.
-#' * `analysis_date`: no date imputation, time imputation to `23:59:59` if time is missing.
-#'
-#' The last dose date is derived as follows:
-#' 1. The `dataset_ex` is filtered using `filter_ex`, if provided.
-#' This is useful for, for example, filtering for valid dose only.
-#' 2. The datasets `dataset` and `dataset_ex` are joined using `by_vars`.
-#' 3. The last dose date is derived:
-#' the last dose date is the maximum date where `dose_end` is lower to or equal to
-#' `analysis_date`, subject to both date values are non-NA.
-#' The last dose date is derived per `by_vars` and `dataset_seq_var`.
-#' 4. The last dose date is appended to the `dataset` and returned to the user.
-#'
-#' Furthermore, the following assumption is checked: start and end dates (datetimes) need to match.
-#' Use `check_dates_only` to control whether only dates or whole date-times need to be equal.
+#' @details The last dose date
 #'
 #' @return Input dataset with additional column `new_var`.
 #'
-#' @author Ondrej Slama
+#' @author Ben Straub
 #'
 #' @keywords adae derivation
 #'
 #' @export
+#'
+#' @seealso [get_last_dose()]
 #'
 #' @examples
 #' library(dplyr, warn.conflicts = FALSE)
@@ -61,7 +25,7 @@
 #'
 #' ae %>%
 #'   head(100) %>%
-#'   derive_last_dose(
+#'   derive_last_dose_date(
 #'     head(ex_single, 100),
 #'     filter_ex = (EXDOSE > 0 | (EXDOSE == 0 & grepl("PLACEBO", EXTRT))) &
 #'       nchar(EXENDTC) >= 10,
@@ -88,7 +52,7 @@ derive_last_dose_date <- function(dataset,
                                   check_dates_only,
                                   traceability_vars ){
 
-  # assert funtions found in assertions.R
+  # assert functions found in assertions.R
   filter_ex <- assert_filter_cond(enquo(filter_ex), optional = TRUE)
   by_vars <- assert_vars(by_vars)
   dose_start <- assert_symbol(enquo(dose_start))
@@ -98,7 +62,7 @@ derive_last_dose_date <- function(dataset,
   new_var <- assert_symbol(enquo(new_var))
   assert_logical_scalar(check_dates_only)
   assert_logical_scalar(output_datetime)
-  stopifnot(is_quosures(traceability_vars) | is.null(traceability_vars))
+  trace_vars_str <- names(traceability_vars)
   assert_data_frame(dataset, quo_c(by_vars, analysis_date, dataset_seq_var))
   assert_data_frame(dataset_ex, quo_c(by_vars, dose_start, dose_end))
 
@@ -112,17 +76,13 @@ derive_last_dose_date <- function(dataset,
                 dataset_seq_var = !!dataset_seq_var,
                 check_dates_only = !!check_dates_only,
                 traceability_vars = traceability_vars) %>%
-  select(colnames(dataset), !!dose_start)
-
-  # User-defined variable rename
-  if (!is.null(new_var)){
-    res <- res %>% rename(!!new_var := !!dose_start)}
-  else {res}
+  select(colnames(dataset), !!new_var := !!dose_start, !!!syms(trace_vars_str))
 
   # return either date or date-time variable
   if (!output_datetime) {
     res <- res %>%  mutate(!!new_var := as.Date(!!new_var))}
-  else {res}
+  else {
+    res <- res %>% mutate(!!new_var := as.POSIXct(as.character(!!new_var)))}
 
   return(res)
 }
--- tests/testthat/test-derive_last_dose_date.R
+++ tests/testthat/test-derive_last_dose_date.R
@@ -24,14 +24,18 @@ input_ex <- tibble::tribble(
   mutate(EXSTDTC = as.Date(EXSTDTC), EXENDTC = as.Date(EXENDTC))
 
 
-test_that("derive_last_dose works as expected", {
+test_that("derive_last_dose_date works as expected output_datetime = FALSE", {
 
-  expected_output <- mutate(
-    input_ae,
-    LDOSEDTM = as.POSIXct(
-      c("2020-01-01", "2020-08-29", "2020-09-02", NA, "2020-01-20", NA, NA), tz = "UTC"
-    )
-  )
+  expected_output <-  tibble::tribble(
+  ~STUDYID,        ~USUBJID,  ~AESEQ,    ~AESTDTC,      ~LDOSEDTM,
+    "my_study",   "subject1",  1,        "2020-01-02",   "2020-01-01",
+    "my_study",   "subject1",  2,        "2020-08-31",   "2020-08-29",
+    "my_study",   "subject1",  3,        "2020-10-10",   "2020-09-02",
+    "my_study",   "subject2",  1,        "2019-05-15",   NA_character_,
+    "my_study",   "subject2",  2,        "2020-02-20",   "2020-01-20",
+    "my_study",   "subject3",  1,        "2020-03-02",   NA_character_,
+    "my_study",   "subject4",  1,        "2020-11-02",   NA_character_) %>%
+    mutate(LDOSEDTM = as.Date(LDOSEDTM))
 
   res <- derive_last_dose_date(
     input_ae,
@@ -44,8 +48,75 @@ test_that("derive_last_dose works as expected", {
     dataset_seq_var = AESEQ,
     new_var = LDOSEDTM,
     check_dates_only = FALSE,
+    output_datetime = FALSE,
     traceability_vars = NULL
   )
+
+  expect_dfs_equal(expected_output, res, keys = c("STUDYID", "USUBJID", "AESEQ", "AESTDTC"))
+
+})
+
+test_that("derive_last_dose_date works as expected with output_datetime = TRUE", {
+
+  expected_output <-  tibble::tribble(
+    ~STUDYID,        ~USUBJID,  ~AESEQ,    ~AESTDTC,      ~LDOSEDTM,
+    "my_study",   "subject1",  1,        "2020-01-02",   "2020-01-01 00:00:00",
+    "my_study",   "subject1",  2,        "2020-08-31",   "2020-08-29 00:00:00",
+    "my_study",   "subject1",  3,        "2020-10-10",   "2020-09-02 00:00:00",
+    "my_study",   "subject2",  1,        "2019-05-15",   NA_character_,
+    "my_study",   "subject2",  2,        "2020-02-20",   "2020-01-20 00:00:00",
+    "my_study",   "subject3",  1,        "2020-03-02",   NA_character_,
+    "my_study",   "subject4",  1,        "2020-11-02",   NA_character_) %>%
+    mutate(LDOSEDTM = as.POSIXct(as.character(LDOSEDTM)))
+
+  res <- derive_last_dose_date(
+    input_ae,
+    input_ex,
+    filter_ex = (EXDOSE > 0) | (EXDOSE == 0 & EXTRT == "placebo"),
+    by_vars = vars(STUDYID, USUBJID),
+    dose_start = EXSTDTC,
+    dose_end = EXENDTC,
+    analysis_date = AESTDTC,
+    dataset_seq_var = AESEQ,
+    new_var = LDOSEDTM,
+    check_dates_only = FALSE,
+    output_datetime = TRUE,
+    traceability_vars = NULL
+  )
+
+  expect_dfs_equal(expected_output, res, keys = c("STUDYID", "USUBJID", "AESEQ", "AESTDTC"))
+
+})
+
+test_that("derive_last_dose_date returns traceability vars", {
+
+  expected_output <-  tibble::tribble(
+    ~STUDYID,    ~USUBJID,   ~AESEQ,  ~AESTDTC,      ~LDOSEDTM,
+    "my_study",  "subject1",  1,      "2020-01-02",  "2020-01-01 00:00:00",
+    "my_study",  "subject1",  2,      "2020-08-31",  "2020-08-29 00:00:00",
+    "my_study",  "subject1",  3,      "2020-10-10",  "2020-09-02 00:00:00",
+    "my_study",  "subject2",  1,      "2019-05-15",  NA_character_,
+    "my_study",  "subject2",  2,      "2020-02-20",  "2020-01-20 00:00:00",
+    "my_study",  "subject3",  1,      "2020-03-02",  NA_character_,
+    "my_study",  "subject4",  1,      "2020-11-02",  NA_character_) %>%
+    mutate(LDOSEDTM = as.POSIXct(as.character(LDOSEDTM)),
+          LDOSEDOM = c("EX", "EX", "EX", NA, "EX", NA, NA),
+          LDOSESEQ = c(1, 2, 3, NA, 2, NA, NA),
+          LDOSEVAR = c("EXSTDTC", "EXSTDTC", "EXSTDTC", NA, "EXSTDTC", NA, NA))
+
+  res <- derive_last_dose_date(
+    input_ae,
+    input_ex,
+    filter_ex = (EXDOSE > 0) | (EXDOSE == 0 & EXTRT == "placebo"),
+    by_vars = vars(STUDYID, USUBJID),
+    dose_start = EXSTDTC,
+    dose_end = EXENDTC,
+    analysis_date = AESTDTC,
+    dataset_seq_var = AESEQ,
+    new_var = LDOSEDTM,
+    check_dates_only = FALSE,
+    output_datetime = TRUE,
+    traceability_vars = dplyr::vars(LDOSEDOM = "EX", LDOSESEQ = EXSEQ, LDOSEVAR = "EXSTDTC"))
 
   expect_dfs_equal(expected_output, res, keys = c("STUDYID", "USUBJID", "AESEQ", "AESTDTC"))
 
