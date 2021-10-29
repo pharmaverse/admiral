6c86e54934590e8ecfd0c5a95641297884c76894
Pooja Kumari <pooja.2.kumari@gsk.com>
2021-10-29 23:08:49 +0530

    draft derive_vars_trt_start.R for Teckla to take over from Pooja K.

---
+++ R/derive_vars_trt_start.R
@@ -0,0 +1,76 @@
+#' Derive Datetime of First Exposure to Treatment
+#'
+#' Derives datetime of first exposure to treatment (`TRTSDTM`)
+#'
+#' @param dataset Input dataset
+#'
+#'   The variables specified by the `by_vars` parameter are expected.
+#'
+#' @param dataset_ex `ex` dataset
+#'
+#'   The variables `EXSTDTC`, `EXSEQ`, and those specified by the `filter_ex`
+#'   parameter are expected.
+#'
+#' @param filter_ex Fiter condition for the ex dataset
+#'
+#'   Only observations of the ex dataset which fulfill the specified condition
+#'   are considered for the treatment start date.
+#'
+#'   Default: `(EXDOSE > 0 | (EXDOSE == 0 & str_detect(EXTRT, "PLACEBO"))) & nchar(EXSTDTC) >= 10`
+#'
+#'   Permitted Values: logical expression
+#'
+#' @param subject_keys Variables to uniquely identify a subject
+#'
+#' A list of quosures where the expressions are symbols as returned by
+#' `vars()` is expected.
+#'
+#' @details For each group (with respect to the variables specified for the
+#'   `by_vars` parameter) the first observation (with respect to the order
+#'   specified for the `order` parameter) is included in the output dataset.
+#'
+#' @author Stefan Bundfuss
+#'
+#' @return The input dataset with `TRTSDTM` variable added
+#'
+#' @export
+#'
+#' @keywords adsl timing derivation
+#'
+#' @examples
+#' library(dplyr, warn.conflicts = FALSE)
+#' data("ex")
+#' data("dm")
+#'
+#' dm %>%
+#'   derive_var_trtsdtm(dataset_ex = ex) %>%
+#'   select(USUBJID, TRTSDTM)
+derive_var_trtsdtm <- function(dataset,
+                               dataset_ex,
+                               filter_ex = NULL,
+                               subject_keys = vars(STUDYID, USUBJID),
+                               dtc = EXSTDTC,
+                               ord = EXSEQ) {
+  assert_data_frame(dataset, subject_keys)
+  assert_data_frame(dataset_ex, required_vars = quo_c(subject_keys, dtc, ord))
+  filter_ex <- assert_filter_cond(enquo(filter_ex), optional = TRUE)
+
+  add <- dataset_ex %>%
+    transmute(
+      !!!subject_keys,
+      TRTSDTM = convert_dtc_to_dtm(dtc,
+        date_imputation = "FIRST",
+        time_imputation = "FIRST",
+        min_dates = NULL,
+        max_dates = NULL
+      )
+    ) %>%
+    filter_if(filter_ex) %>%
+    filter_extreme(
+      order = vars(dtc, ord),
+      by_vars = subject_keys,
+      mode = "first"
+    )
+
+  left_join(dataset, add, by = vars2chr(subject_keys))
+}
