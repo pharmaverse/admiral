e6999a39818a63ff252939032fbf27ef6b15cd23
teckla-gsk <82049812+teckla-gsk@users.noreply.github.com>
2021-11-09 23:44:57 -0500

    update based on user feedback

--- R/derive_vars_trt_end.R
+++ R/derive_vars_trt_end.R
@@ -1,14 +1,14 @@
-#' Derive Datetime of First Exposure to Treatment
+#' Derive Datetime of Last Exposure to Treatment
 #'
-#' Derives datetime of first exposure to treatment (`TRTSDTM`)
+#' Derives datetime of last exposure to treatment (`TRTEDTM`)
 #'
 #' @param dataset Input dataset
 #'
-#'   The variables specified by the `subject_keys` parameter are expected.
+#'   The variables specified by the `by_vars` parameter are expected.
 #'
 #' @param dataset_ex `ex` dataset
 #'
-#'   The variables `EXSTDTC`, `EXSEQ`, and those specified by the `filter_ex`
+#'   The variables `EXENDTC`, `EXSEQ`, and those specified by the `filter_ex`,
 #'   `dtc` and `subject_keys`parameters are expected.
 #'
 #' @param filter_ex Filter condition for the ex dataset
@@ -25,9 +25,9 @@
 #' A list of quosures where the expressions are symbols as returned by
 #' `vars()` is expected.
 #'
-#' @param new_var name of treatment start date e.g. `TRTSDTM`
+#' @param new_var name of treatment end date e.g. `TRTEDTM`
 #'
-#' @param dtc name of variable from which start date is derived e.g. `EXSTDTC`
+#' @param dtc name of variable from which end date is derived e.g. `EXENDTC`
 #'
 #' @param date_imputation The value to impute the day/month when a datepart is
 #'   missing.
@@ -71,7 +71,7 @@
 #' @param ord_vars
 #'  Order of variables for which either the last or first observation
 #'  is picked as treatment start date
-#'  e.g. vars(EXSTDTC,EXSEQ)
+#'  e.g. vars(EXENDTC,EXSEQ)
 #'
 #' @param ord_filter
 #'  The mode by which the filter_extreme is chosen.
@@ -86,7 +86,7 @@
 #'
 #' @author Stefan Bundfuss, Teckla Akinyi
 #'
-#' @return The input dataset with start date variable added e.g. `TRTSDTM`
+#' @return The input dataset with start date variable added e.g. `TRTEDTM`
 #'
 #' @export
 #'
@@ -98,61 +98,63 @@
 #' data("dm")
 #'
 #' dm %>%
-#'   derive_vars_trt_start(dataset_ex = ex
+#'   derive_vars_trt_end(dataset_ex = ex
 #'                         filter_ex =  (EXDOSE > 0 | (EXDOSE == 0 & str_detect(EXTRT, "PLACEBO"))) & nchar(EXSTDTC) >= 10, # nolint
 #'                         subject_keys = vars(STUDYID, USUBJID),
-#'                         new_var=TRTSDTM,
-#'                         dtc=EXSTDTC,
-#'                         date_imputation = "first",
-#'                         time_imputation="first",
+#'                         new_var=TRTEDTM,
+#'                         dtc=EXENDTC,
+#'                         date_imputation = "last",
+#'                         time_imputation="last",
 #'                         min_dates = NULL,
 #'                         max_dates = NULL,
-#'                         ord_vars = vars(EXSTDTC,EXSEQ),
-#'                         ord_filter="first") %>%
-#'   select(USUBJID, TRTSDTM)
-derive_vars_trt_start <- function(dataset,
-                                 dataset_ex,
-                                 filter_ex = NULL,
-                                 subject_keys,
-                                 new_var,
-                                 dtc,
-                                 date_imputation,
-                                 time_imputation,
-                                 min_dates = NULL,
-                                 max_dates = NULL,
-                                 ord_vars,
-                                 ord_filter) {
-    new_var <- assert_symbol(enquo(new_var))
-    dtc <- assert_symbol(enquo(dtc))
-    ord <- assert_symbol(enquo(ord))
-    assert_data_frame(dataset,subject_keys)
-    assert_data_frame(dataset_ex, required_vars = quo_c(subject_keys, ord_vars))
-    assert_character_scalar(ord_filter, values = c("first","last"))
+#'                         ord_vars = vars(EXENDTC,EXSEQ),
+#'                         ord_filter="last") %>%
+#'   select(USUBJID, TRTEDTM)
 
-    filter_ex <- assert_filter_cond(enquo(filter_ex), optional = TRUE)
+derive_vars_trt_end <- function(dataset,
+                                  dataset_ex,
+                                  filter_ex = NULL,
+                                  subject_keys,
+                                  new_var,
+                                  dtc,
+                                  date_imputation,
+                                  time_imputation,
+                                  min_dates = NULL,
+                                  max_dates = NULL,
+                                  ord_vars,
+                                  ord_filter) {
+  new_var <- assert_symbol(enquo(new_var))
+  dtc <- assert_symbol(enquo(dtc))
+  ord <- assert_symbol(enquo(ord))
+  assert_data_frame(dataset,subject_keys)
+  assert_data_frame(dataset_ex, required_vars = quo_c(subject_keys, ord_vars))
+  assert_character_scalar(ord_filter, values = c("first","last"))
 
-    add <- dataset_ex %>%
-      mutate(imputed_dtc = convert_dtc_to_dtm(!!dtc,
-                                              date_imputation = date_imputation,
-                                              time_imputation = time_imputation,
-                                              min_dates = min_dates,
-                                              max_dates = max_dates)
-      )
-    if (!quo_is_null(filter_pre_timepoint)){
-      add %>% filter_if(filter_ex) %>%
-        filter_extreme(
-          order = vars(!!dtc, !!ord_vars),
-          by_vars = !!!subject_keys,
-          mode = ord_filter) %>%
-        mutate(!!new_var := imputed_dtc)
-    } else {
+  filter_ex <- assert_filter_cond(enquo(filter_ex), optional = TRUE)
+
+  add <- dataset_ex %>%
+    mutate(imputed_dtc = convert_dtc_to_dtm(!!dtc,
+                                            date_imputation = date_imputation,
+                                            time_imputation = time_imputation,
+                                            min_dates = min_dates,
+                                            max_dates = max_dates)
+    )
+  if (!quo_is_null(filter_pre_timepoint)){
+    add %>% filter_if(filter_ex) %>%
       filter_extreme(
-        order = ord,
+        order = vars(!!dtc, !!ord_vars),
         by_vars = !!!subject_keys,
         mode = ord_filter) %>%
-        mutate(!!new_var := imputed_dtc)
-    }
-
-    left_join(dataset, add, by = vars2chr(subject_keys)) %>%
-      select(-imputed_dtc)
+      mutate(!!new_var := imputed_dtc)
+  } else {
+    filter_extreme(
+      order = ord,
+      by_vars = !!!subject_keys,
+      mode = ord_filter) %>%
+      mutate(!!new_var := imputed_dtc)
   }
+
+  left_join(dataset, add, by = vars2chr(subject_keys)) %>%
+    select(-imputed_dtc)
+}
+
