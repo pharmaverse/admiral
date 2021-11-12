7b5b8ce1e6af9cc5cf614f3bab2f60bb1ce42170
neitmant <thomas.neitmann@roche.com>
2021-11-12 10:42:31 +0100

    Test `get_duplicates_dataset()`

--- tests/testthat/test-duplicates.R
+++ tests/testthat/test-duplicates.R
@@ -1,6 +1,4 @@
-context("test-duplicates")
-
-test_that("multiplication works", {
+test_that("duplicate records are extracted", {
   input <- tibble::tribble(
     ~USUBJID, ~COUNTRY, ~AAGE,
     "P01",    "GER",    22,
@@ -15,5 +13,29 @@ test_that("multiplication works", {
   expect_equal(
     expected_ouput,
     extract_duplicate_records(input, vars(USUBJID))
+  )
+})
+
+test_that("dataset of duplicate records can be accessed using `get_duplicates_dataset()`", {
+  input <- tibble::tribble(
+    ~USUBJID, ~COUNTRY, ~AAGE,
+    "P01",    "GER",    22,
+    "P01",    "JPN",    34,
+    "P02",    "CZE",    41,
+    "P03",    "AUS",    39,
+    "P04",    "BRA",    21,
+    "P04",    "BRA",    21
+  )
+  expected_ouput <- input[c(1:2, 5:6), ]
+
+  expect_error(
+    signal_duplicate_records(input, vars(USUBJID)),
+    "Dataset contains duplicate records with respect to `USUBJID`",
+    fixed = TRUE
+  )
+
+  expect_equal(
+    expected_ouput,
+    get_duplicates_dataset()
   )
 })
