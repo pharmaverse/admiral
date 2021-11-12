7adfbcc61454a376f450d5ef36349ee45b41a568
Pavan Kumar Kanagondhi Prasanna Kumar <pavan.10.kumar@gsk.com>
2021-11-12 12:14:35 +0000

    done

--- R/derive_summary_records.R
+++ R/derive_summary_records.R
@@ -199,6 +199,7 @@ derive_summary_records <- function(dataset,
   # Summarise the analysis value and bind to the original dataset
   bind_rows(
     dataset,
+    subset_ds %>%
       group_by(!!!by_vars) %>%
       summarise(!!analysis_var := summary_fun(!!analysis_var)) %>%
       mutate(!!!set_values_to)
