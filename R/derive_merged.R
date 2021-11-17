a28937ba8788e0f94b8cff3c380c64ef52103f5d
Bundfuss, Stefan {MDBB~Basel} <stefan.bundfuss@roche.com>
2021-11-17 17:08:51 +0100

    607_merge_experiment: start of the experiment

---
+++ R/derive_merged.R
@@ -0,0 +1,316 @@
+#' Merge Variables
+#'
+#' @export
+#'
+#' @examples
+#' library(admiral.test)
+#' data("vs")
+#' data("dm")
+#'
+#' derive_vars_merged(vs,
+#'                    dataset_add = select(dm, -DOMAIN),
+#'                    by_vars = vars(STUDYID, USUBJID))
+derive_vars_merged <- function(dataset,
+                               dataset_add,
+                               by_vars,
+                               order = NULL,
+                               new_vars = NULL,
+                               mode = NULL,
+                               filter_add = NULL,
+                               check_type = "warning") {
+  filter_add <- assert_filter_cond(enquo(filter_add), optional = TRUE)
+  assert_vars(by_vars)
+
+  add_data <- filter_if(dataset_add, filter_add)
+  if (!is.null(order)) {
+    add_data <- filter_extreme(add_data,
+                               by_vars = by_vars,
+                               order = order,
+                               mode = mode,
+                               check_type = check_type)
+  }
+  if (!is.null(new_vars)) {
+    add_data <- select(add_data, !!!by_vars, !!!new_vars)
+  }
+  left_join(dataset, add_data, by = vars2chr(by_vars))
+}
+
+#' Merge a (imputed) Date Variable
+#'
+#' @export
+#'
+#' @examples
+#' library(admiral.test)
+#' data("dm")
+#' data("ex")
+#' derive_vars_merged_dt(
+#'   select(dm, STUDYID, USUBJID),
+#'   dataset_add = ex,
+#'   by_vars = vars(STUDYID, USUBJID),
+#'   new_vars_prefix = "TRTS",
+#'   date = EXSTDTC,
+#'   date_imputation = "first",
+#'   order = vars(TRTSDT),
+#'   mode = "first"
+#' )
+#'
+#' derive_vars_merged_dt(
+#'   select(dm, STUDYID, USUBJID),
+#'   dataset_add = ex,
+#'   by_vars = vars(STUDYID, USUBJID),
+#'   new_vars_prefix = "TRTE",
+#'   date = EXENDTC,
+#'   date_imputation = "last",
+#'   order = vars(TRTEDT),
+#'   mode = "last"
+#' )
+derive_vars_merged_dt <- function(dataset,
+                                  dataset_add,
+                                  by_vars,
+                                  order,
+                                  new_vars_prefix,
+                                  filter_add,
+                                  mode,
+                                  date,
+                                  date_imputation,
+                                  flag_imputation = TRUE,
+                                  min_dates = NULL,
+                                  max_dates = NULL) {
+  date <- assert_symbol(enquo(date))
+  old_vars <- names(dataset_add)
+  add_data <- derive_vars_dt(
+    dataset_add,
+    new_vars_prefix = new_vars_prefix,
+    dtc = !!date,
+    date_imputation = date_imputation,
+    flag_imputation = flag_imputation,
+    min_dates = min_dates,
+    max_dates = max_dates
+  )
+  new_vars = lapply(setdiff(names(add_data), old_vars), sym)
+  derive_vars_merged(dataset,
+                     dataset_add = add_data,
+                     by_vars = by_vars,
+                     order = order,
+                     new_vars = new_vars,
+                     mode = mode)
+}
+
+#' Merge a (imputed) Datetime Variable
+#'
+#' @export
+#'
+#' @examples
+#' library(admiral.test)
+#' data("dm")
+#' data("ex")
+#' derive_vars_merged_dtm(
+#'   select(dm, STUDYID, USUBJID),
+#'   dataset_add = ex,
+#'   by_vars = vars(STUDYID, USUBJID),
+#'   new_vars_prefix = "TRTS",
+#'   date = EXSTDTC,
+#'   date_imputation = "first",
+#'   time_imputation = "first",
+#'   order = vars(TRTSDTM),
+#'   mode = "first"
+#' )
+#'
+#' derive_vars_merged_dtm(
+#'   select(dm, STUDYID, USUBJID),
+#'   dataset_add = ex,
+#'   by_vars = vars(STUDYID, USUBJID),
+#'   new_vars_prefix = "TRTE",
+#'   date = EXENDTC,
+#'   date_imputation = "last",
+#'   time_imputation = "last",
+#'   order = vars(TRTEDTM),
+#'   mode = "last"
+#' )
+
+derive_vars_merged_dtm <- function(dataset,
+                                  dataset_add,
+                                  by_vars,
+                                  order,
+                                  new_vars_prefix,
+                                  filter_add,
+                                  mode,
+                                  date,
+                                  date_imputation,
+                                  time_imputation,
+                                  flag_imputation = "auto",
+                                  min_dates = NULL,
+                                  max_dates = NULL) {
+  date <- assert_symbol(enquo(date))
+  old_vars <- names(dataset_add)
+  add_data <- derive_vars_dtm(
+    dataset_add,
+    new_vars_prefix = new_vars_prefix,
+    dtc = !!date,
+    date_imputation = date_imputation,
+    time_imputation = time_imputation,
+    flag_imputation = flag_imputation,
+    min_dates = min_dates,
+    max_dates = max_dates
+  )
+  new_vars = lapply(setdiff(names(add_data), old_vars), sym)
+  derive_vars_merged(dataset,
+                     dataset_add = add_data,
+                     by_vars = by_vars,
+                     order = order,
+                     new_vars = new_vars,
+                     mode = mode)
+}
+
+#' Merge a Categorized Variable
+#'
+#' @export
+#'
+#' @examples
+#' library(admiral.test)
+#' data("dm")
+#' data("vs")
+#' wgt_cat <- function(wgt) {
+#'   case_when(wgt < 50 ~ "low",
+#'             wgt > 90 ~ "high",
+#'             TRUE ~ "normal")
+#' }
+#'
+#' derive_var_merged_cat(
+#'   dm,
+#'   dataset_add = vs,
+#'   by_vars = vars(STUDYID, USUBJID),
+#'   order = vars(VSDTC, VSSEQ),
+#'   filter_add = VSTESTCD == "WEIGHT" & substr(VISIT, 1, 9) == "SCREENING" ,
+#'   new_var = WGTBLCAT,
+#'   source_var = VSSTRESN,
+#'   cat_fun = wgt_cat,
+#'   mode = "last"
+#' )
+derive_var_merged_cat <- function(dataset,
+                                  dataset_add,
+                                  by_vars,
+                                  order,
+                                  new_var,
+                                  source_var,
+                                  cat_fun,
+                                  filter_add = NULL,
+                                  mode) {
+  new_var <- assert_symbol(enquo(new_var))
+  source_var <- assert_symbol(enquo(source_var))
+  filter_add <- assert_filter_cond(enquo(filter_add), optional = TRUE)
+
+  add_data <- mutate(dataset_add,
+                     !!new_var := cat_fun(!!source_var))
+  derive_vars_merged(dataset,
+                     dataset_add = add_data,
+                     by_vars = by_vars,
+                     order = order,
+                     new_vars = vars(!!new_var),
+                     filter_add = !!filter_add,
+                     mode = mode)
+}
+
+#' Merge a Existence Flag
+#'
+#' @export
+#'
+#' @examples
+#'
+#' library(admiral.test)
+#' data("dm")
+#' data("ae")
+#' derive_var_merged_exist_flag(
+#'   dm,
+#'   dataset_add = ae,
+#'   by_vars = vars(STUDYID, USUBJID),
+#'   new_var = AESERFL,
+#'   condition = AESER == "Y"
+#' )
+derive_var_merged_exist_flag <- function(
+  dataset,
+  dataset_add,
+  by_vars,
+  new_var,
+  condition,
+  true_value = 'Y',
+  false_value = NA_character_,
+  filter_add
+) {
+  new_var <- assert_symbol(enquo(new_var))
+  condition <- assert_filter_cond(enquo(condition), optional = TRUE)
+
+  add_data <- mutate(dataset_add, cond_flag := if_else(!!condition, 1, 0, 0))
+
+  derive_vars_merged(
+    dataset,
+    dataset_add = add_data,
+    by_vars = by_vars,
+    new_vars = vars(cond_flag),
+    order = vars(cond_flag),
+    check_type = "none",
+    mode = "last"
+  ) %>%
+    mutate(!!new_var := if_else(cond_flag == 1, true_value, false_value, false_value)) %>%
+    select(-cond_flag)
+}
+
+#' Merge a Character Variable
+#'
+#' @export
+#'
+#' @examples
+#' library(admiral.test)
+#' data("dm")
+#' data("ds")
+#' derive_var_merged_character(
+#'   dm,
+#'   dataset_add = ds,
+#'   by_vars = vars(STUDYID, USUBJID),
+#'   new_var = DISPSTAT,
+#'   filter_add = DSCAT == "DISPOSITION EVENT",
+#'   source_var = DSDECOD,
+#'   case = "title"
+#' )
+derive_var_merged_character <- function(dataset,
+                                        dataset_add,
+                                        by_vars,
+                                        order = NULL,
+                                        new_var,
+                                        source_var,
+                                        case = NULL,
+                                        filter_add = NULL,
+                                        mode = NULL) {
+
+  new_var <- assert_symbol(enquo(new_var))
+  source_var <- assert_symbol(enquo(source_var))
+  case <-
+    assert_character_scalar(
+      case,
+      values = c("lower", "upper", "title"),
+      case_sensitive = FALSE,
+      optional = TRUE
+    )
+  filter_add <- assert_filter_cond(enquo(filter_add), optional = TRUE)
+
+  if (is.null(case)) {
+    trans <- expr(!!source_var)
+  }
+  else if (case == "lower") {
+    trans <- expr(stringr::str_to_lower(!!source_var))
+  }
+  else if (case == "upper") {
+    trans <- expr(str_to_upper(!!source_var))
+  }
+  else if (case == "title") {
+    trans <- expr(stringr::str_to_title(!!source_var))
+  }
+  add_data <- mutate(dataset_add, !!new_var := !!trans)
+  derive_vars_merged(dataset,
+                     dataset_add = add_data,
+                     by_vars = by_vars,
+                     order = order,
+                     new_vars = vars(!!new_var),
+                     filter_add = !!filter_add,
+                     mode = mode)
+}
