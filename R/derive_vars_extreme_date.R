b1eea698d376c2fa8b294807b8a2b31c40fd6700
teckla-gsk <82049812+teckla-gsk@users.noreply.github.com>
2021-12-07 16:55:30 -0500

    updated based on comments from review

--- R/derive_vars_extreme_date.R
+++ R/derive_vars_extreme_date.R
@@ -1,8 +1,7 @@
 #' Derive Datetime of Any Extreme Dates
 #'
-#' Derives datetime of any extreme dates e.g. treatment start or end dates
-#'
-#' @param dataset Input dataset
+#' Derives datetime of any extreme dates
+#'     e.g. Treatment, Phase or Period start or end dates
 #'
 #' @param subject_keys Variables to uniquely identify a subject
 #'
@@ -29,17 +28,25 @@
 #'
 #' @param dtc name of variable from which the extreme date is derived e.g. `EXENDTC`
 #'
-#' @param new_var name of the extreme date derived e.g.treatment end date `TRTEDTM`
+#' @param  new_var_prefix name of the extreme date derived
+#'          e.g.Treatment start and end date `TRTST`, `TRT01ED`
+#'              Phase start and end date `AP01ST`, `AP01ED`
+#'
+#' @inheritParams convert_dtc_to_dtm
 #'
 #' @inheritParams impute_dtc
+#'
+#' @inheritParams filter_extreme
 #'
 #' @details For each group (with respect to the variables specified for the
 #'   `by_vars` parameter) the first observation (with respect to the order
 #'   specified for the `order` parameter) is included in the output dataset.
 #'
-#' @author Stefan Bundfuss, Teckla Akinyi
+#' @author Teckla Akinyi
 #'
-#' @return The input dataset with extreme date variable added e.g. `TRTEDTM`
+#' @return The input dataset with extreme datetime variables (and date or time imputation flags)
+#'  added e.g. Treatment start datetime and associated flags `TRTSTDTM`, `TRTSTDTF`, `TRTSTTMF`
+#'              Phase start datetime and associated flags `APO1STDTM`, `APO1STDTF`, `APO1STTMF`
 #'
 #' @export
 #'
@@ -56,7 +63,7 @@
 #'                           filter_dataset_date = EXDOSE>0,
 #'                           dtc = EXSTDTC,
 #'                           order = vars(EXSTDTC,EXSEQ),
-#'                           new_var=TRTEDTM,
+#'                           new_var_prefix=TRTEDTM,
 #'                           date_imputation = "first",
 #'                           time_imputation = "first",
 #'                           flag_imputation = "AUTO",
@@ -64,43 +71,203 @@
 #'                           max_dates = NULL,
 #'                           mode="LAST") %>%
 #'   select(USUBJID, TRTEDTM)
-derive_vars_extreme_date <- function(dataset,
-                                     subject_keys =vars(STUDYID,USUBJID),
+derive_vars_extreme_dtm <- function(dataset,
+                                     subject_keys =vars(STUDYID, USUBJID),
                                      dataset_date,
                                      filter_dataset_date = NULL,
-                                     dtc,
                                      order,
-                                     new_var,
-                                     date_imputation,
-                                     time_imputation,
+                                     new_var_prefix,
+                                     dtc,
+                                     date_imputation = NULL,
+                                     time_imputation = NULL,
                                      flag_imputation = "AUTO",
                                      min_dates = NULL,
                                      max_dates = NULL,
                                      mode) {
 
-  new_var <- assert_symbol(enquo(new_var))
   dtc <- assert_symbol(enquo(dtc))
   assert_data_frame(dataset, subject_keys)
   assert_data_frame(dataset_date, required_vars = quo_c(subject_keys, order))
   assert_character_scalar(mode, values = c("first", "last"), case_sensitive = F)
 
   filter_dataset_date <- assert_filter_cond(enquo(filter_dataset_date), optional = TRUE)
-  filter_dataset_date <- enquo(filter_dataset_date)
 
-  assert_character_scalar(flag_imputation, values = c("auto", "both", "date", "time", "none"),
-                          case_sensitive = FALSE)
+  flag_imputation <- assert_character_scalar(flag_imputation,
+                                             values = c("auto", "both", "date", "time", "none"),
+                                             case_sensitive = FALSE)
+  assert_character_scalar(new_var_prefix)
+  dtm <- paste0(new_var_prefix, "DTM")
 
-  #Issue warning that new_var should be DTM
-  if (str_detect(rlang::quo_name(new_var), "DTM", negate = T)) {
-    msg <- sprintf(
-      "This function derives the treatment datetime, new_var must end in DTM.")
-    warn(msg)
-  }
+  # Issue a warning if --DTM already exists
+  warn_if_vars_exist(dataset, dtm)
 
+  # use derive_vars_dtm
   add <- dataset_date %>%
-    mutate(imputed_dtc = convert_dtc_to_dtm(!!dtc,
+         mutate(imputed_dtc = convert_dtc_to_dtm(!!dtc,
                                             date_imputation = date_imputation,
                                             time_imputation = time_imputation,
+                                            min_dates = min_dates,
+                                            max_dates = max_dates)
+         )
+
+  #filter EX data based on study requirements
+  if (quo_is_null(filter_dataset_date)) {
+    add
+    msg <- sprintf(
+      "Input EX dataset has no been filtered. Check study requirement if ex needs to be filtered.")  # nolint
+    inform(msg)
+  } else {
+    add <- add %>% filter(!!filter_dataset_date)
+  }
+
+  add <- add %>%
+    filter_extreme(order = order,
+                   by_vars = subject_keys,
+                   mode = mode) %>%
+    mutate(!!sym(dtm) := imputed_dtc) %>%
+    select(!!!subject_keys, !!sym(dtm), !!dtc, imputed_dtc)
+
+  #add imputation flags
+  if (flag_imputation %in% c("both", "date") ||
+      flag_imputation == "auto" && !is.null(date_imputation)) {
+    # add --DTF if not there already
+    dtf <- paste0(new_var_prefix, "DTF")
+    dtf_exist <- dtf %in% colnames(add)
+    if (!dtf_exist) {
+      add <- add %>%
+        mutate(!!sym(dtf) := compute_dtf(dtc = !!dtc, dt = imputed_dtc))
+    } else {
+      msg <- sprintf(
+        "The %s variable is already present in the input dataset and will not be re-derived.",
+        dtf
+      )
+      inform(msg)
+    }
+  }
+
+  if (flag_imputation %in% c("both", "time") ||
+      flag_imputation == "auto" && !is.null(time_imputation)) {
+    # add --TMF variable
+    tmf <- paste0(new_var_prefix, "TMF")
+    warn_if_vars_exist(dataset, tmf)
+    add <- add %>%
+      mutate(!!sym(tmf) := compute_tmf(dtc = !!dtc, dtm = imputed_dtc))
+  }
+  add <- add %>% select(-!!dtc, -imputed_dtc)
+
+  left_join(dataset, add, by = vars2chr(subject_keys))
+}
+
+
+
+
+
+#' Derive Date of Any Extreme Dates
+#'
+#' Derives date of any extreme dates
+#'     e.g. Treatment, Phase or Period start or end dates
+#'
+#' @param subject_keys Variables to uniquely identify a subject
+#'
+#'   The variables specified by the `subject_keys` parameter are expected.
+#'
+#'   Default: vars(STUDYID,USUBJID)
+#'
+#' @param dataset_date dataset that contains the date values e.g. `ex`
+#'
+#'   The variables `--ENDTC`, `--SEQ`, and those specified by the `filter_dataset_date`,
+#'   `dtc` and `subject_keys` parameters are expected.
+#'
+#' @param filter_dataset_date Filter condition for the dataset with dates
+#'
+#'   Only observations of the dataset which fulfill the specified condition
+#'   are considered for the treatment start date.
+#'
+#'   Default: NULL
+#'
+#'   Permitted Values: logical expression
+#'
+#' A list of quosures where the expressions are symbols as returned by
+#' `vars()` is expected.
+#'
+#' @param dtc name of variable from which the extreme date is derived e.g. `EXENDTC`
+#'
+#' @param  new_var_prefix name of the extreme date derived
+#'          e.g.Treatment start and end date `TRTST`, `TRT01ED`
+#'              Phase start and end date `AP01ST`, `AP01ED`
+#'
+#' @inheritParams convert_dtc_to_dt
+#'
+#' @inheritParams impute_dtc
+#'
+#' @inheritParams filter_extreme
+#'
+#' @details For each group (with respect to the variables specified for the
+#'   `by_vars` parameter) the first observation (with respect to the order
+#'   specified for the `order` parameter) is included in the output dataset.
+#'
+#' @author Teckla Akinyi
+#'
+#' @return The input dataset with extreme datetime variables (and date imputation flags) added
+#'         e.g. Treatment start datetime and associated flags `TRTSTDT`, `TRTSTDTF`
+#'              Phase start datetime and associated flags `APO1STDTM`, `APO1STDTF`
+#'
+#' @export
+#'
+#' @keywords adsl timing derivation
+#'
+#' @examples
+#' library(dplyr, warn.conflicts = FALSE)
+#' data("ex")
+#' data("dm")
+#'
+#'dm %>%
+#'  derive_vars_extreme_date(subject_keys=vars(STUDYID,USUBJID),
+#'                           dataset_date = ex,
+#'                           filter_dataset_date = EXDOSE>0,
+#'                           dtc = EXSTDTC,
+#'                           order = vars(EXSTDTC,EXSEQ),
+#'                           new_var_prefix=TRTEDTM,
+#'                           date_imputation = "first",
+#'                           time_imputation = "first",
+#'                           flag_imputation = "AUTO",
+#'                           min_dates = NULL,
+#'                           max_dates = NULL,
+#'                           mode="LAST") %>%
+#'   select(USUBJID, TRTEDTM)
+derive_vars_extreme_dt <- function(dataset,
+                                    subject_keys =vars(STUDYID, USUBJID),
+                                    dataset_date,
+                                    filter_dataset_date = NULL,
+                                    order,
+                                    new_var_prefix,
+                                    dtc,
+                                    date_imputation = NULL,
+                                    flag_imputation = "AUTO",
+                                    min_dates = NULL,
+                                    max_dates = NULL,
+                                    mode) {
+
+  dtc <- assert_symbol(enquo(dtc))
+  assert_data_frame(dataset, subject_keys)
+  assert_data_frame(dataset_date, required_vars = quo_c(subject_keys, order))
+  assert_character_scalar(mode, values = c("first", "last"), case_sensitive = F)
+
+  filter_dataset_date <- assert_filter_cond(enquo(filter_dataset_date), optional = TRUE)
+
+  flag_imputation <- assert_character_scalar(flag_imputation,
+                                             values = c("auto", "both", "date", "time", "none"),
+                                             case_sensitive = FALSE)
+  assert_character_scalar(new_var_prefix)
+  dt <- paste0(new_var_prefix, "DT")
+
+  # Issue a warning if --DTM already exists
+  warn_if_vars_exist(dataset, dt)
+
+  # use derive_vars_dtm
+  add <- dataset_date %>%
+    mutate(imputed_dtc = convert_dtc_to_dt(!!dtc,
+                                            date_imputation = date_imputation,
                                             min_dates = min_dates,
                                             max_dates = max_dates)
     )
@@ -119,14 +286,14 @@ derive_vars_extreme_date <- function(dataset,
     filter_extreme(order = order,
                    by_vars = subject_keys,
                    mode = mode) %>%
-    mutate(!!new_var := imputed_dtc) %>%
-    select(!!!subject_keys, !!new_var, !!dtc, imputed_dtc)
+    mutate(!!sym(dt) := imputed_dtc) %>%
+    select(!!!subject_keys, !!sym(dt), !!dtc, imputed_dtc)
 
   #add imputation flags
   if (flag_imputation %in% c("both", "date") ||
       flag_imputation == "auto" && !is.null(date_imputation)) {
     # add --DTF if not there already
-    dtf <- paste0("TRT", "DTF")
+    dtf <- paste0(new_var_prefix, "DTF")
     dtf_exist <- dtf %in% colnames(add)
     if (!dtf_exist) {
       add <- add %>%
@@ -140,14 +307,6 @@ derive_vars_extreme_date <- function(dataset,
     }
   }
 
-  if (flag_imputation %in% c("both", "time") ||
-      flag_imputation == "auto" && !is.null(time_imputation)) {
-    # add --TMF variable
-    tmf <- paste0("TRT", "TMF")
-    warn_if_vars_exist(dataset, tmf)
-    add <- add %>%
-      mutate(!!sym(tmf) := compute_tmf(dtc = !!dtc, dtm = imputed_dtc))
-  }
   add <- add %>% select(-!!dtc, -imputed_dtc)
 
   left_join(dataset, add, by = vars2chr(subject_keys))
--- tests/testthat/test-derive_vars_extreme_date.R
+++ tests/testthat/test-derive_vars_extreme_date.R
@@ -1,7 +1,7 @@
 context("test-derive_vars_extreme_date")
 
-#test 1
-test_that("Test1::Adds treatment end date - no imputations
+#----test 1----
+test_that("Test1::Adds treatment end date - time imputations
           choosing the LAST ex date with filter of EXDOSE>0 applied", {
             input <- tibble::tribble(
               ~STUDYID, ~USUBJID,
@@ -21,20 +21,20 @@ test_that("Test1::Adds treatment end date - no imputations
 
             # nolint start
             expected_output <- tibble::tribble(
-              ~STUDYID, ~USUBJID, ~TRTEDTM,
-              "TEST01", "PAT01", "2014-01-17T00:00:00",
-              "TEST01", "PAT02", "2012-06-19T00:00:00"
+              ~STUDYID, ~USUBJID, ~TRTEDTM,~TRTETMF,
+              "TEST01", "PAT01", "2014-01-17T23:59:59", "H",
+              "TEST01", "PAT02", "2012-06-19T23:59:59",  "H"
             ) %>% mutate(
               TRTEDTM = as_iso_dtm(TRTEDTM),
             )
             # nolint end
 
-            actual_output <- derive_vars_extreme_date(input,
+            actual_output <- derive_vars_extreme_dtm(input,
                                                  input_ex,
-                                                 new_var=TRTEDTM,
+                                                 new_var_prefix = "TRTE",
                                                  dtc=EXENDTC,
-                                                 date_imputation = "first",
-                                                 time_imputation="first",
+                                                 date_imputation = NULL,
+                                                 time_imputation= "last",
                                                  min_dates = NULL,
                                                  max_dates = NULL,
                                                  order = vars(EXENDTC,EXSEQ),
@@ -43,18 +43,18 @@ test_that("Test1::Adds treatment end date - no imputations
                                                  filter_dataset_date = EXDOSE>0 ,
                                                  mode="LAST")
 
-
             expect_dfs_equal(
               expected_output,
               actual_output,
               keys = c("STUDYID", "USUBJID")
             )
-          })
+           })
 
-#test 2
-test_that("Test2::Adds treatment start date - no imputations
-          choosing the LAST ex date with no filter applied
-          -expect a warning thrown for no filter applied", {
+#----test 2----
+ test_that("Test2::Adds treatment start date - time imputations
+           choosing the LAST ex date with no filter applied
+           -expect a warning thrown for no filter applied", {
+
             input <- tibble::tribble(
               ~STUDYID, ~USUBJID,
               "TEST01", "PAT01",
@@ -73,27 +73,77 @@ test_that("Test2::Adds treatment start date - no imputations
 
             # nolint start
             expected_output <- tibble::tribble(
-              ~STUDYID, ~USUBJID, ~TRTEDTM,
-              "TEST01", "PAT01", "2014-06-19T00:00:00",
-              "TEST01", "PAT02", "2012-06-19T00:00:00"
+              ~STUDYID, ~USUBJID, ~TRTEDTM,~TRTETMF,
+              "TEST01", "PAT01", "2014-06-19T23:59:59", "H",
+              "TEST01", "PAT02", "2012-06-19T23:59:59",  "H"
             ) %>% mutate(
               TRTEDTM = as_iso_dtm(TRTEDTM),
             )
             # nolint end
 
-            actual_output <- derive_vars_extreme_date(input,
-                                                 input_ex,
-                                                 new_var=TRTEDTM,
-                                                 dtc=EXENDTC,
-                                                 date_imputation = "first",
-                                                 time_imputation="first",
-                                                 min_dates = NULL,
-                                                 max_dates = NULL,
-                                                 order = vars(EXENDTC,EXSEQ),
-                                                 subject_keys=vars(STUDYID,USUBJID),
-                                                 filter_dataset_date = NULL,
-                                                 flag_imputation = "AUTO",
-                                                 mode="last")
+            actual_output <- derive_vars_extreme_dtm(input,
+                                                     input_ex,
+                                                     new_var_prefix = "TRTE",
+                                                     dtc=EXENDTC,
+                                                     date_imputation = NULL,
+                                                     time_imputation= "last",
+                                                     min_dates = NULL,
+                                                     max_dates = NULL,
+                                                     order = vars(EXENDTC,EXSEQ),
+                                                     subject_keys=vars(STUDYID,USUBJID),
+                                                     flag_imputation = "AUTO",
+                                                     filter_dataset_date = NULL ,
+                                                     mode="LAST")
+
+            expect_dfs_equal(
+              expected_output,
+              actual_output,
+              keys = c("STUDYID", "USUBJID")
+            )
+          })
+
+#----test 3----
+ test_that("Test3::Adds phase start date - with date & time imputations
+          choosing the first ex date with filter of EXDOSE>0 applied", {
+             input <- tibble::tribble(
+               ~STUDYID, ~USUBJID,
+               "TEST01", "PAT01",
+              "TEST01", "PAT02"
+             )
+
+             input_ex <- tibble::tribble(
+               ~STUDYID, ~USUBJID, ~EXENDTC, ~EXSEQ, ~EXDOSE,
+               "TEST01", "PAT01", "2014-01", 1 , 0,
+               "TEST01", "PAT01", "2014-01-17", 2 , 38,
+               "TEST01", "PAT01", "2014-06"   , 3 , 0,
+               "TEST01", "PAT02", "2012-01-02", 1 , 54,
+               "TEST01", "PAT02", "2012-01-17", 2 , 81,
+               "TEST01", "PAT02", "2012-06"   , 3 , 54,
+             )
+
+             # nolint start
+             expected_output <- tibble::tribble(
+               ~STUDYID, ~USUBJID, ~APHO1STDTM , ~APHO1STDTF, ~APHO1STTMF,
+               "TEST01", "PAT01", "2014-01-31T23:59:59", "D", "H",
+               "TEST01", "PAT02", "2012-01-02T23:59:59", NA_character_, "H"
+             ) %>% mutate(
+               APHO1STDTM = as_iso_dtm(APHO1STDTM),
+            )
+             # nolint end
+
+             actual_output <- derive_vars_extreme_dtm(input,
+                                                      input_ex,
+                                                      new_var_prefix = "APHO1ST",
+                                                      dtc=EXENDTC,
+                                                      date_imputation = "last",
+                                                      time_imputation= "last",
+                                                      min_dates = NULL,
+                                                      max_dates = NULL,
+                                                      order = vars(EXENDTC,EXSEQ),
+                                                      subject_keys=vars(STUDYID,USUBJID),
+                                                      flag_imputation = "AUTO",
+                                                      filter_dataset_date = NULL ,
+                                                      mode="first")
 
 
             expect_dfs_equal(
@@ -103,11 +153,11 @@ test_that("Test2::Adds treatment start date - no imputations
             )
           })
 
-#test 3
-test_that("Test3::Adds treatment start date - no imputations
-          choosing the last ex date with filter of EXDOSE>0 applied,
-          but new_var not named --DTM,
-          expect warning thrown on name of new_var", {
+###################REPEAT ALL THE ABOVE FOR DERIVE_VARS_EXTREME_DT
+
+#----test 4----
+ test_that("Test4::Adds treatment end date - NO imputations
+          choosing the LAST ex date with filter of EXDOSE>0 applied", {
             input <- tibble::tribble(
               ~STUDYID, ~USUBJID,
               "TEST01", "PAT01",
@@ -127,27 +177,21 @@ test_that("Test3::Adds treatment start date - no imputations
             # nolint start
             expected_output <- tibble::tribble(
               ~STUDYID, ~USUBJID, ~TRTEDT,
-              "TEST01", "PAT01", "2014-01-17T00:00:00",
-              "TEST01", "PAT02", "2012-06-19T00:00:00"
+              "TEST01", "PAT01", "2014-01-17T23:59:59",
+              "TEST01", "PAT02", "2012-06-19T23:59:59",
             ) %>% mutate(
-              TRTEDT = as_iso_dtm(TRTEDT),
+              TRTEDT = as.Date(TRTEDT),
             )
             # nolint end
 
-            actual_output <- derive_vars_extreme_date(input,
-                                                 input_ex,
-                                                 new_var=TRTEDT,
-                                                 dtc=EXENDTC,
-                                                 date_imputation = "first",
-                                                 time_imputation="first",
-                                                 min_dates = NULL,
-                                                 max_dates = NULL,
-                                                 order = vars(EXENDTC,EXSEQ),
-                                                 subject_keys=vars(STUDYID,USUBJID),
-                                                 flag_imputation = "AUTO",
-                                                 filter_dataset_date = EXDOSE>0 ,
-                                                 mode="last")
-
+            actual_output <- derive_vars_extreme_dt(input,
+                                                     input_ex,
+                                                     new_var_prefix = "TRTE",
+                                                     dtc=EXENDTC,
+                                                     order = vars(EXENDTC,EXSEQ),
+                                                     subject_keys=vars(STUDYID,USUBJID),
+                                                     filter_dataset_date = EXDOSE>0 ,
+                                                     mode="LAST")
 
             expect_dfs_equal(
               expected_output,
@@ -156,47 +200,47 @@ test_that("Test3::Adds treatment start date - no imputations
             )
           })
 
-#test 4
-test_that("Test4::Adds treatment END date IMPUTING WHEN NECESSARY - and imputation flags
-          choosing the last ex date with filter of EXDOSE>0 applied", {
-            input_ex <- tibble::tribble(
-              ~STUDYID, ~USUBJID, ~EXENDTC, ~EXSEQ, ~EXDOSE,
-              "TEST01", "PAT01", "2014-01-02", 1 , 0,
-              "TEST01", "PAT01", "2014-01", 2 , 38,
-              "TEST01", "PAT01", "2014-06-02", 3 , 0,
-              "TEST01", "PAT02", "2012-01-02", 1 , 54,
-              "TEST01", "PAT02", "2012-01-17", 2 , 81,
-              "TEST01", "PAT02", "2012-06-19", 3 , 54,
-            )
+#----test 5----
+ test_that("Test5::Adds phase start date - with date imputations
+          choosing the first ex date with filter no filter applied", {
             input <- tibble::tribble(
               ~STUDYID, ~USUBJID,
               "TEST01", "PAT01",
               "TEST01", "PAT02"
             )
 
+            input_ex <- tibble::tribble(
+              ~STUDYID, ~USUBJID, ~EXENDTC, ~EXSEQ, ~EXDOSE,
+              "TEST01", "PAT01", "2014-01", 1 , 0,
+              "TEST01", "PAT01", "2014-01-17", 2 , 38,
+              "TEST01", "PAT01", "2014-06"   , 3 , 0,
+              "TEST01", "PAT02", "2012-01-02", 1 , 54,
+              "TEST01", "PAT02", "2012-01-17", 2 , 81,
+              "TEST01", "PAT02", "2012-06"   , 3 , 54,
+            )
+
             # nolint start
             expected_output <- tibble::tribble(
-              ~STUDYID, ~USUBJID, ~TRTEDTM, ~TRTDTF,
-              "TEST01", "PAT01", "2014-01-01T00:00:00", "D",
-              "TEST01", "PAT02", "2012-06-19T00:00:00", NA_character_
+              ~STUDYID, ~USUBJID, ~APHO1STDT, ~APHO1STDTF,
+              "TEST01", "PAT01", "2014-01-31T23:59:59", "D",
+              "TEST01", "PAT02", "2012-01-02T23:59:59", NA_character_
             ) %>% mutate(
-              TRTEDTM = as_iso_dtm(TRTEDTM),
+              APHO1STDT = as.Date(APHO1STDT)
             )
             # nolint end
 
-            actual_output <- derive_vars_extreme_date(input,
-                                                 input_ex,
-                                                 new_var=TRTEDTM,
-                                                 dtc=EXENDTC,
-                                                 date_imputation = "first",
-                                                 time_imputation="first",
-                                                 min_dates = NULL,
-                                                 max_dates = NULL,
-                                                 order = vars(EXENDTC,EXSEQ),
-                                                 subject_keys=vars(STUDYID,USUBJID),
-                                                 flag_imputation = "date",
-                                                 filter_dataset_date = EXDOSE>0 ,
-                                                 mode="LAST")
+            actual_output <- derive_vars_extreme_dt(input,
+                                                     input_ex,
+                                                     new_var_prefix = "APHO1ST",
+                                                     dtc=EXENDTC,
+                                                     date_imputation = "last",
+                                                     min_dates = NULL,
+                                                     max_dates = NULL,
+                                                     order = vars(EXENDTC,EXSEQ),
+                                                     subject_keys=vars(STUDYID,USUBJID),
+                                                     flag_imputation = "AUTO",
+                                                     filter_dataset_date = NULL ,
+                                                     mode="first")
 
 
             expect_dfs_equal(
