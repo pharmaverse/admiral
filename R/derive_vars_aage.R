113a29e4837d054fedf39664956c25ddd7349988
Tom Ratford <tomratford@gmail.com>
2022-01-31 17:38:50 +0000

    removed irrelevant link and changed ranges

--- R/derive_vars_aage.R
+++ R/derive_vars_aage.R
@@ -316,9 +316,7 @@ NULL
 
 #' @rdname derive_var_agegr_fda
 #' @export
-#' @details `derive_var_agegr_fda` Derive age groups according to FDA
-#' (\url{https://prsinfo.clinicaltrials.gov/results_definitions.html} ->
-#' Baseline Measure Information).
+#' @details `derive_var_agegr_fda` Derive age groups according to FDA.
 #' @examples
 #' library(dplyr, warn.conflicts = FALSE)
 #' library(admiral.test)
@@ -352,8 +350,8 @@ derive_var_agegr_fda <- function(dataset, age_var, age_unit = NULL, new_var) {
     mutate(
       !!new_var := cut(
         x = temp_age,
-        breaks = c(0, 19, 65, Inf),
-        labels = c("<=18", "19-64", ">=65"),
+        breaks = c(0, 18, 65, Inf),
+        labels = c("<18", "18-64", ">=65"),
         include.lowest = TRUE,
         right = FALSE
       )
