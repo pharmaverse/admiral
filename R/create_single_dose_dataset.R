f1a6adbfdf22b1019b354a80d25a005a898fade7
Andrew Smith <andrew.m.smith@gsk.com>
2022-02-24 10:38:51 -0500

    moving to testing

--- R/create_single_dose_dataset.R
+++ R/create_single_dose_dataset.R
@@ -18,53 +18,63 @@
 #' @rdname dose_freq_lookup
 #'
 dose_freq_lookup <- tibble::tribble(
-~NCICode, ~CDISCSubmissionValue
-"C64526","1 TIME PER WEEK",
-"C139179","10 DAYS PER MONTH",
-"C176288","2 TIMES PER CYCLE",
-"C64497","2 TIMES PER WEEK",
-"C98861","2 TIMES PER YEAR",
-"C176289","3 TIMES PER CYCLE",
-"C98859","3 TIMES PER MONTH",
-"C64528","3 TIMES PER WEEK",
-"C98860","3 TIMES PER YEAR",
-"C98852","4 TIMES PER MONTH",
-"C64531","4 TIMES PER WEEK",
-"C98853","4 TIMES PER YEAR",
-"C98849","5 TIMES PER DAY",
-"C98850","5 TIMES PER MONTH",
-"C85552","5 TIMES PER WEEK",
-"C98851","5 TIMES PER YEAR",
-"C98855","6 TIMES PER DAY",
-"C98856","6 TIMES PER MONTH",
-"C98857","6 TIMES PER WEEK",
-"C98858","6 TIMES PER YEAR",
-"C139180","7 TIMES PER DAY",
-"C98854","7 TIMES PER WEEK",
-"C139181","8 TIMES PER DAY",
-"C139182","9 TIMES PER DAY",
-"C64496","BID",
-"C71129","BIM",
-"C161332","EVERY 12 WEEKS",
-"C161336","EVERY 16 WEEKS",
-"C71127","EVERY 2 WEEKS",
-"C64535","EVERY 3 WEEKS",
-"C161333","EVERY 3 YEARS",
-"C64529","EVERY 4 WEEKS",
-"C103390","EVERY 5 WEEKS",
-"C161334","EVERY 5 YEARS",
-"C89788","EVERY 6 WEEKS"
-) %>%
+  ~NCICode, ~CDISCSubmissionValue,
+  "C64526","1 TIME PER WEEK",
+  "C139179","10 DAYS PER MONTH",
+#  "C176288","2 TIMES PER CYCLE",
+  "C64497","2 TIMES PER WEEK",
+  "C98861","2 TIMES PER YEAR",
+#  "C176289","3 TIMES PER CYCLE",
+  "C98859","3 TIMES PER MONTH",
+  "C64528","3 TIMES PER WEEK",
+  "C98860","3 TIMES PER YEAR",
+  "C98852","4 TIMES PER MONTH",
+  "C64531","4 TIMES PER WEEK",
+  "C98853","4 TIMES PER YEAR",
+  "C98849","5 TIMES PER DAY",
+  "C98850","5 TIMES PER MONTH",
+  "C85552","5 TIMES PER WEEK",
+  "C98851","5 TIMES PER YEAR",
+  "C98855","6 TIMES PER DAY",
+  "C98856","6 TIMES PER MONTH",
+  "C98857","6 TIMES PER WEEK",
+  "C98858","6 TIMES PER YEAR",
+  "C139180","7 TIMES PER DAY",
+  "C98854","7 TIMES PER WEEK",
+  "C139181","8 TIMES PER DAY",
+  "C139182","9 TIMES PER DAY",
+  "C64496","BID",
+  "C71129","BIM",
+  "C161332","EVERY 12 WEEKS",
+  "C161336","EVERY 16 WEEKS",
+  "C71127","EVERY 2 WEEKS",
+  "C64535","EVERY 3 WEEKS",
+  "C161333","EVERY 3 YEARS",
+  "C64529","EVERY 4 WEEKS",
+  "C103390","EVERY 5 WEEKS",
+  "C161334","EVERY 5 YEARS",
+  "C89788","EVERY 6 WEEKS"
+  ) %>%
 mutate(DoseCount = case_when(
-  str_detect(CDISCSubmissionValue, "PER") ~ as.numeric(str_remove_all(CDISCSubmissionValue,"[\\D]")),
-  str_detect(CDISCSubmissionValue, "EVERY") ~ 1/as.numeric(str_remove_all(CDISCSubmissionValue,"[\\D]")),
-  str_detect(CDISCSubmissionValue, "BID|BIM") ~ 2),
+    str_detect(CDISCSubmissionValue, "PER") ~
+      as.numeric(str_remove_all(CDISCSubmissionValue,"[\\D]")),
+    str_detect(CDISCSubmissionValue, "EVERY") ~
+      1/as.numeric(str_remove_all(CDISCSubmissionValue,"[\\D]")),
+    str_detect(CDISCSubmissionValue, "BID|BIM") ~ 2),
   DoseWindow = case_when(
-    str_detect(CDISCSubmissionValue, "EVERY|PER") ~ str_remove_all(sub(".* (\\w+)$", "\\1", x),"S"),
-    CDISCSubmissionValue=="BID" ~ str_remove_all(sub(".* (\\w+)$", "\\1", x),"S"),
-
+    str_detect(CDISCSubmissionValue, "EVERY|PER") ~
+      str_remove_all(sub(".* (\\w+)$", "\\1", CDISCSubmissionValue),"S"),
+    CDISCSubmissionValue == "BID" ~ "DAY",
+    CDISCSubmissionValue == "BIM" ~ "MONTH",
+  )) %>%
+mutate(
+  DayConversionFactor = case_when(
+    DoseWindow == "DAY" ~ 1,
+    DoseWindow == "WEEK" ~ (1/7),
+    DoseWindow == "MONTH" ~ (1/30.4375),
+    DoseWindow == "YEAR" ~ (1/365.25),
   )
-       )
+)
 
 #' Create dataset of single doses
 #'
@@ -121,10 +131,10 @@ mutate(DoseCount = case_when(
 #'
 #' create_single_dose_dataset(data, by_vars = vars(USUBJID))
 create_single_dose_dataset <- function(dataset,
-                                    by_vars,
-                                    dose_freq = EXDOSFRQ,
-                                    start_date = ASTDT,
-                                    end_date = AENDT) {
+                                       by_vars,
+                                       dose_freq = EXDOSFRQ,
+                                       start_date = ASTDT,
+                                       end_date = AENDT) {
 
   col_names <- colnames(dataset)
   by_vars <- assert_vars(by_vars)
@@ -138,31 +148,27 @@ create_single_dose_dataset <- function(dataset,
   dataset <- dataset %>%
     left_join(lookup, by = as.character(dose_freq)[2]) %>%
     mutate(dose_windows = case_when(
-      DFUnit == "Day" ~ compute_duration(!!start_date, !!end_date, out_unit = "days"),
-      DFUnit == "Week" ~ compute_duration(!!start_date, !!end_date, out_unit = "weeks"),
-      DFUnit == "Month" ~ compute_duration(!!start_date, !!end_date, out_unit = "months"),
-      DFUnit == "Year" ~ compute_duration(!!start_date, !!end_date, out_unit = "years")
+      DoseWindow == "DAY" ~ compute_duration(!!start_date, !!end_date, out_unit = "days"),
+      DoseWindow == "WEEK" ~ compute_duration(!!start_date, !!end_date, out_unit = "weeks"),
+      DoseWindow == "MONTH" ~ compute_duration(!!start_date, !!end_date, out_unit = "months"),
+      DoseWindow == "YEAR" ~ compute_duration(!!start_date, !!end_date, out_unit = "years")
     )) %>%
-    mutate(dose_count = floor(dose_windows*DFFactor) + (floor(dose_windows*DFFactor)==0))
+    mutate(dose_count = floor(dose_windows*DoseCount) + (floor(dose_windows*DoseCount)==0))
 
 
   dataset <- dataset[rep(row.names(dataset), dataset$dose_count), ]
 
   dataset <- dataset %>%
     group_by(!!!by_vars, !!dose_freq, !!start_date, !!end_date) %>%
-    mutate(temp_dose_multiplier = (row_number() - 1)/DFFactor) %>%
+    mutate(time_increment = (row_number() - 1)/(DoseCount*(DayConversionFactor))) %>%
     ungroup() %>%
-    mutate(time_differential = case_when(
-      DFUnit == "Day" ~ days(temp_dose_multiplier),
-      DFUnit == "Week" ~ weeks(temp_dose_multiplier),
-      DFUnit == "Month" ~ months(temp_dose_multiplier),
-      DFUnit == "Year" ~ years(temp_dose_multiplier)
-      ))
+    mutate(day_differential = days(floor(time_increment)))
+
 
   dataset <- dataset %>%
     mutate(!!dose_freq := "ONCE",
-           !!end_date := !!start_date + time_differential,
-           !!start_date := !!start_date + time_differential
+           !!end_date := !!start_date + day_differential,
+           !!start_date := !!start_date + day_differential
     ) %>%
     select(all_of(!!!vars(col_names)))
   return(dataset)
