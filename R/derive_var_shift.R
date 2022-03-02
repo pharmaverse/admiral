387ce463d8beeb771ec13c9a28aa51a537b1b1b2
Yang, Annie {MDBA~South San Francisco} <yang.chenyue@gene.com>
2022-03-02 06:54:06 +0100

    Rename post baseline parameter to `filter_post_baseline`. Add `na_val` and `sep_val`.

--- R/derive_var_shift.R
+++ R/derive_var_shift.R
@@ -6,7 +6,7 @@
 #'
 #' @param dataset Input dataset
 #'
-#'   The columns specified by `from_var` and the `to_var`parameters are expected.
+#'   The columns specified by `from_var` and the `to_var` parameters are expected.
 #'
 #' @param new_var Name of the character shift variable to create.
 #'
@@ -14,15 +14,23 @@
 #'
 #' @param to_var Variable containing value to shift to.
 #'
-#' @param post_baseline_condition Condition to identify post-baseline records. If not
+#' @param na_val Character string to replace missing values in `from_var` or `to_var`.
+#'
+#'  Default: "NULL"
+#'
+#' @param sep_val Character string to concatenate values of `from_var` and `to_var`.
+#'
+#'  Default: " to "
+#'
+#' @param filter_post_baseline Condition to identify post-baseline records. If not
 #' specified, `new_var` is populated for all records.
 #'
 #'  Default: `NULL`
 #'
 #' @details `new_var` is derived by concatenating the values of `from_var` to values of `to_var`
 #' (e.g. "NORMAL to HIGH"). When `from_var` or `to_var` has missing value, the
-#' missing value is replaced by `NULL` (e.g. "NORMAL to NULL"). If `post_baseline_condition` is
-#' specified, `new_var` is populated only for post-baseline records. If `post_baseline_condition`
+#' missing value is replaced by `na_val` (e.g. "NORMAL to NULL"). If `filter_post_baseline` is
+#' specified, `new_var` is populated only for post-baseline records. If `filter_post_baseline`
 #' is `NULL` (default), `new_var` is populated for all records.
 #'
 #'
@@ -36,23 +44,24 @@
 #'
 #'
 #' @examples
+#' library(dplyr, warn.conflicts = FALSE)
+#'
 #' data <- tibble::tribble(
-#' ~USUBJID, ~PARAMCD, ~AVAL, ~ABLFL, ~BNRIND, ~ANRIND,
-#' "P01",    "ALB",     33,    "Y",    "LOW",   "LOW",
-#' "P01",    "ALB",     38,    "",     "LOW",   "NORMAL",
-#' "P01",    "ALB",     NA,    "",     "LOW",   "",
-#' "P02",    "ALB",     37,    "Y",    "NORMAL","NORMAL",
-#' "P02",    "ALB",     49,    "",     "NORMAL","HIGH",
-#' "P02",    "SODIUM",  147,   "Y",    "HIGH",  "HIGH"
+#'   ~USUBJID, ~PARAMCD, ~AVAL, ~ABLFL, ~BNRIND, ~ANRIND,
+#'   "P01",    "ALB",     33,    "Y",    "LOW",   "LOW",
+#'   "P01",    "ALB",     38,    NA,     "LOW",   "NORMAL",
+#'   "P01",    "ALB",     NA,    NA,     "LOW",   NA,
+#'   "P02",    "ALB",     37,    "Y",    "NORMAL","NORMAL",
+#'   "P02",    "ALB",     49,    NA,     "NORMAL","HIGH",
+#'   "P02",    "SODIUM",  147,   "Y",    "HIGH",  "HIGH"
 #' )
 #'
-#' library(dplyr, warn.conflicts = FALSE)
 #' data %>%
 #'   convert_blanks_to_na() %>%
 #'   derive_var_shift(
-#'    new_var = SHIFT1,
-#'    from_var = BNRIND,
-#'    to_var = ANRIND
+#'     new_var = SHIFT1,
+#'     from_var = BNRIND,
+#'     to_var = ANRIND
 #'   )
 #'
 #' # or only populate post-baseline records
@@ -62,46 +71,41 @@
 #'     new_var = SHIFT1,
 #'     from_var = BNRIND,
 #'     to_var = ANRIND,
-#'     post_baseline_condition = (ABLFL != "Y")
+#'     filter_post_baseline = ABLFL != "Y"
 #'   )
 #'
 derive_var_shift <- function(dataset,
                              new_var,
                              from_var,
                              to_var,
-                             post_baseline_condition = NULL) {
-
+                             na_val = "NULL",
+                             sep_val = " to ",
+                             filter_post_baseline = NULL) {
   new_var <- assert_symbol(enquo(new_var))
   from_var <- assert_symbol(enquo(from_var))
   to_var <- assert_symbol(enquo(to_var))
+  na_val <- assert_character_scalar(na_val)
+  sep_val <- assert_character_scalar(sep_val)
   assert_data_frame(dataset, required_vars = vars(!!from_var, !!to_var))
-  post_baseline_condition <- assert_filter_cond(enquo(post_baseline_condition), optional = TRUE)
+  filter_post_baseline <- assert_filter_cond(enquo(filter_post_baseline), optional = TRUE)
 
-  # Derive shift variable. If from_var or to_var has missing value then set to "NULL".
+  # Derive shift variable. If from_var or to_var has missing value then set to na_val.
   dataset <- dataset %>%
     mutate(
-      temp_from_var = case_when(
-        is.na(!!from_var) ~ "NULL",
-        TRUE ~ as.character(!!from_var)
-      ),
-      temp_to_var = case_when(
-        is.na(!!to_var) ~ "NULL",
-        TRUE ~ as.character(!!to_var)
-      )
+      temp_from_var = if_else(is.na(!!from_var), !!na_val, as.character(!!from_var)),
+      temp_to_var = if_else(is.na(!!to_var), !!na_val, as.character(!!to_var))
     ) %>%
     mutate(
-      !!new_var := paste(temp_from_var, temp_to_var, sep = " to ")
+      !!new_var := paste(temp_from_var, temp_to_var, sep = !!sep_val)
     ) %>%
     select(-starts_with("temp_"))
 
   # If post-baseline condition specified, then only populate shift for post-baseline records.
-  if (!is.null(quo_get_expr(post_baseline_condition))) {
+  if (!quo_is_null(filter_post_baseline)) {
     dataset <- dataset  %>%
       mutate(
-        !!new_var := case_when(
-          !eval(quo_get_expr(post_baseline_condition)) ~ NA_character_,
-          TRUE ~ !!new_var
-        )
+        !!new_var := if_else(!!filter_post_baseline, !!new_var, NA_character_,
+                             missing = !!new_var)
       )
   }
 
--- tests/testthat/test-derive_var_shift.R
+++ tests/testthat/test-derive_var_shift.R
@@ -2,17 +2,17 @@ test_that("Shift based on character variables", {
   input <- tibble::tribble(
     ~USUBJID, ~PARAMCD, ~AVAL, ~ABLFL, ~BNRIND, ~ANRIND,
     "P01",    "ALB",     33,    "Y",    "LOW",   "LOW",
-    "P01",    "ALB",     38,    "",     "LOW",   "NORMAL",
+    "P01",    "ALB",     38,    NA,     "LOW",   "NORMAL",
     "P02",    "ALB",     37,    "Y",    "NORMAL", "NORMAL",
-    "P02",    "ALB",     49,    "",     "NORMAL", "HIGH",
+    "P02",    "ALB",     49,    NA,     "NORMAL", "HIGH",
     "P02",    "SODIUM",  147,   "Y",    "HIGH",  "HIGH"
   )
   expected_output <- tibble::tribble(
     ~USUBJID, ~PARAMCD, ~AVAL, ~ABLFL, ~BNRIND, ~ANRIND, ~SHIFT1,
     "P01",    "ALB",     33,    "Y",    "LOW",   "LOW",   "LOW to LOW",
-    "P01",    "ALB",     38,    "",     "LOW",   "NORMAL", "LOW to NORMAL",
+    "P01",    "ALB",     38,    NA,     "LOW",   "NORMAL", "LOW to NORMAL",
     "P02",    "ALB",     37,    "Y",    "NORMAL", "NORMAL", "NORMAL to NORMAL",
-    "P02",    "ALB",     49,    "",     "NORMAL", "HIGH",  "NORMAL to HIGH",
+    "P02",    "ALB",     49,    NA,     "NORMAL", "HIGH",  "NORMAL to HIGH",
     "P02",    "SODIUM",  147,   "Y",    "HIGH",  "HIGH",  "HIGH to HIGH"
   )
 
@@ -24,7 +24,7 @@ test_that("Shift based on character variables", {
       to_var = ANRIND
     ),
     expected_output
-    )
+  )
 })
 
 
@@ -32,21 +32,21 @@ test_that("Shift based on character variables with missing values", {
   input <- tibble::tribble(
     ~USUBJID, ~PARAMCD, ~AVAL, ~ABLFL, ~BNRIND, ~ANRIND,
     "P01",    "ALB",     33,    "Y",    "LOW",   "LOW",
-    "P01",    "ALB",     38,    "",     "LOW",   "NORMAL",
-    "P01",    "ALB",     NA,    "",     "LOW",   "",
-    "P02",    "ALB",     NA,    "Y",    "",      "",
-    "P02",    "ALB",     49,    "",     "",      "HIGH",
+    "P01",    "ALB",     38,    NA,     "LOW",   "NORMAL",
+    "P01",    "ALB",     NA,    NA,     "LOW",   NA,
+    "P02",    "ALB",     NA,    "Y",    NA,      NA,
+    "P02",    "ALB",     49,    NA,     NA,      "HIGH",
     "P02",    "SODIUM",  147,   "Y",    "HIGH",  "HIGH"
-  ) %>% convert_blanks_to_na
+  )
   expected_output <- tibble::tribble(
     ~USUBJID, ~PARAMCD, ~AVAL, ~ABLFL, ~BNRIND, ~ANRIND, ~SHIFT1,
     "P01",    "ALB",     33,    "Y",    "LOW",   "LOW",   "LOW to LOW",
-    "P01",    "ALB",     38,    "",     "LOW",   "NORMAL", "LOW to NORMAL",
-    "P01",    "ALB",     NA,    "",     "LOW",   "",      "LOW to NULL",
-    "P02",    "ALB",     NA,    "Y",    "",      "",      "NULL to NULL",
-    "P02",    "ALB",     49,    "",     "",      "HIGH",  "NULL to HIGH",
+    "P01",    "ALB",     38,    NA,     "LOW",   "NORMAL", "LOW to NORMAL",
+    "P01",    "ALB",     NA,    NA,     "LOW",   NA,      "LOW to NULL",
+    "P02",    "ALB",     NA,    "Y",    NA,      NA,      "NULL to NULL",
+    "P02",    "ALB",     49,    NA,     NA,      "HIGH",  "NULL to HIGH",
     "P02",    "SODIUM",  147,   "Y",    "HIGH",  "HIGH",  "HIGH to HIGH"
-  ) %>% convert_blanks_to_na
+  )
 
   expect_equal(
     derive_var_shift(
@@ -64,21 +64,21 @@ test_that("Shift based on numeric variables with missing values", {
   input <- tibble::tribble(
     ~USUBJID, ~PARAMCD, ~AVAL, ~ABLFL, ~BASE,
     "P01",    "ALB",     33.1,  "Y",  33.1,
-    "P01",    "ALB",     38.5,  "",   33.1,
-    "P01",    "ALB",     NA,    "",   33.1,
+    "P01",    "ALB",     38.5,  NA,   33.1,
+    "P01",    "ALB",     NA,    NA,   33.1,
     "P02",    "ALB",     NA,    "Y",  NA,
-    "P02",    "ALB",     49.0,  "",   NA,
+    "P02",    "ALB",     49.0,  NA,   NA,
     "P02",    "SODIUM",  147.5, "Y",  147.5
   ) %>% convert_blanks_to_na
   expected_output <- tibble::tribble(
     ~USUBJID, ~PARAMCD, ~AVAL, ~ABLFL, ~BASE, ~SHIFT2,
     "P01",    "ALB",     33.1,  "Y",  33.1,    "33.1 to 33.1",
-    "P01",    "ALB",     38.5,  "",   33.1,    "33.1 to 38.5",
-    "P01",    "ALB",     NA,    "",   33.1,    "33.1 to NULL",
+    "P01",    "ALB",     38.5,  NA,   33.1,    "33.1 to 38.5",
+    "P01",    "ALB",     NA,    NA,   33.1,    "33.1 to NULL",
     "P02",    "ALB",     NA,    "Y",  NA,      "NULL to NULL",
-    "P02",    "ALB",     49,    "",   NA,      "NULL to 49",
+    "P02",    "ALB",     49,    NA,   NA,      "NULL to 49",
     "P02",    "SODIUM",  147.5, "Y",  147.5,   "147.5 to 147.5"
-  ) %>% convert_blanks_to_na
+  )
 
   expect_equal(
     derive_var_shift(
@@ -96,21 +96,21 @@ test_that("Only populate post-baseline", {
   input <- tibble::tribble(
     ~USUBJID, ~PARAMCD, ~AVAL, ~ABLFL, ~BASE,
     "P01",    "ALB",     33.1,  "Y",  33.1,
-    "P01",    "ALB",     38.5,  "",   33.1,
-    "P01",    "ALB",     NA,    "",   33.1,
+    "P01",    "ALB",     38.5,  NA,   33.1,
+    "P01",    "ALB",     NA,    NA,   33.1,
     "P02",    "ALB",     NA,    "Y",  NA,
-    "P02",    "ALB",     49.0,  "",   NA,
+    "P02",    "ALB",     49.0,  NA,   NA,
     "P02",    "SODIUM",  147.5, "Y",  147.5
-  ) %>% convert_blanks_to_na
+  )
   expected_output <- tibble::tribble(
     ~USUBJID, ~PARAMCD, ~AVAL, ~ABLFL, ~BASE, ~SHIFT2,
     "P01",    "ALB",     33.1,  "Y",  33.1,    NA_character_,
-    "P01",    "ALB",     38.5,  "",   33.1,    "33.1 to 38.5",
-    "P01",    "ALB",     NA,    "",   33.1,    "33.1 to NULL",
+    "P01",    "ALB",     38.5,  NA,   33.1,    "33.1 to 38.5",
+    "P01",    "ALB",     NA,    NA,   33.1,    "33.1 to NULL",
     "P02",    "ALB",     NA,    "Y",  NA,      NA_character_,
-    "P02",    "ALB",     49,    "",   NA,      "NULL to 49",
+    "P02",    "ALB",     49,    NA,   NA,      "NULL to 49",
     "P02",    "SODIUM",  147.5, "Y",  147.5,   NA_character_
-  ) %>% convert_blanks_to_na
+  )
 
   expect_equal(
     derive_var_shift(
@@ -118,7 +118,41 @@ test_that("Only populate post-baseline", {
       new_var = SHIFT2,
       from_var = BASE,
       to_var = AVAL,
-      post_baseline_condition = (ABLFL != "Y")
+      filter_post_baseline = ABLFL != "Y"
+    ),
+    expected_output
+  )
+})
+
+
+test_that("Shift with user-specified na_val and sep_val", {
+  input <- tibble::tribble(
+    ~USUBJID, ~PARAMCD, ~AVAL, ~ABLFL, ~BNRIND, ~ANRIND,
+    "P01",    "ALB",     33,    "Y",    "LOW",   "LOW",
+    "P01",    "ALB",     38,    NA,     "LOW",   "NORMAL",
+    "P01",    "ALB",     NA,    NA,     "LOW",   NA,
+    "P02",    "ALB",     NA,    "Y",    NA,      NA,
+    "P02",    "ALB",     49,    NA,     NA,      "HIGH",
+    "P02",    "SODIUM",  147,   "Y",    "HIGH",  "HIGH"
+  )
+  expected_output <- tibble::tribble(
+    ~USUBJID, ~PARAMCD, ~AVAL, ~ABLFL, ~BNRIND, ~ANRIND, ~SHIFT1,
+    "P01",    "ALB",     33,    "Y",    "LOW",   "LOW",   "LOW - LOW",
+    "P01",    "ALB",     38,    NA,     "LOW",   "NORMAL", "LOW - NORMAL",
+    "P01",    "ALB",     NA,    NA,     "LOW",   NA,      "LOW - MISSING",
+    "P02",    "ALB",     NA,    "Y",    NA,      NA,      "MISSING - MISSING",
+    "P02",    "ALB",     49,    NA,     NA,      "HIGH",  "MISSING - HIGH",
+    "P02",    "SODIUM",  147,   "Y",    "HIGH",  "HIGH",  "HIGH - HIGH"
+  )
+
+  expect_equal(
+    derive_var_shift(
+      input,
+      new_var = SHIFT1,
+      from_var = BNRIND,
+      to_var = ANRIND,
+      na_val = "MISSING",
+      sep_val = " - "
     ),
     expected_output
   )
