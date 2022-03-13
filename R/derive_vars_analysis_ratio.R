0ec1b465687d7155a2fa3f02ebe9fc7c29ef2aa0
bms63 <ben.x.straub@gsk.com>
2022-03-13 21:12:06 +0000

    #943 Tests written for missing values

--- R/derive_vars_analysis_ratio.R
+++ R/derive_vars_analysis_ratio.R
@@ -1,28 +1,15 @@
 #' Ratio Derivations
 #'
-#' Derives a character shift variable containing concatenated shift in
-#' values based on user-defined pairing, e.g., shift from baseline to
-#' analysis value, shift from baseline grade to analysis grade, ...
+#' Derives a three ratio variables for adlb dataset.
 #'
 #' @param dataset Input dataset
 #'
-#' @param
+#' @param soure_vars
 #'
-#' @param
+#' @param ratio_vars
 #'
-#' @param
+#'  Default: "all"
 #'
-#' @param
-#'
-#'  Default: "NULL"
-#'
-#' @param
-#'
-#'  Default: " to "
-#'
-#' @param
-#'
-#'  Default: `NULL`
 #'
 #' @details
 #'
@@ -39,44 +26,28 @@
 #' @examples
 #' library(dplyr, warn.conflicts = FALSE)
 #'
-#' data <- tibble::tribble(
-#'   ~USUBJID, ~PARAMCD, ~AVAL, ~ABLFL, ~BNRIND, ~ANRIND,
-#'   "P01",    "ALB",     33,    "Y",    "LOW",   "LOW",
-#'   "P01",    "ALB",     38,    NA,     "LOW",   "NORMAL",
-#'   "P01",    "ALB",     NA,    NA,     "LOW",   NA,
-#'   "P02",    "ALB",     37,    "Y",    "NORMAL","NORMAL",
-#'   "P02",    "ALB",     49,    NA,     "NORMAL","HIGH",
-#'   "P02",    "SODIUM",  147,   "Y",    "HIGH",  "HIGH"
-#' )
+#' data <-tibble::tribble(
+#'  ~USUBJID, ~PARAMCD, ~SEQ,  ~AVAL, ~BASE, ~ANRLO, ~ANRHI,
+#'  "P01",    "ALT",     1,     27,    27,    6,      34,
+#'  "P01",    "ALT",     2,     41,    27,    6,      34,
+#'  "P01",    "ALT",     3,     17,    27,    6,      34,
+#'  "P02",    "ALB",     1,     38,    38,    33,     49,
+#'  "P02",    "ALB",     2,     39,    38,    33,     49,
+#'  "P02",    "ALB",     3,     37,    38,    33,     49
+#')
 #'
 #' data %>%
-#'   convert_blanks_to_na() %>%
-#'   derive_var_shift(
-#'     new_var = SHIFT1,
-#'     from_var = BNRIND,
-#'     to_var = ANRIND
-#'   )
-#'
-#' # or only populate post-baseline records
-#' data %>%
-#'   convert_blanks_to_na() %>%
-#'   derive_var_shift(
-#'     new_var = SHIFT1,
-#'     from_var = BNRIND,
-#'     to_var = ANRIND,
-#'     filter_post_baseline = ABLFL != "Y"
-#'   )
+#'   derive_vars_analysis_ratio(ratio_vars = "high")
 #'
 derive_vars_analysis_ratio <- function(dataset,
                                        soure_vars = vars(AVAL, BASE, ANRLO, ANRHI),
                                        ratio_vars = "all") {
-
   soure_vars <- assert_vars(soure_vars)
   ratio_vars <- assert_character_scalar(ratio_vars)
 
   assert_data_frame(dataset, required_vars = quo_c(soure_vars))
 
-  dataset  <- dataset %>%
+  dataset <- dataset %>%
     mutate(
       R2BASE = if_else(!is.na(AVAL) & !is.na(BASE) & BASE != 0, AVAL / BASE, NA_real_),
       R2ANRLO = if_else(!is.na(AVAL) & !is.na(ANRLO) & ANRLO != 0, AVAL / ANRLO, NA_real_),
@@ -85,17 +56,22 @@ derive_vars_analysis_ratio <- function(dataset,
 
   # Remove High/Low Variables based on user-input
   switch(ratio_vars,
-           all  = {dataset <- dataset %>%
-             select(everything(), R2BASE, R2ANRLO, R2ANRHI)},
+    all = {
+      dataset <- dataset %>%
+        select(everything(), R2BASE, R2ANRLO, R2ANRHI)
+    },
 
-           low  = {dataset <- dataset %>%
-             select(everything(), R2BASE, R2ANRLO, -R2ANRHI)},
+    low = {
+      dataset <- dataset %>%
+        select(everything(), R2BASE, R2ANRLO, -R2ANRHI)
+    },
 
-           high = {dataset <- dataset %>%
-             select(everything(), R2BASE, -R2ANRLO, R2ANRHI)}
+    high = {
+      dataset <- dataset %>%
+        select(everything(), R2BASE, -R2ANRLO, R2ANRHI)
+    }
   )
 
-    dataset
-
+  dataset
 }
 
