dbd68a7d480ab57e0cc63b1d00cce64cac6bae95
bms63 <ben.x.straub@gsk.com>
2022-03-21 20:12:45 +0000

    #943 Allowing user to override suffix

--- R/derive_var_analysis_ratio.R
+++ R/derive_var_analysis_ratio.R
@@ -1,24 +1,31 @@
 #' Ratio Derivations
 #'
-#' Derives a three ratio variables for adlb dataset.
+#' Derives a ratio variable for the adlb dataset, e.g Ratio to Baseline, Ratio
+#' to Analysis Range y Lower Limit or Ratio to Analysis Range y Upper Limit.
 #'
 #' @param dataset Input dataset
 #'
-#' @param soure_vars Columns `AVAL`, `BASE`, `ANRLO` and `ANRHI` are expected.
+#' @param numer_var Variable containing values to be used in the numerator of
+#'  the ratio calculation.
 #'
-#' @param ratio_vars Users can select from three values: "all", "low" and "high".
-#' Selecting all will append `R2BASE`, `R2ANRLO`, `R2ANRHI` to the dataset.
-#' Selecting "low" will append only `R2BASE`, `R2ANRLO` and "high" only
-#' `R2BASE`, `R2ANRHI`.
+#' @param denom_var Variable containing values to be used in the numerator of the of
+#'  the ratio calculation.
 #'
-#'  Default: "all"
+#' @param ratio_var_suffix User supplied suffix to be concatenated with `"R2"`
+#'
+#' @param override_prefix A user wishing to remove the default prefix and supply
+#'  their own complete new variable can set this to TRUE.
+#'
+#'  Default is `"FALSE"`.
+#'
+#' @param new_var Name of the new ratio variable being created.
 #'
 #' @details
 #'
 #'
 #' @author Ben Straub
 #'
-#' @return The input dataset with the ratio variables added
+#' @return The input dataset with a ratio variable appended
 #'
 #' @keywords adam bds adlb derivation
 #'
@@ -39,27 +46,44 @@
 #')
 #'
 #' data %>%
-#'   derive_var_analysis_ratio()
+#'  derive_var_analysis_ratio(numer_var = AVAL, denom_var = BASE, ratio_var = BASE) %>%
+#'  derive_var_analysis_ratio(numer_var = AVAL, denom_var = ANRLO, ratio_var = ANR1LO) %>%
+#'  derive_var_analysis_ratio(numer_var = AVAL, denom_var = ANRHI, ratio_var = ANR1HI)
 #'
 derive_var_analysis_ratio <- function(dataset,
                                       numer_var,
                                       denom_var,
-                                      ratio_var
+                                      ratio_var_suffix,
+                                      override_prefix = FALSE,
+                                      new_var
                                       ) {
   numer_var <- assert_symbol(enquo(numer_var))
   denom_var <- assert_symbol(enquo(denom_var))
-  ratio_var <- assert_symbol(enquo(ratio_var))
+  #ratio_var_suffix <- assert_symbol(enquo(ratio_var_suffix))
 
   assert_data_frame(dataset, required_vars = quo_c(numer_var, denom_var))
 
+  if(!override_prefix){
+  r2 <- paste0("R2", ratio_var_suffix)
   dataset <- dataset %>%
     mutate(
-      !!ratio_var := if_else(!is.na(!!numer_var) &
+      !!sym(r2) := if_else(!is.na(!!numer_var) &
                             !is.na(!!denom_var) &
                             !!denom_var != 0,
                             !!numer_var / !!denom_var,
-                            NA_real_),
+                            NA_real_)
+    )
+  } else {
+
+    dataset <- dataset %>%
+      mutate(
+        !!sym(new_var) := if_else(!is.na(!!numer_var) &
+                               !is.na(!!denom_var) &
+                               !!denom_var != 0,
+                             !!numer_var / !!denom_var,
+                             NA_real_),
       )
+  }
 
   dataset
 }
