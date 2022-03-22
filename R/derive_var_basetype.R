de96aa4f44b897ebd43ee926517dd3f9c52c4c98
upadhyor <ojesh.r.upadhyay@gsk.com>
2022-03-22 19:33:13 +0000

    #732 - updated with backticks

--- R/derive_var_basetype.R
+++ R/derive_var_basetype.R
@@ -1,11 +1,11 @@
 #' Derive BASETYPE Variable
 #'
-#' Baseline Type (BASETYPE) is needed when there is more than one definition of
-#' baseline for a given Analysis Parameter (PARAM) in the same dataset.  For a
-#' given parameter, if Baseline Value (BASE) is populated, and there is more than
-#' one definition of baseline, then BASETYPE must be non-null on all records of
-#' any type for that parameter. Each value of BASETYPE refers to a definition of
-#' baseline that characterizes the value of BASE on that row.  Please see
+#' Baseline Type `BASETYPE` is needed when there is more than one definition of
+#' baseline for a given Analysis Parameter `PARAM` in the same dataset.  For a
+#' given parameter, if Baseline Value `BASE` is populated, and there is more than
+#' one definition of baseline, then `BASETYPE` must be non-null on all records of
+#' any type for that parameter. Each value of `BASETYPE` refers to a definition of
+#' baseline that characterizes the value of `BASE` on that row.  Please see
 #' section 4.2.1.6 of the ADaM Implementation Guide, version 1.3 for further
 #' background.
 #'
