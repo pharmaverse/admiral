cdda08901fae5c11be66a693bc0cd777169acd27
Yang, Annie {MDBA~South San Francisco} <yang.chenyue@gene.com>
2022-04-01 03:18:28 +0200

    1. Remove `filter_diff`. 2. Remove reference to AVALU and SI unit. 3. Update function to use PARAMCD and AVAL. 4. Include unit arguments.
    Merge branch 'devel' into 941_derive_param_wbc_abs

--- tests/testthat/test-derive_param_wbc_abs.R
+++ tests/testthat/test-derive_param_wbc_abs.R
@@ -1,56 +1,58 @@
 test_that("Test adding absolute records for each by group", {
   input <- tibble::tribble(
-  ~USUBJID, ~LBTESTCD, ~LBSTRESN, ~LBSTRESU, ~VISIT,
-  "P01",    "WBC",     33,   "10^9/L",       "CYCLE 1 DAY 1",
-  "P01",    "WBC",     38,   "10^9/L",       "CYCLE 2 DAY 1",
-  "P01",    "LYMLE",   90,   "fraction of 1", "CYCLE 1 DAY 1",
-  "P01",    "LYMLE",   70,   "fraction of 1", "CYCLE 2 DAY 1"
+    ~USUBJID, ~PARAMCD, ~AVAL, ~PARAM, ~VISIT,
+  "P01",    "WBC",     33,   "Leukocyte Count (10^9/L)",       "CYCLE 1 DAY 1",
+  "P01",    "WBC",     38,   "Leukocyte Count (10^9/L)",       "CYCLE 2 DAY 1",
+  "P01",    "LYMLE",   90,   "Lymphocytes (fraction of 1)", "CYCLE 1 DAY 1",
+  "P01",    "LYMLE",   70,   "Lymphocytes (fraction of 1)", "CYCLE 2 DAY 1"
   )
   expected_output <- tibble::tribble(
-    ~USUBJID, ~LBTESTCD, ~LBSTRESN, ~LBSTRESU, ~VISIT, ~PARAMCD, ~DTYPE, ~AVAL,
-    "P01",    "WBC",     33,   "10^9/L",       "CYCLE 1 DAY 1", NA_character_, NA_character_, NA,
-    "P01",    "WBC",     38,   "10^9/L",       "CYCLE 2 DAY 1", NA_character_, NA_character_, NA,
-    "P01",    "LYMLE",   90,   "fraction of 1", "CYCLE 1 DAY 1", NA_character_, NA_character_, NA,
-    "P01",    "LYMLE",   70,   "fraction of 1", "CYCLE 2 DAY 1", NA_character_, NA_character_, NA,
-    "P01",    NA_character_, NA, NA_character_, "CYCLE 1 DAY 1", "LYMPHSI", "CALCULATION", 29.7,
-    "P01",    NA_character_, NA, NA_character_, "CYCLE 2 DAY 1", "LYMPHSI", "CALCULATION", 26.6
+    ~USUBJID, ~PARAMCD, ~AVAL, ~PARAM, ~VISIT, ~DTYPE,
+    "P01",    "WBC",     33,   "Leukocyte Count (10^9/L)",       "CYCLE 1 DAY 1", NA_character_,
+    "P01",    "WBC",     38,   "Leukocyte Count (10^9/L)",       "CYCLE 2 DAY 1", NA_character_,
+    "P01",    "LYMLE",   90,   "Lymphocytes (fraction of 1)", "CYCLE 1 DAY 1", NA_character_,
+    "P01",    "LYMLE",   70,   "Lymphocytes (fraction of 1)", "CYCLE 2 DAY 1", NA_character_,
+    "P01",    "LYMPH", 29.7, "Lymphocytes Abs (10^9/L)", "CYCLE 1 DAY 1", "CALCULATION",
+    "P01",    "LYMPH", 26.6, "Lymphocytes Abs (10^9/L)", "CYCLE 2 DAY 1", "CALCULATION"
   )
 
   expect_equal(
     derive_param_wbc_abs(dataset = input,
-                          by_vars = vars(USUBJID, VISIT),
-                          set_values_to = vars(PARAMCD = "LYMPHSI",
-                                               DTYPE = "CALCULATION"),
-                                  filter_diff = LBSTRESU == "fraction of 1",
-                                  wbc_code = "WBC",
-                                  diff_code = "LYMLE"),
+                         by_vars = vars(USUBJID, VISIT),
+                         set_values_to = vars(PARAMCD = "LYMPH",
+                                              PARAM = "Lymphocytes Abs (10^9/L)",
+                                              DTYPE = "CALCULATION"),
+                         get_unit_expr = extract_unit(PARAM),
+                         wbc_code = "WBC",
+                         diff_code = "LYMLE"),
     expected_output
   )
 })
 
 test_that("Test when only one of WBC/differential is present", {
   input <- tibble::tribble(
-    ~USUBJID, ~LBTESTCD, ~LBSTRESN, ~LBSTRESU, ~VISIT,
-    "P01",    "WBC",     33,   "10^9/L",       "CYCLE 1 DAY 1",
-    "P01",    "WBC",     38,   "10^9/L",       "CYCLE 2 DAY 1",
-    "P01",    "LYMLE",   70,   "fraction of 1", "CYCLE 2 DAY 1",
-    "P01",    "LYMLE",   80,   "fraction of 1", "CYCLE 3 DAY 1"
+    ~USUBJID, ~PARAMCD, ~AVAL, ~PARAM, ~VISIT,
+    "P01",    "WBC",     33,   "Leukocyte Count (10^9/L)",       "CYCLE 1 DAY 1",
+    "P01",    "WBC",     38,   "Leukocyte Count (10^9/L)",       "CYCLE 2 DAY 1",
+    "P01",    "LYMLE",   70,   "Lymphocytes (fraction of 1)", "CYCLE 2 DAY 1",
+    "P01",    "LYMLE",   80,   "Lymphocytes (fraction of 1)", "CYCLE 3 DAY 1"
   )
   expected_output <- tibble::tribble(
-    ~USUBJID, ~LBTESTCD, ~LBSTRESN, ~LBSTRESU, ~VISIT, ~PARAMCD, ~DTYPE, ~AVAL,
-    "P01",    "WBC",     33,   "10^9/L",       "CYCLE 1 DAY 1", NA_character_, NA_character_, NA,
-    "P01",    "WBC",     38,   "10^9/L",       "CYCLE 2 DAY 1", NA_character_, NA_character_, NA,
-    "P01",    "LYMLE",   70,   "fraction of 1", "CYCLE 2 DAY 1", NA_character_, NA_character_, NA,
-    "P01",    "LYMLE",   80,   "fraction of 1", "CYCLE 3 DAY 1", NA_character_, NA_character_, NA,
-    "P01",    NA_character_, NA, NA_character_, "CYCLE 2 DAY 1", "LYMPHSI", "CALCULATION", 26.6
+    ~USUBJID, ~PARAMCD, ~AVAL, ~PARAM, ~VISIT, ~DTYPE,
+    "P01",    "WBC",     33,   "Leukocyte Count (10^9/L)",       "CYCLE 1 DAY 1", NA_character_,
+    "P01",    "WBC",     38,   "Leukocyte Count (10^9/L)",       "CYCLE 2 DAY 1", NA_character_,
+    "P01",    "LYMLE",   70,   "Lymphocytes (fraction of 1)", "CYCLE 2 DAY 1", NA_character_,
+    "P01",    "LYMLE",   80,   "Lymphocytes (fraction of 1)", "CYCLE 3 DAY 1", NA_character_,
+    "P01",    "LYMPH" , 26.6,  "Lymphocytes Abs (10^9/L)", "CYCLE 2 DAY 1", "CALCULATION"
   )
 
   expect_equal(
     derive_param_wbc_abs(dataset = input,
                           by_vars = vars(USUBJID, VISIT),
-                          set_values_to = vars(PARAMCD = "LYMPHSI",
+                          set_values_to = vars(PARAMCD = "LYMPH",
+                                               PARAM = "Lymphocytes Abs (10^9/L)",
                                                DTYPE = "CALCULATION"),
-                          filter_diff = LBSTRESU == "fraction of 1",
+                          get_unit_expr = extract_unit(PARAM),
                           wbc_code = "WBC",
                           diff_code = "LYMLE"),
     expected_output
@@ -59,28 +61,29 @@ test_that("Test when only one of WBC/differential is present", {
 
 test_that("Test when absolute record already present in source dataset 1", {
   input <- tibble::tribble(
-    ~USUBJID, ~LBTESTCD, ~LBSTRESN, ~LBSTRESU, ~VISIT,
-    "P01",    "WBC",     33,   "10^9/L",       "CYCLE 1 DAY 1",
-    "P01",    "WBC",     38,   "10^9/L",       "CYCLE 2 DAY 1",
-    "P01",    "LYMPHSI", 27,   "10^9/L",    "CYCLE 2 DAY 1",
-    "P01",    "LYMLE",   70,   "fraction of 1", "CYCLE 2 DAY 1",
-    "P01",    "LYMLE",   80,   "fraction of 1", "CYCLE 3 DAY 1"
+    ~USUBJID, ~PARAMCD, ~AVAL, ~PARAM, ~VISIT,
+    "P01",    "WBC",     33,   "Leukocyte Count (10^9/L)",       "CYCLE 1 DAY 1",
+    "P01",    "WBC",     38,   "Leukocyte Count (10^9/L)",       "CYCLE 2 DAY 1",
+    "P01",    "LYMPH",   27,   "Lymphocytes Abs (10^9/L)",    "CYCLE 2 DAY 1",
+    "P01",    "LYMLE",   70,   "Lymphocytes (fraction of 1)", "CYCLE 2 DAY 1",
+    "P01",    "LYMLE",   80,   "Lymphocytes (fraction of 1)", "CYCLE 3 DAY 1"
   )
   expected_output <- tibble::tribble(
-    ~USUBJID, ~LBTESTCD, ~LBSTRESN, ~LBSTRESU, ~VISIT,
-    "P01",    "WBC",     33,   "10^9/L",       "CYCLE 1 DAY 1",
-    "P01",    "WBC",     38,   "10^9/L",       "CYCLE 2 DAY 1",
-    "P01",    "LYMPHSI", 27,   "10^9/L",    "CYCLE 2 DAY 1",
-    "P01",    "LYMLE",   70,   "fraction of 1", "CYCLE 2 DAY 1",
-    "P01",    "LYMLE",   80,   "fraction of 1", "CYCLE 3 DAY 1"
+    ~USUBJID, ~PARAMCD, ~AVAL, ~PARAM, ~VISIT,
+    "P01",    "WBC",     33,   "Leukocyte Count (10^9/L)",       "CYCLE 1 DAY 1",
+    "P01",    "WBC",     38,   "Leukocyte Count (10^9/L)",       "CYCLE 2 DAY 1",
+    "P01",    "LYMPH",   27,   "Lymphocytes Abs (10^9/L)",    "CYCLE 2 DAY 1",
+    "P01",    "LYMLE",   70,   "Lymphocytes (fraction of 1)", "CYCLE 2 DAY 1",
+    "P01",    "LYMLE",   80,   "Lymphocytes (fraction of 1)", "CYCLE 3 DAY 1"
   )
 
   expect_equal(
     derive_param_wbc_abs(dataset = input,
                           by_vars = vars(USUBJID, VISIT),
-                          set_values_to = vars(PARAMCD = "LYMPHSI",
+                          set_values_to = vars(PARAMCD = "LYMPH",
+                                               PARAM = "Lymphocytes Abs (10^9/L)",
                                                DTYPE = "CALCULATION"),
-                          filter_diff = LBSTRESU == "fraction of 1",
+                          get_unit_expr = extract_unit(PARAM),
                           wbc_code = "WBC",
                           diff_code = "LYMLE"),
     expected_output
@@ -90,30 +93,31 @@ test_that("Test when absolute record already present in source dataset 1", {
 
 test_that("Test when absolute record already present in source dataset 2", {
   input <- tibble::tribble(
-    ~USUBJID, ~LBTESTCD, ~LBSTRESN, ~LBSTRESU, ~VISIT,
-    "P01",    "WBC",     33,   "10^9/L",       "CYCLE 1 DAY 1",
-    "P01",    "WBC",     38,   "10^9/L",       "CYCLE 2 DAY 1",
-    "P01",    "LYMPHSI", 27,   "10^9/L",    "CYCLE 2 DAY 1",
-    "P01",    "LYMLE",   70,   "fraction of 1", "CYCLE 2 DAY 1",
-    "P01",    "LYMLE",   80,   "fraction of 1", "CYCLE 1 DAY 1"
+    ~USUBJID, ~PARAMCD, ~AVAL, ~PARAM, ~VISIT,
+    "P01",    "WBC",     33,   "Leukocyte Count (10^9/L)",       "CYCLE 1 DAY 1",
+    "P01",    "WBC",     38,   "Leukocyte Count (10^9/L)",       "CYCLE 2 DAY 1",
+    "P01",    "LYMPH",   27,   "Lymphocytes Abs (10^9/L)",    "CYCLE 2 DAY 1",
+    "P01",    "LYMLE",   70,   "Lymphocytes (fraction of 1)", "CYCLE 2 DAY 1",
+    "P01",    "LYMLE",   80,   "Lymphocytes (fraction of 1)", "CYCLE 1 DAY 1"
   )
 
   expected_output <- tibble::tribble(
-    ~USUBJID, ~LBTESTCD, ~LBSTRESN, ~LBSTRESU, ~VISIT, ~PARAMCD, ~DTYPE, ~AVAL,
-    "P01",    "WBC",     33,   "10^9/L",       "CYCLE 1 DAY 1", NA_character_, NA_character_, NA,
-    "P01",    "WBC",     38,   "10^9/L",       "CYCLE 2 DAY 1", NA_character_, NA_character_, NA,
-    "P01",    "LYMPHSI", 27,   "10^9/L",    "CYCLE 2 DAY 1",  NA_character_, NA_character_, NA,
-    "P01",    "LYMLE",   70,   "fraction of 1", "CYCLE 2 DAY 1", NA_character_, NA_character_, NA,
-    "P01",    "LYMLE",   80,   "fraction of 1", "CYCLE 1 DAY 1", NA_character_, NA_character_, NA,
-    "P01",    NA_character_, NA, NA_character_, "CYCLE 1 DAY 1", "LYMPHSI", "CALCULATION", 26.4
+    ~USUBJID, ~PARAMCD, ~AVAL, ~PARAM, ~VISIT, ~DTYPE,
+    "P01",    "WBC",     33,   "Leukocyte Count (10^9/L)",       "CYCLE 1 DAY 1", NA_character_,
+    "P01",    "WBC",     38,   "Leukocyte Count (10^9/L)",       "CYCLE 2 DAY 1", NA_character_,
+    "P01",    "LYMPH", 27,     "Lymphocytes Abs (10^9/L)",    "CYCLE 2 DAY 1",  NA_character_,
+    "P01",    "LYMLE",   70,   "Lymphocytes (fraction of 1)", "CYCLE 2 DAY 1", NA_character_,
+    "P01",    "LYMLE",   80,   "Lymphocytes (fraction of 1)", "CYCLE 1 DAY 1", NA_character_,
+    "P01",    "LYMPH", 26.4,   "Lymphocytes Abs (10^9/L)", "CYCLE 1 DAY 1", "CALCULATION"
   )
 
   expect_equal(
     derive_param_wbc_abs(dataset = input,
                           by_vars = vars(USUBJID, VISIT),
-                          set_values_to = vars(PARAMCD = "LYMPHSI",
+                          set_values_to = vars(PARAMCD = "LYMPH",
+                                               PARAM = "Lymphocytes Abs (10^9/L)",
                                                DTYPE = "CALCULATION"),
-                          filter_diff = LBSTRESU == "fraction of 1",
+                          get_unit_expr = extract_unit(PARAM),
                           wbc_code = "WBC",
                           diff_code = "LYMLE"),
     expected_output
