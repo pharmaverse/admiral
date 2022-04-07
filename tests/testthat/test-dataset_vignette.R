8f0b9d8ed011580c4f0608d5a0147d5e17eb9a1a
bms63 <ben.x.straub@gsk.com>
2022-04-07 15:28:31 +0000

    #1055 Slight change to the test name

--- tests/testthat/test-dataset_vignette.R
+++ tests/testthat/test-dataset_vignette.R
@@ -8,7 +8,7 @@ test_that("a 'datatables' object is output when run inside pkgdown", {
   expect_s3_class(dataset_vignette(head(adsl)), "datatables")
 })
 
-test_that("a 'datatables' object is output when run outside pkgdown", {
+test_that("a 'knitr_kable' object is output when run outside pkgdown", {
   Sys.setenv(IN_PKGDOWN = "false")
   on.exit(Sys.setenv(IN_PKGDOWN = ""))
   expect_s3_class(
