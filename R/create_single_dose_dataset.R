105b63d62bd3f139c1ccd7b4330cdf5ad1804045
amsmith214 <andrew.m.smith@gsk.com>
2022-05-03 16:18:10 +0000

    sorting out lstalvdt and extreme_date docs

--- R/create_single_dose_dataset.R
+++ R/create_single_dose_dataset.R
@@ -1,6 +1,5 @@
 #' Pre-Defined Dose Frequencies
 #'
-#' @description
 #' These pre-defined dose frequencies are sourced from
 #' [CDISC](https://evs.nci.nih.gov/ftp1/CDISC/SDTM/SDTM%20Terminology.pdf). The
 #' number of rows to generate using `create_single_dose_dataset()` arguments
@@ -12,15 +11,15 @@
 #' traceability.
 #'
 #' `DOSE_COUNT` represents the number of doses received in one single unit of
-#' `DOSE_WINDOW`. For example, for `CDISC_VALUE=="10 DAYS PER MONTH`,
+#' `DOSE_WINDOW`. For example, for `CDISC_VALUE=="10 DAYS PER MONTH"`,
 #' `DOSE_WINDOW=="MONTH"` and `DOSE_COUNT==10`. Similarly, for
-#' `CDISC_VALUE=="EVERY 2 WEEKS`, `DOSE_WINDOW=="WEEK"` and
+#' `CDISC_VALUE=="EVERY 2 WEEKS"`, `DOSE_WINDOW=="WEEK"` and
 #' `DOSE_COUNT==0.5` (to yield one dose every two weeks).
 #'
 #' `CONVERSION_FACTOR` is used to convert `DOSE_WINDOW` units `"WEEK"`,
 #'  `"MONTH"`, and `"YEAR"` to the unit `"DAY"`.
 #'
-#' For example, for `CDISC_VALUE=="10 DAYS PER MONTH`, `CONVERSION_FACTOR`
+#' For example, for `CDISC_VALUE=="10 DAYS PER MONTH"`, `CONVERSION_FACTOR`
 #' is `0.0329`. One day of a month is assumed to be `1 / 30.4375` of a month (one
 #' day is assumed to be `1/365.25` of a year).
 #' Given only `start_date` and `end_date` in the aggregate dataset, `CONVERSION_FACTOR`
