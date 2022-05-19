45acb9d848e82fbae96c915391eb733ae39303b0
bms63 <ben.x.straub@gsk.com>
2022-05-19 14:43:37 +0000

    #1147 Fixing filter_if example

--- R/user_utils.R
+++ R/user_utils.R
@@ -65,10 +65,11 @@ negate_vars <- function(vars = NULL) {
 #'
 #' @examples
 #' library(admiraltest)
+#' library(admiral)
 #' data(admiral_vs)
 #'
 #' filter_if(admiral_vs, rlang::quo(NULL))
-#' filter_if(admiral_vs, rlang::quo(VSTESTCD == "Weight"))
+#' filter_if(admiral_vs, rlang::quo(VSTESTCD == "WEIGHT"))
 filter_if <- function(dataset, filter) {
   assert_data_frame(dataset)
   assert_filter_cond(filter, optional = TRUE)
