2cd9d6360137817b6381b00da9ada1507b9dcc00
Harris, Alana {MDBB~Welwyn} <alana.harris@roche.com>
2022-05-31 14:20:21 +0200

    #525 - adding assertions

--- R/get_summary_records.R
+++ R/get_summary_records.R
@@ -64,13 +64,23 @@ get_summary_records <- function(dataset,
                                 summary_fun,
                                 set_values_to){
 
-  # Quotes the filter argument and ensure that it is a condition
+  assert_vars(by_vars)
+  analysis_var <- assert_symbol(enquo(analysis_var))
   filter <- assert_filter_cond(enquo(filter), optional = TRUE)
+  assert_s3_class(summary_fun, "function")
+  assert_data_frame(
+    dataset,
+    required_vars = quo_c(by_vars, analysis_var)
+  )
+  if (!is.null(set_values_to)) {
+    assert_varval_list(set_values_to, optional = TRUE)
+  }
+
 
   # Summarise the analysis value
   dataset %>%
-    group_by(!!!by_vars) %>%
     filter_if(filter) %>%
+    group_by(!!!by_vars) %>%
     summarise(!!analysis_var := summary_fun(!!analysis_var)) %>%
     mutate(!!!set_values_to) %>%
     ungroup()
