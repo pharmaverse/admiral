fe79eb2af4f1e4ebc6c1315f17f30eec1010b72d
Ondrej Slama <ondrej.slama@roche.com>
2022-06-29 07:39:25 +0000

    switched input to file path instead of document_context object

--- R/addin_format_testthat.R
+++ R/addin_format_testthat.R
@@ -1,29 +1,36 @@
 
 # Returns the call for updating a given test_that test file
 # by adding a function name, a test number, and a section.
-# Call the function either by using wrapper "format_test_that_file" or
-# programmatically in a for loop on list.files("tests/testthat", pattern = "^test-")
-prepare_test_that_file <- function(file_info) {
+# Call the function either by using RStudio Addin "format_test_that_file" or
+# programmatically in a for loop on
+# `list.files("tests/testthat", pattern="^test-.*\\.R$", full.names = T)`
+# and using the code in "format_test_that_file"
+prepare_test_that_file <- function(path) {
 
-  assert_s3_class(file_info, "document_context")
+  assert_character_scalar(path)
+
+  # check that file exists
+  if (!file.exists(path)) {
+    stop("Invalid file path, the file does not exist.")
+  }
 
   # check that testthat is used
   uses_testhat <- devtools::uses_testthat()
   if (!uses_testhat) {
-    stop("This addin requires that testthat package is used.")
+    stop("This Addin requires that testthat package is used.")
   }
 
   # check that testing file is opened
-  uses_test_file <- grepl("tests/testthat/test-", file_info$path, fixed = T)
+  uses_test_file <- grepl("tests/testthat/test-", path, fixed = T)
   if (!uses_test_file) {
-    stop("This addin works only on unit test files that follow a testthat structure.")
+    stop("This Addin works only on unit test files that follow a testthat structure.")
   }
 
   # parse the name of the testing function
-  testing_fun <- sub("^test-", "", sub(".R$", "", basename(file_info$path)))
+  testing_fun <- sub("^test-", "", sub(".R$", "", basename(path)))
 
   # get file content
-  file_content <- file_info$contents
+  file_content <- readLines(path)
 
   # get locations of tests - match 'test_that("' strings
   test_that_loc <- grep('^test_that\\("', file_content)
@@ -35,7 +42,7 @@ prepare_test_that_file <- function(file_info) {
   )
 
   # get test descriptions
-  test_that_lines <- file_info$contents[test_that_loc]
+  test_that_lines <- file_content[test_that_loc]
   test_that_desc_parsed <- stringr::str_extract(
     string = test_that_lines,
     pattern = paste0(
@@ -46,7 +53,7 @@ prepare_test_that_file <- function(file_info) {
   )
 
   # formulate new test descriptions (update only those that don't include test_title)
-  new_descriptions <- ifelse(
+  new_desc <- ifelse(
     stringr::str_detect(test_that_desc_parsed, test_title),
     test_that_desc_parsed,
     paste0(test_title, test_that_desc_parsed)
@@ -56,42 +63,39 @@ prepare_test_that_file <- function(file_info) {
   test_that_lines_updated <- stringr::str_replace(
     string = test_that_lines,
     pattern = '(?<=test_that\\(").*"',
-    replacement = paste0(new_descriptions, '"')
+    replacement = paste0(new_desc, '"')
   )
 
   # formulate headers according to RStudio editor functionality
-  headers <- paste0("# ---- ", new_descriptions, " ----")
+  headers <- paste0("# ---- ", new_desc, " ----")
 
   # arguments to modify the file content - replace the new test descriptions
   l_desc <- list(
     location = Map(c, Map(c, test_that_loc, 1), Map(c, test_that_loc, Inf)),
-    text = test_that_lines_updated,
-    id = file_info$id
+    text = test_that_lines_updated
   )
 
   # arguments to modify the file content - add headers if not present
   idx_valid <- !headers %in% file_content
   l_header <- list(
     location = Map(c, test_that_loc, 1)[idx_valid],
-    text = paste0(headers[idx_valid], "\n"),
-    id = file_info$id
+    text = paste0(headers[idx_valid], "\n")
   )
 
-  return(
-    list(
-      descriptions = l_desc,
-      headers_cond = any(idx_valid),
-      headers = l_header,
-      file_info = file_info
-    )
+  list(
+    descriptions = l_desc,
+    headers_cond = any(idx_valid),
+    headers = l_header
   )
 }
 
 # Function for the RStudio Addin, see inst/rstudio/addins.dcf.
 # Wrapper of prepare_test_that_file.
 format_test_that_file <- function() {
-  result <- prepare_test_that_file(file_info = rstudioapi::getActiveDocumentContext())
-  do.call(rstudioapi::insertText, result$descriptions)
-  `if`(result$headers_cond, do.call(rstudioapi::insertText, result$headers))
-  rstudioapi::documentSave(id = result$file_info$id)
+  file_info <- rstudioapi::getActiveDocumentContext()
+  result <- prepare_test_that_file(path = file_info$path)
+  do.call(rstudioapi::insertText, append(result$descriptions, list(id = file_info$id)))
+  if (result$headers_cond)
+    do.call(rstudioapi::insertText, append(result$headers, list(id = file_info$id)))
+  rstudioapi::documentSave(id = file_info$id)
 }
