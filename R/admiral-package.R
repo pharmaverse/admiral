5a5d2423ba9019642a9f6b4c4d66ee1cc2d5f2fb
bms63 <ben.x.straub@gsk.com>
2022-08-10 22:05:53 +0000

    #1231 Need to call admiraldev in vignettes.  removed left_join from exported function.

--- R/admiral-package.R
+++ R/admiral-package.R
@@ -26,14 +26,5 @@
 #' @importFrom hms as_hms
 #' @importFrom lifecycle deprecate_warn deprecated deprecate_stop
 #' @importFrom readxl read_excel
-#' @importFrom admiraldev left_join assert_character_scalar assert_character_vector assert_data_frame
-#'             assert_expr assert_filter_cond assert_function assert_function_param
-#'             assert_has_variables assert_integer_scalar assert_list_element assert_list_of
-#'             assert_logical_scalar assert_named_exprs assert_numeric_vector assert_one_to_one assert_order_vars
-#'             assert_param_does_not_exist assert_s3_class assert_symbol assert_unit assert_vars assert_varval_list
-#'             get_constant_vars get_duplicates get_source_vars is_auto is_date is_named is_order_vars is_timeunit
-#'             is_valid_date_entry is_valid_day is_valid_dtc is_valid_hour is_valid_sec_min is_valid_month is_valid_time_entry what_is_it
-#'             suppress_warning warn_if_incomplete_dtc warn_if_inconsistent_list
-#'             warn_if_invalid_dtc warn_if_vars_exist expect_dfs_equal backquote enumerate quo_c squote dquote
-#'             dataset_vignette arg_name convert_dtm_to_dtc extract_vars `%notin%` replace_values_by_names
+#' @import admiraldev
 "_PACKAGE"
