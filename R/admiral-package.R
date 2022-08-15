0f7df5d2fd8f3410515119fe468b2b34142ba520
neitmant <thomas.neitmann@roche.com>
2022-08-15 15:32:15 +0200

    Import all of {admiraldev}

--- R/admiral-package.R
+++ R/admiral-package.R
@@ -1,5 +1,6 @@
 #' @keywords internal
 #' @family internal
+#' @import admiraldev
 #' @importFrom dplyr arrange bind_rows case_when desc ends_with filter full_join group_by
 #'             if_else mutate mutate_at mutate_if n pull rename rename_at row_number select slice
 #'             semi_join starts_with transmute ungroup vars n_distinct union distinct
