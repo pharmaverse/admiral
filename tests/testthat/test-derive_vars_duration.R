1400e5ff8d412c09a0d43f911ee9c26838578302
Syedmubasheer <syedmubasheer95@yahoo.com>
2022-08-24 12:04:49 +0000

    Updated Test cases as per Thomas comments

--- tests/testthat/test-derive_vars_duration.R
+++ tests/testthat/test-derive_vars_duration.R
@@ -3,13 +3,16 @@ library(lubridate)
 
 test_that("derive_vars_duration Test 1: Duration and unit variable are added", {
   input <- tribble(
-    ~BRTHDT, ~RANDDT,
-    ymd("1984-09-06"), ymd("2020-02-24"),
-    ymd("1985-01-01"), NA,
-    NA, ymd("2021-03-10"),
-    NA, NA
+    ~USUBJID, ~BRTHDT, ~RANDDT,
+    "P01", ymd("1984-09-06"), ymd("2020-02-24"),
+    "P02", ymd("1985-01-01"), NA,
+    "P03", NA, ymd("2021-03-10"),
+    "P04", NA, NA
   )
-  expected_output <- mutate(input, AGE = c(35, NA, NA, NA), AGEU = c("YEARS", NA, NA, NA))
+  expected_output <- mutate(input,
+    AGE = c(35, NA, NA, NA),
+    AGEU = c("YEARS", NA_character_, NA_character_, NA_character_)
+  )
   actual_output <- derive_vars_duration(
     input,
     new_var = AGE,
@@ -20,16 +23,16 @@ test_that("derive_vars_duration Test 1: Duration and unit variable are added", {
     trunc_out = TRUE
   )
 
-  expect_equal(actual_output, expected_output)
+  expect_dfs_equal(actual_output, expected_output, keys = "USUBJID")
 })
 
 test_that("derive_vars_duration Test 2: Duration and unit variable are added", {
   input <- tribble(
-    ~ASTDT, ~AENDT,
-    ymd("2021-03-05"), ymd("2021-03-02"),
-    ymd("2019-09-18"), ymd("2019-09-18"),
-    ymd("1985-01-01"), NA,
-    NA, NA
+    ~USUBJID, ~ASTDT, ~AENDT,
+    "P01", ymd("2021-03-05"), ymd("2021-03-02"),
+    "P02", ymd("2019-09-18"), ymd("2019-09-18"),
+    "P03", ymd("1985-01-01"), NA,
+    "P04", NA, NA
   )
   expected_output <- mutate(input,
     ADURN = c(-3, 1, NA, NA),
@@ -44,20 +47,20 @@ test_that("derive_vars_duration Test 2: Duration and unit variable are added", {
     out_unit = "days"
   )
 
-  expect_equal(actual_output, expected_output)
+  expect_dfs_equal(actual_output, expected_output, keys = "USUBJID")
 })
 
 test_that("derive_vars_duration Test 3: Duration and unit variable are added", {
   input <- tribble(
-    ~ADTM, ~TRTSDTM,
-    ymd_hms("2019-08-09T04:30:56"), ymd_hms("2019-08-09T05:00:00"),
-    ymd_hms("2019-11-11T10:30:00"), ymd_hms("2019-11-11T11:30:00"),
-    ymd("2019-11-11"), ymd_hms("2019-11-11T04:00:00"),
-    NA, ymd_hms("2019-11-11T12:34:56"),
+    ~USUBJID, ~ADTM, ~TRTSDTM,
+    "P01", ymd_hms("2019-08-09T04:30:56"), ymd_hms("2019-08-09T05:00:00"),
+    "P02", ymd_hms("2019-11-11T10:30:00"), ymd_hms("2019-11-11T11:30:00"),
+    "P03", ymd("2019-11-11"), ymd_hms("2019-11-11T04:00:00"),
+    "P04", NA, ymd_hms("2019-11-11T12:34:56"),
   )
   expected_output <- mutate(input,
     ADURN = c(30, 60, 240, NA),
-    ADURU = c("MINUTES", "MINUTES", "MINUTES", NA)
+    ADURU = c("MINUTES", "MINUTES", "MINUTES", NA_character_)
   )
   actual_output <- derive_vars_duration(
     input,
@@ -70,5 +73,5 @@ test_that("derive_vars_duration Test 3: Duration and unit variable are added", {
     add_one = FALSE
   )
 
-  expect_equal(actual_output, expected_output)
+  expect_dfs_equal(actual_output, expected_output, keys = "USUBJID")
 })
