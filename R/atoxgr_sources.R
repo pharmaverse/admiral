78d8e139e0ba55b35a979ba344d17a9462337d6d
Miller, Gordon {MDBI~Welwyn} <gordon.miller@roche.com>
2022-08-24 12:25:23 +0200

    945 Fixed styler issue

--- R/atoxgr_sources.R
+++ R/atoxgr_sources.R
@@ -41,5 +41,5 @@
 #' @export
 
 atoxgr_criteria_ctcv4 <- system.file("adlb_grading_spec.xlsx", package = "admiral") %>%
-    read_excel(sheet = "NCICTCAEv4") %>%
-    mutate(GRADE_CRITERIA_CODE = gsub("[\r\n]", " ", GRADE_CRITERIA_CODE))
+  read_excel(sheet = "NCICTCAEv4") %>%
+  mutate(GRADE_CRITERIA_CODE = gsub("[\r\n]", " ", GRADE_CRITERIA_CODE))
