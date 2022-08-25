beacae619572a10ddfc542e834401a8d0dccf662
bms63 <ben.x.straub@gsk.com>
2022-08-25 12:57:39 +0000

    CRAN Feedback:  Description update.  Using donttest rather than dontrun

--- R/expect_dfs_equal.R
+++ R/expect_dfs_equal.R
@@ -18,12 +18,11 @@
 #' @export
 #'
 #' @examples
-#' \dontrun{
-#' testthat::test_that("a missing row is detected", {
-#'   data(dm)
-#'   expect_dfs_equal(dm, dm[-1L, ], keys = "USUBJID")
-#' })
-#' }
+#' library(admiral.test)
+#' \donttest{(testthat::test_that("a missing row is detected", {
+#'   data(admiral_dm)
+#'   expect_dfs_equal(admiral_dm, admiral_dm[-1L, ], keys = "USUBJID")
+#' })}
 expect_dfs_equal <- function(base, compare, keys, ...) {
   diff <- diffdf::diffdf(base, compare, keys, suppress_warnings = TRUE, ...)
   if (diffdf::diffdf_has_issues(diff)) {
