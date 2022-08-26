043ebadc26151389a00076c07216b6bb67c0e3c0
Yang, Annie {MDBA~South San Francisco} <yang.chenyue@gene.com>
2022-08-26 02:49:24 +0200

    #724 - improve documentation

--- R/derive_vars_query.R
+++ R/derive_vars_query.R
@@ -1,6 +1,16 @@
 #' Derive Query Variables
 #'
-#' @details For each unique element in `VAR_PREFIX`, the corresponding "NAM"
+#' @details This function can be used to derive CDISC variables such as
+#'   `SMQzzNAM`, `SMQzzCD`, `SMQzzSC`, `SMQzzSCN`, and `CQzzNAM` in ADAE and
+#'   ADMH, and variables such as `SDGzzNAM`, `SDGzzCD`, and `SDGzzSC` in ADCM.
+#'   An example usage of this function can be found in the
+#'   [OCCDS vignette](https://r.roche.com/s/71c86bafe62a4200fced1/files/Projects/Roche_GSK_Admiral/docs/articles/occds.html).
+#'
+#'   A query dataset is expected as an input to this function. See the
+#'   [Queries Dataset Documentation vignette](https://r.roche.com/s/71c86bafe62a4200fced1/files/Projects/Roche_GSK_Admiral/docs/articles/queries_dataset.html)
+#'   for descriptions, or call `data("queries")` for an example of a query dataset.
+#'
+#'   For each unique element in `VAR_PREFIX`, the corresponding "NAM"
 #'   variable will be created. For each unique `VAR_PREFIX`, if `QUERY_ID` is
 #'   not "" or NA, then the corresponding "CD" variable is created; similarly,
 #'   if `QUERY_SCOPE` is not "" or NA, then the corresponding "SC" variable will
