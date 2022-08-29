bbfa8d705abacc8f0bc7bdd482402843f4388c9c
bms63 <ben.x.straub@gsk.com>
2022-08-29 20:57:27 +0000

    Fixing styling issue

--- R/assertions.R
+++ R/assertions.R
@@ -1,4 +1,4 @@
-filter_if <- function (dataset, filter) {
+filter_if <- function(dataset, filter) {
   assert_data_frame(dataset, check_is_grouped = FALSE)
   assert_filter_cond(filter, optional = TRUE)
   if (quo_is_null(filter)) {
