2ba62f8932ca10201eeadf8cd524bce66e6c3d60
Eric <Eric.M.Simms@gsk.com>
2022-09-27 02:39:35 +0000

    test-derive_var_last_dose_date: Replaced library(tibble), library(dplyr) and library(lubridate) with tibble::function(), dplyr::function() and lubridate::function().

--- tests/testthat/test-derive_var_last_dose_date.R
+++ tests/testthat/test-derive_var_last_dose_date.R
@@ -1,7 +1,4 @@
-library(tibble)
-library(dplyr)
-library(lubridate)
-input_ae <- tribble(
+input_ae <- tibble::tribble(
   ~STUDYID, ~USUBJID, ~AESEQ, ~AESTDTC,
   "my_study", "subject1", 1, "2020-01-02",
   "my_study", "subject1", 2, "2020-08-31",
@@ -10,11 +7,11 @@ input_ae <- tribble(
   "my_study", "subject2", 2, "2020-02-20",
   "my_study", "subject3", 1, "2020-03-02",
   "my_study", "subject4", 1, "2020-11-02"
-) %>% mutate(
-  AESTDT = ymd(AESTDTC)
+) %>% dplyr::mutate(
+  AESTDT = lubridate::ymd(AESTDTC)
 )
 
-input_ex <- tribble(
+input_ex <- tibble::tribble(
   ~STUDYID, ~USUBJID, ~EXSTDTC, ~EXENDTC, ~EXSEQ, ~EXDOSE, ~EXTRT,
   "my_study", "subject1", "2020-01-01", "2020-01-01", 1, 10, "treatment",
   "my_study", "subject1", "2020-08-29", "2020-08-29", 2, 10, "treatment",
@@ -24,7 +21,7 @@ input_ex <- tribble(
   "my_study", "subject2", "2020-01-20", "2020-01-20", 2, 0, "placebo",
   "my_study", "subject3", "2020-03-15", "2020-03-15", 1, 10, "treatment"
 ) %>%
-  mutate(EXSTDT = as.Date(EXSTDTC), EXENDT = as.Date(EXENDTC))
+  dplyr::mutate(EXSTDT = as.Date(EXSTDTC), EXENDT = as.Date(EXENDTC))
 
 # derive_var_last_dose_date ----
 ## Test 1: works as expected output_datetime = FALSE ----
@@ -39,9 +36,9 @@ test_that("derive_var_last_dose_date Test 1: works as expected output_datetime =
     "my_study", "subject3", 1, "2020-03-02", NA_character_,
     "my_study", "subject4", 1, "2020-11-02", NA_character_
   ) %>%
-    mutate(
+    dplyr::mutate(
       LDOSEDTM = as.Date(LDOSEDTM),
-      AESTDT = ymd(AESTDTC)
+      AESTDT = lubridate::ymd(AESTDTC)
     )
 
   res <- derive_var_last_dose_date(
@@ -62,7 +59,7 @@ test_that("derive_var_last_dose_date Test 1: works as expected output_datetime =
 
 ## Test 2: works as expected with output_datetime = TRUE ----
 test_that("derive_var_last_dose_date Test 2: works as expected with output_datetime = TRUE", {
-  expected_output <- tribble(
+  expected_output <- tibble::tribble(
     ~STUDYID, ~USUBJID, ~AESEQ, ~AESTDTC, ~LDOSEDTM,
     "my_study", "subject1", 1, "2020-01-02", "2020-01-01 00:00:00",
     "my_study", "subject1", 2, "2020-08-31", "2020-08-29 00:00:00",
@@ -72,9 +69,9 @@ test_that("derive_var_last_dose_date Test 2: works as expected with output_datet
     "my_study", "subject3", 1, "2020-03-02", NA_character_,
     "my_study", "subject4", 1, "2020-11-02", NA_character_
   ) %>%
-    mutate(
+    dplyr::mutate(
       LDOSEDTM = as.POSIXct(as.character(LDOSEDTM), tz = "UTC"),
-      AESTDT = ymd(AESTDTC)
+      AESTDT = lubridate::ymd(AESTDTC)
     )
 
   res <- derive_var_last_dose_date(
@@ -95,7 +92,7 @@ test_that("derive_var_last_dose_date Test 2: works as expected with output_datet
 
 ## Test 3: returns traceability vars ----
 test_that("derive_var_last_dose_date Test 3: returns traceability vars", {
-  expected_output <- tribble(
+  expected_output <- tibble::tribble(
     ~STUDYID, ~USUBJID, ~AESEQ, ~AESTDTC, ~LDOSEDTM,
     "my_study", "subject1", 1, "2020-01-02", "2020-01-01 00:00:00",
     "my_study", "subject1", 2, "2020-08-31", "2020-08-29 00:00:00",
@@ -105,12 +102,12 @@ test_that("derive_var_last_dose_date Test 3: returns traceability vars", {
     "my_study", "subject3", 1, "2020-03-02", NA_character_,
     "my_study", "subject4", 1, "2020-11-02", NA_character_
   ) %>%
-    mutate(
+    dplyr::mutate(
       LDOSEDTM = as.POSIXct(as.character(LDOSEDTM), tz = "UTC"),
       LDOSEDOM = c("EX", "EX", "EX", NA, "EX", NA, NA),
       LDOSESEQ = c(1, 2, 3, NA, 2, NA, NA),
       LDOSEVAR = c("EXENDTC", "EXENDTC", "EXENDTC", NA, "EXENDTC", NA, NA),
-      AESTDT = ymd(AESTDTC)
+      AESTDT = lubridate::ymd(AESTDTC)
     )
 
   res <- derive_var_last_dose_date(
