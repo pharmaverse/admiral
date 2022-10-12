60a4dbfed4073c3568f57061f77b7fec6acff5c1
zdz2101 <zhu.zelos@gmail.com>
2022-10-12 14:59:33 -0400

    Match formating from previous test 3

--- tests/testthat/test-dataset_vignette.R
+++ tests/testthat/test-dataset_vignette.R
@@ -27,7 +27,8 @@ test_that("dataset_vignette, test 3: A 'knitr_kable' object is output when run o
 test_that("dataset_vignette, test 4: A 'datatables' object is output when run inside pkgdown using display_vars input", {
   Sys.setenv(IN_PKGDOWN = "true")
   on.exit(Sys.setenv(IN_PKGDOWN = ""))
-  expect_s3_class(dataset_vignette(head(admiral_dm), display_vars = vars(STUDYID, USUBJID)),
-                  "datatables"
+  expect_s3_class(
+    dataset_vignette(head(admiral_dm), display_vars = vars(STUDYID, USUBJID)),
+    "datatables"
   )
 })
