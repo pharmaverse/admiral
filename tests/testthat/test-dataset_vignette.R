34eb54d64aae0db1f8a3ab28498b30121042038e
zdz2101 <zhu.zelos@gmail.com>
2022-10-13 11:28:37 -0400

    Fix formatting, use tibble::tribble() for dummy data, and add test for error when calling column not in dataset.

--- tests/testthat/test-dataset_vignette.R
+++ tests/testthat/test-dataset_vignette.R
@@ -1,13 +1,19 @@
 # dataset_vignette ----
 ## Test 1: A 'knitr_kable' object is output when run outside pkgdown ----
 test_that("dataset_vignette Test 1: A 'knitr_kable' object is output when run outside pkgdown", {
-  dm <- tibble(
-    STUDYID = rep("1001", 100),
-    USUBJID = as.character(1:100),
-    COUNTRY = rep("USA", 100)
+  Sys.setenv(IN_PKGDOWN = "false")
+  on.exit(Sys.setenv(IN_PKGDOWN = ""))
+
+  dm <- tibble::tribble(
+    ~STUDYID, ~USUBJID, ~COUNTRY,
+    "STUDY1", "1",      "USA",
+    "STUDY1", "2",      "USA",
+    "STUDY1", "3",      "USA",
+    "STUDY1", "4",      "USA"
   )
 
   expect_s3_class(dataset_vignette(dm), "knitr_kable")
+  expect_s3_class(dataset_vignette(dm, display_vars = vars(STUDYID, USUBJID)), "knitr_kable")
 })
 
 ## Test 2: A 'datatables' object is output when run inside pkgdown ----
@@ -15,39 +21,28 @@ test_that("dataset_vignette Test 2: A 'datatables' object is output when run ins
   Sys.setenv(IN_PKGDOWN = "true")
   on.exit(Sys.setenv(IN_PKGDOWN = ""))
 
-  dm <- tibble(
-    STUDYID = rep("1001", 100),
-    USUBJID = as.character(1:100),
-    COUNTRY = rep("USA", 100)
+  dm <- tibble::tribble(
+    ~STUDYID, ~USUBJID, ~COUNTRY,
+    "STUDY1", "1",      "USA",
+    "STUDY1", "2",      "USA",
+    "STUDY1", "3",      "USA",
+    "STUDY1", "4",      "USA"
   )
+
 
   expect_s3_class(dataset_vignette(dm), "datatables")
-})
-
-## Test 3: A 'knitr_kable' object is output when run outside pkgdown with display_vars ----
-test_that("dataset_vignette Test 3: A 'knitr_kable' object is output when run outside pkgdown with display_vars", {
-  Sys.setenv(IN_PKGDOWN = "false")
-  on.exit(Sys.setenv(IN_PKGDOWN = ""))
-
-  dm <- tibble(
-    STUDYID = rep("1001", 100),
-    USUBJID = as.character(1:100),
-    COUNTRY = rep("USA", 100)
-  )
-
-  expect_s3_class(dataset_vignette(dm, display_vars = vars(STUDYID, USUBJID)), "knitr_kable")
-})
-
-## Test 4: A 'datatables' object is output when run inside pkgdown with display_vars ----
-test_that("dataset_vignette Test 4: A 'datatables' object is output when run inside pkgdown with display_vars", {
-  Sys.setenv(IN_PKGDOWN = "true")
-  on.exit(Sys.setenv(IN_PKGDOWN = ""))
-
-  dm <- tibble(
-    STUDYID = rep("1001", 100),
-    USUBJID = as.character(1:100),
-    COUNTRY = rep("USA", 100)
-  )
-
   expect_s3_class(dataset_vignette(dm, display_vars = vars(STUDYID, USUBJID)), "datatables")
+})
+
+## Test 3: An error is output when calling variable not in dataset ----
+test_that("dataset_vignette Test 3: An error is output when calling variable not in dataset", {
+  dm <- tibble::tribble(
+    ~STUDYID, ~USUBJID, ~COUNTRY,
+    "STUDY1", "1",      "USA",
+    "STUDY1", "2",      "USA",
+    "STUDY1", "3",      "USA",
+    "STUDY1", "4",      "USA"
+  )
+
+  expect_error(dataset_vignette(dm, display_vars = vars(AGE)))
 })
