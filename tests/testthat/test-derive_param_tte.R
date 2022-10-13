561dbf67db989c51e03c5387ada6a06874b9a352
Jeff Dickinson <jeffreyd@gene.com>
2022-10-13 07:46:56 -0500

    Update tests/testthat/test-derive_param_tte.R
    Co-authored-by: Thomas Neitmann <thomas.neitmann@roche.com>

--- tests/testthat/test-derive_param_tte.R
+++ tests/testthat/test-derive_param_tte.R
@@ -636,9 +636,6 @@ test_that("derive_param_tte Test 9: error is issued if parameter code already ex
 
 ## Test 10: error is issued if package does not exist ----
 test_that("list_tte_source_objects Test 1: error is issued if package does not exist", {
-
-  expected_output <- list_tte_source_objects(package = "admiral")
-
   expect_error(
     list_tte_source_objects(package = "tte"),
     regexp = "No package called 'tte' is installed and hence no `tte_source` objects are available"
