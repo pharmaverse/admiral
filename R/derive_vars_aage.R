c9110c83e0027e6e5857f53dd89f82590fceea5f
fshanlee <fshanlee@gmail.com>
2022-11-03 18:37:23 +0000

    Moved the call to assert_character_scalar(), for checking age_unit, to nearer the start of the function, so that age_unit is always checked. Made use of the case_sensitive parameter of assert_character_scalar(), to re-assign age_unit, so that subsequent references to this parameter don't require calls to tolower().

--- R/derive_vars_aage.R
+++ R/derive_vars_aage.R
@@ -81,6 +81,7 @@ derive_vars_aage <- function(dataset,
   )
 }
 
+
 #' Derive Age in Years
 #'
 #' @details This function is used to convert age variables into years.
@@ -132,12 +133,15 @@ derive_var_age_years <- function(dataset, age_var, age_unit = NULL, new_var) {
   age_var <- age_variable
   unit_var <- paste0(quo_get_expr(age_var), "U")
 
-  if (!is.null(age_unit)) {
-    if (length(age_unit) > 1) {
-      err_msg <- "Only one unit can be specified via the `age_unit` parameter"
-      abort(err_msg)
-    }
-  }
+  age_unit <- assert_character_scalar(
+    age_unit,
+    values = c(
+      "years", "months", "weeks", "days",
+      "hours", "minutes", "seconds"
+    ),
+    case_sensitive = FALSE,
+    optional = TRUE
+  )
 
   new_var <- assert_symbol(enquo(new_var))
   warn_if_vars_exist(dataset, quo_text(new_var))
@@ -150,16 +154,9 @@ derive_var_age_years <- function(dataset, age_var, age_unit = NULL, new_var) {
       )
       abort(err_msg)
     } else {
-      assert_character_scalar(
-        tolower(age_unit),
-        values = c(
-          "years", "months", "weeks", "days",
-          "hours", "minutes", "seconds"
-        )
-      )
       ds <- dataset %>%
         mutate(
-          !!new_var := time_length(duration(!!age_var, units = tolower(age_unit)),
+          !!new_var := time_length(duration(!!age_var, units = age_unit),
             unit = "years"
           )
         )
@@ -184,7 +181,7 @@ derive_var_age_years <- function(dataset, age_var, age_unit = NULL, new_var) {
           unit_var
         )
         warn(msg)
-      } else if (unit != tolower(age_unit)) {
+      } else if (unit != age_unit) {
         msg <- paste(
           "The variable unit", unit_var, "is associated with", quo_get_expr(age_var),
           "but the argument `age_unit` has been specified with a different value.",
