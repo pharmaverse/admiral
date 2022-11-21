2160c5299bb0a2a3a2c61e920c3705276e0ac2b1
sadchla-codes <sadchla.mathieu@gmail.com>
2022-11-21 17:31:23 +0000

    Fixing the header for test units to close #1589

--- tests/testthat/test-user_utils.R
+++ tests/testthat/test-user_utils.R
@@ -1,6 +1,5 @@
 # convert_blanks_to_na ----
 ## Test 1: blank strings are turned into `NA` ----
-# convert_blanks_to_na Test 1: blank strings are turned into `NA` ----
 test_that("convert_blanks_to_na Test 1: blank strings are turned into `NA`", {
   expect_identical(
     convert_blanks_to_na(c("a", "", "b")),
@@ -9,7 +8,6 @@ test_that("convert_blanks_to_na Test 1: blank strings are turned into `NA`", {
 })
 
 ## Test 2: attributes are preserved when converting blanks to `NA` ----
-# convert_blanks_to_na Test 2: attributes are preserved when converting blanks to `NA` ----
 test_that("convert_blanks_to_na Test 2: attributes are preserved when converting blanks to `NA`", {
   input <- structure(letters, names = rev(letters), label = "Letters")
   input[c(1, 9, 23)] <- NA
@@ -20,7 +18,6 @@ test_that("convert_blanks_to_na Test 2: attributes are preserved when converting
 })
 
 ## Test 3: blank strings are turned into `NA` inside data frames ----
-# convert_blanks_to_na Test 3: blank strings are turned into `NA` inside data frames ----
 test_that("convert_blanks_to_na Test 3: blank strings are turned into `NA` inside data frames", {
   input <- tibble::tibble(
     a = structure(c("a", "b", "", "c"), label = "A"),
@@ -38,9 +35,8 @@ test_that("convert_blanks_to_na Test 3: blank strings are turned into `NA` insid
   expect_identical(convert_blanks_to_na(input), expected_output)
 })
 
-# convert_blanks_to_na.list ----
+
 ## Test 4: `convert_blanks_to_na.list` produces a lists ----
-# convert_blanks_to_na.list Test 4: `convert_blanks_to_na.list` produces a lists ----
 test_that("convert_blanks_to_na.list Test 4: `convert_blanks_to_na.list` produces a lists", {
   x <- c("", "", "")
   expected_output <- lapply(x, convert_blanks_to_na)
@@ -49,9 +45,7 @@ test_that("convert_blanks_to_na.list Test 4: `convert_blanks_to_na.list` produce
   expect_equal(expected_output, actual_output)
 })
 
-# convert_na_to_blanks ----
-## Test 5: `NA`  strings are turned into blank  ----
-# ---- convert_na_to_blanks Test 5: `NA`  strings are turned into blank  ----
+#  Test 5: convert_na_to_blanks Test 5----
 test_that("convert_na_to_blanks Test 5: `NA` strings are turned into blank ", {
   expect_identical(
     convert_na_to_blanks(c("a", NA, "b")),
@@ -60,7 +54,6 @@ test_that("convert_na_to_blanks Test 5: `NA` strings are turned into blank ", {
 })
 
 ## Test 6: attributes are preserved when converting `NA` to blanks  ----
-# convert_na_to_blanks Test 6: attributes are preserved when converting `NA` to blanks ----
 test_that("convert_na_to_blanks Test 6: attributes are preserved when converting `NA` to blanks", {
   input <- structure(letters, names = rev(letters), label = "Letters")
   input[c(1, 9, 23)] <- NA_character_
@@ -71,7 +64,6 @@ test_that("convert_na_to_blanks Test 6: attributes are preserved when converting
 })
 
 ## Test 7: `NA` are turned into blank strings inside data frames ----
-# convert_na_to_blanks Test 7: `NA` are turned into blank strings inside data frames ----
 test_that("convert_na_to_blanks Test 7: `NA` are turned into blank strings inside data frames", {
   input <- tibble::tibble(
     a = structure(c("a", "b", NA, "c"), label = "A"),
@@ -92,7 +84,6 @@ test_that("convert_na_to_blanks Test 7: `NA` are turned into blank strings insid
 
 # convert_na_to_blanks.list ----
 ## Test 8: `convert_na_to_blanks.list` produces a lists ----
-# convert_na_to_blanks.list Test 8: `convert_na_to_blanks.list` produces a lists ----
 test_that("convert_na_to_blanks.list Test 8: `convert_na_to_blanks.list` produces a lists", {
   x <- c(NA_character_, NA_character_, NA_character_)
   expected_output <- lapply(x, convert_na_to_blanks)
@@ -103,20 +94,17 @@ test_that("convert_na_to_blanks.list Test 8: `convert_na_to_blanks.list` produce
 
 # negate_vars ----
 ## Test 9: negate_vars returns list of negated variables ----
-# negate_vars Test 9: negate_vars returns list of negated variables ----
 test_that("negate_vars Test 9: negate_vars returns list of negated variables", {
   expect_identical(negate_vars(vars(var1, var2)), rlang::exprs(-var1, -var2))
 })
 
 ## Test 10: negate_vars returns NULL if input is NULL ----
-# negate_vars Test 10: negate_vars returns NULL if input is NULL ----
 test_that("negate_vars Test 6: negate_vars returns NULL if input is NULL", {
   expect_identical(negate_vars(NULL), NULL)
 })
 
 # get_one_to_many_dataset ----
 ## Test 11: returns a data frame after a previous error ----
-# get_one_to_many_dataset Test 11: returns a data frame after a previous error ----
 test_that("get_one_to_many_dataset Test 11: returns a data frame after a previous error", {
   try(assert_one_to_one(admiral_adsl, vars(STUDYID), vars(SITEID)), silent = TRUE)
 
@@ -125,7 +113,6 @@ test_that("get_one_to_many_dataset Test 11: returns a data frame after a previou
 
 # get_many_to_one_dataset ----
 ## Test 12: returns a data frame after a previous error ----
-# get_many_to_one_dataset Test 12: returns a data frame after a previous error ----
 test_that("get_many_to_one_dataset Test 12: returns a data frame after a previous error", {
   try(assert_one_to_one(admiral_adsl, vars(SITEID), vars(STUDYID)), silent = TRUE)
 
@@ -134,7 +121,6 @@ test_that("get_many_to_one_dataset Test 12: returns a data frame after a previou
 
 # print.source ----
 ## Test 13: `source` objects are printed as intended ----
-# print.source Test 13: `source` objects are printed as intended ----
 test_that("print.source Test 13: `source` objects are printed as intended", {
   ttae <- event_source(
     dataset_name = "ae",
@@ -162,7 +148,6 @@ test_that("print.source Test 13: `source` objects are printed as intended", {
 })
 
 ## Test 14: `source` objects containing `source` objects ----
-# print.source Test 14: `source` objects containing `source` objects ----
 test_that("print.source Test 14: `source` objects containing `source` objects", {
   slice <-
     derivation_slice(
@@ -180,7 +165,6 @@ test_that("print.source Test 14: `source` objects containing `source` objects", 
 })
 
 ## Test 15: `source` objects containing `data.frame` ----
-# print.source Test 15: `source` objects containing `data.frame` ----
 test_that("print.source Test 15: `source` objects containing `data.frame`", {
   cqterms <- tibble::tribble(
     ~TERM_NAME,                  ~TERM_ID,
@@ -214,9 +198,7 @@ test_that("print.source Test 15: `source` objects containing `data.frame`", {
   )
 })
 
-# print_named_list ----
-## Test 16: named list ----
-# print_named_list Test 16: named list ----
+## Test 16 print_named_list ----
 test_that("print_named_list Test 16: named list", {
   expect_identical(
     capture.output(print_named_list(list(a = 1, b = 2))),
@@ -228,7 +210,6 @@ test_that("print_named_list Test 16: named list", {
 })
 
 ## Test 17: unnamed list ----
-# print_named_list Test 17: unnamed list ----
 test_that("print_named_list Test 17: unnamed list", {
   expect_identical(
     capture.output(print_named_list(list(1, 2))),
