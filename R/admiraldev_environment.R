2881a8a980a8256495805aa0f057c9535847639d
Zelos Zhu <zdz2101@github.com>
2022-11-23 17:32:15 +0000

    #179 - Add comment about how get_dataset retrieves the objects created

--- R/admiraldev_environment.R
+++ R/admiraldev_environment.R
@@ -3,3 +3,6 @@ admiraldev_environment <- new.env(parent = emptyenv())
 # assertions.R
 admiraldev_environment$many_to_one <- NULL
 admiraldev_environment$one_to_many <- NULL
+
+# datasets.R
+# get_dataset() is used to retrieve many_to_one and one_to_many
