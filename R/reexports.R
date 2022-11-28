c35c0db4f19ae412707ac5478ac651ab5703a01f
Zelos Zhu <zdz2101@github.com>
2022-11-28 17:55:05 +0000

    #1591 - Include noRd tags

--- R/reexports.R
+++ R/reexports.R
@@ -1,8 +1,19 @@
+#' Function Re-exports
+#'
+#' @details
+#' Sometimes functions from other libraries become critically important while using admiral
+#' and thus should be included as an export. This applies especially to functions which are
+#' frequently called within `admiral` function arguments. The goal of these exports is such that
+#' admiral comes ready "out of the box", similar to how one might think the pipe operator, `%>%`,
+#' comes from `dplyr` but is actually native to `magrittr`.
+#' @noRd
 #' @export
 dplyr::vars
 
+#' @noRd
 #' @export
 dplyr::desc
 
+#' @noRd
 #' @export
 magrittr::`%>%`
