13324d8df38a17f53374179d3d5f2fca00688230
Zelos Zhu <zdz2101@github.com>
2022-11-29 15:00:49 +0000

    #1572 - Add roxygen like blurb about why/how we use environments from admiraldev

--- R/admiral_environment.R
+++ R/admiral_environment.R
@@ -1,4 +1,18 @@
+#' Environment Objects
+#'
+#' @details
+#' Once in a while, we may encounter "locked binding for 'xxx'." errors
+#' during the development process while building out functions. This may arise because
+#' we want to create dynamic data/objects based on user-inputs that need modification
+#' at points in time after the package has been loaded. To manage such data or objects,
+#' R has a data structure known as an 'environment'. These environment objects are created
+#' at build time, but can be populated with values after the package has been loaded and
+#' update those values over the course of an R session. For more details how environments work,
+#' see relevant sections on environments in R Packages and Advanced R textbooks for more details.
+#' @noRd
+
 admiral_environment <- new.env(parent = emptyenv())
+
 # See respective ...R page for usage
 
 # admiral_options.R ----
