969c50d9c43adb837e9d3504b09ece8ffbea349e
Declan Hodges <declanhodges2@hotmail.com>
2023-02-08 16:42:14 +0000

    Updated man pages, and finished off function documentation and unit tests.

--- R/compute_scale.R
+++ R/compute_scale.R
@@ -10,21 +10,21 @@
 #' @param source_range The permitted source range
 #'
 #'   A numeric vector containing two elements is expected, representing the
-#'   lower and upper bounds of the source range.
+#'   lower and upper bounds of the permitted source range.
 #'
-#' @param target_range The permitted target range
+#' @param target_range The target range
 #'
 #'   A numeric vector containing two elements is expected, representing the
 #'   lower and upper bounds of the target range.
 #'
 #' @param flip_direction Flip direction of the scale?
 #'
-#'   The transformed values will be flipped within the target range.
-#'   REMOVE: Is there a better word for flip in this context?
+#'   The transformed values will be reversed within the target range, e.g.
+#'   within the range 0 to 100, 25 would be reversed to 75.
 #'
 #'   Default: `FALSE`
 #'
-#'   Permitted Values: `TRUE` `FALSE`
+#'   Permitted Values: `TRUE`, `FALSE`
 #'
 #' @param min_n Minimum number of values for computation
 #'
@@ -36,12 +36,11 @@
 #'
 #'   Default: 1
 #'
-#' @details
-#' Returns a numeric value. If source contains less than min_n values, the
-#' result is set to NA.
+#' @details Returns a numeric value. If source contains less than min_n values,
+#'   the result is set to `NA`.
 #'
-#' @return A value representing the average of source transformed to the target
-#'   range or `NA` if source doesn't contain `min_n` values.
+#' @return The average of source transformed to the target range or `NA` if
+#'   source doesn't contain `min_n` values.
 #'
 #' @keywords com_bds_findings
 #'
@@ -50,13 +49,19 @@
 #' @export
 #'
 #' @examples
+#' compute_scale(
+#'   source = c(1, 4, 3, 5),
+#'   source_range = c(1, 5),
+#'   target_range = c(0, 100),
+#'   flip_direction = TRUE,
+#'   min_n = 3
+#' )
 #'
 compute_scale <- function(source,
                           source_range,
                           target_range,
                           flip_direction = FALSE,
-                          min_n = 1){
-
+                          min_n = 1) {
   # Function argument checks
   assert_numeric_vector(source)
   assert_numeric_vector(source_range)
@@ -66,22 +71,21 @@ compute_scale <- function(source,
 
 
   # Computation
-  if(sum(!is.na(source)) >= min_n){
+  if (sum(!is.na(source)) >= min_n) {
     source_mean <- mean(source, na.rm = TRUE)
 
     scale_constant <- min(target_range) - min(source_range)
     scale_coefficient <- (max(target_range) - min(target_range)) / (max(source_range) - min(source_range))
 
-    target = (source_mean + scale_constant) * scale_coefficient
+    target <- (source_mean + scale_constant) * scale_coefficient
 
-    if(flip_direction == TRUE){
-      target = max(target_range) - target
+    if (flip_direction == TRUE) {
+      target <- max(target_range) - target
     }
-  }
-  else{
-    target = NA
+  } else {
+    target <- NA
   }
 
   target
-
 }
+
--- tests/testthat/test-compute_scale.R
+++ tests/testthat/test-compute_scale.R
@@ -1,9 +1,10 @@
+# Input test dataset
 adqs <- tibble::tribble(
   ~STUDYID, ~USUBJID, ~PARAMCD, ~AVISIT, ~AVISITN, ~AVAL,
   "ADMIRAL01", "01-701-1015", "ITEM1", "WEEK 10", 100, 5,
   "ADMIRAL01", "01-701-1015", "ITEM2", "WEEK 10", 100, 1,
   "ADMIRAL01", "01-701-1015", "ITEM3", "WEEK 10", 100, 2,
-  "ADMIRAL01", "01-701-1015", "ITEM4", "WEEK 10", 100, 4,
+  "ADMIRAL01", "01-701-1015", "ITEM4", "WEEK 10", 100, NA,
   "ADMIRAL01", "01-701-1015", "ITEM1", "WEEK 20", 200, 1,
   "ADMIRAL01", "01-701-1015", "ITEM2", "WEEK 20", 200, 3,
   "ADMIRAL01", "01-701-1015", "ITEM3", "WEEK 20", 200, 5,
@@ -16,66 +17,78 @@ adqs <- tibble::tribble(
 
 ## Test 1: compute_scale() works as expected ----
 test_that("compute_scale Test 1: compute_scale works as expected", {
-
   input <- adqs %>%
     filter(USUBJID == "01-701-1015" & AVISITN == 100) %>%
     pull()
 
   expected_output <- mean((input - 1) * 25, na.rm = TRUE)
 
-  expect_equal(compute_scale(input, c(1, 5), c(0, 100), min_n = 3), expected_output)
-
+  expect_equal(compute_scale(input,
+                             c(1, 5),
+                             c(0, 100),
+                             min_n = 3),
+               expected_output)
 })
 
 ## Test 2: scale is flipped if flip_direction == TRUE ----
 test_that("compute_scale Test 2: scale is flipped if flip_direction == TRUE", {
-
   input <- adqs %>%
-    filter(USUBJID == "01-701-1015" & AVISITN == 200 & PARAMCD %in% c("ITEM5", "ITEM6", "ITEM7")) %>%
+    filter(USUBJID == "01-701-1015" &
+             AVISITN == 200 &
+             PARAMCD %in% c("ITEM5", "ITEM6", "ITEM7")) %>%
     pull()
 
   expected_output <- 100 - (mean((input - 1) * 50, na.rm = TRUE))
 
-  expect_equal(compute_scale(input, c(1, 3), c(0, 100), flip_direction = TRUE, min_n = 3), expected_output)
-
+  expect_equal(compute_scale(input,
+                             c(1, 3),
+                             c(0, 100),
+                             flip_direction = TRUE,
+                             min_n = 3),
+               expected_output)
 })
 
 ## Test 3: result is missing if min_n is not met ----
 test_that("compute_scale Test 3: result is missing if min_n is not met", {
-
   input <- adqs %>%
     filter(USUBJID == "01-701-1281" & AVISITN == 100) %>%
     pull()
 
   expected_output <- NA
 
-  expect_equal(compute_scale(input, c(1, 5), c(0, 100), min_n = 2), expected_output)
-
+  expect_equal(compute_scale(input,
+                             c(1, 5),
+                             c(0, 100),
+                             min_n = 2),
+               expected_output)
 })
 
 ## Test 4: compute_scale() works as expected within derive_summary_records() ----
-test_that("compute_scale Test 4: compute_scale() works as expected within derive_summary_records()", {
+test_that("compute_scale Test 4: compute_scale() works as expected within
+          derive_summary_records()", {
+  expected_output <- bind_rows(
+    adqs,
+    adqs %>%
+      filter(PARAMCD %in% c("ITEM1", "ITEM2", "ITEM3")) %>%
+      group_by(STUDYID, USUBJID, AVISIT, AVISITN) %>%
+      summarise(n = n(), AVAL = mean(AVAL, na.rm = TRUE)) %>%
+      mutate(
+        PARAMCD = "ITEMAVG",
+        AVAL = if_else(n >= 3, 100 - ((AVAL - 1) * 25), NA)
+      ) %>%
+      select(-n)
+  )
 
-  expected_output <- bind_rows(adqs,
-                               adqs %>%
-                                filter(PARAMCD %in% c("ITEM1", "ITEM2", "ITEM3")) %>%
-                                  group_by(STUDYID, USUBJID, AVISIT, AVISITN) %>%
-                                  summarise(n = n(), AVAL = mean(AVAL, na.rm = TRUE)) %>%
-                                  mutate(
-                                    PARAMCD = "ITEMAVG",
-                                    AVAL = if_else(n >= 3, 100 - ((AVAL - 1) * 25), NA)
-                                  ) %>%
-                                 select(-n)
-                              )
-
-  expect_equal(derive_summary_records(
-                adqs,
-                by_vars = exprs(STUDYID, USUBJID, AVISIT, AVISITN),
-                filter = (PARAMCD %in% c("ITEM1", "ITEM2", "ITEM3")),
-                analysis_var = AVAL,
-                summary_fun = function(x) compute_scale(x, c(1, 5), c(0, 100), flip_direction = TRUE, min_n = 3),
-                set_values_to = vars(PARAMCD = "ITEMAVG")
-              ),
-  expected_output)
-
+  expect_equal(
+    derive_summary_records(
+      adqs,
+      by_vars = exprs(STUDYID, USUBJID, AVISIT, AVISITN),
+      filter = (PARAMCD %in% c("ITEM1", "ITEM2", "ITEM3")),
+      analysis_var = AVAL,
+      summary_fun = function(x)
+        compute_scale(x, c(1, 5), c(0, 100), flip_direction = TRUE, min_n = 3),
+      set_values_to = exprs(PARAMCD = "ITEMAVG")
+    ),
+    expected_output
+  )
 })
