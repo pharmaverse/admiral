f4462ea00247a6a989a86aa7701a7ef0f317973c
Ben Straub <ben.x.straub@gsk.com>
2023-02-16 21:03:10 -0500

    Update ad_admh.R

--- inst/templates/ad_admh.R
+++ inst/templates/ad_admh.R
@@ -163,7 +163,7 @@ admh <- admh %>%
     by_vars = exprs(STUDYID, USUBJID)
   )
 
-# test
+# testing
 derive_var_agegr_fda(admiral.test::admiral_dm, AGE, age_unit = "years", new_var = AGEGRP)
 
 # Final Steps, Select final variables and Add labels
