a69eb56c6c19af117828629ded357a3a526826af
Bundfuss, Stefan {MDBB~Basel} <stefan.bundfuss@roche.com>
2023-04-25 18:32:15 +0200

    1725_enhance_derive_extreme_records: add derive_param_extreme_event functionality to derive_extreme_records

--- R/derive_extreme_records.R
+++ R/derive_extreme_records.R
@@ -101,42 +101,96 @@
 #'     DTYPE = "LOV"
 #'   )
 #' )
-derive_extreme_records <- function(dataset,
+derive_extreme_records <- function(dataset = NULL,
+                                   dataset_add = NULL,
+                                   dataset_ref = NULL,
                                    by_vars = NULL,
-                                   order,
-                                   mode,
+                                   order = NULL,
+                                   mode = NULL,
+                                   filter_add = NULL,
                                    check_type = "warning",
-                                   filter = NULL,
-                                   set_values_to) {
+                                   exist_flag = NULL,
+                                   true_value = "Y",
+                                   false_value = "N",
+                                   set_values_to,
+                                   filter) {
+  if (!missing(filter)) {
+    deprecate_warn("0.11.0", "derive_extreme_records(filter = )", "derive_extreme_records(filter_add = )")
+    filter_add <- enexpr(filter)
+  }
+
   # Check input parameters
   assert_vars(by_vars, optional = TRUE)
-  assert_order_vars(order)
+  assert_order_vars(order, optional = TRUE)
   assert_data_frame(
     dataset,
     required_vars = expr_c(
       by_vars, extract_vars(order)
-    )
+    ),
+    optional = TRUE
   )
-  mode <- assert_character_scalar(mode, values = c("first", "last"), case_sensitive = FALSE)
+  mode <- assert_character_scalar(
+    mode,
+    values = c("first", "last"),
+    case_sensitive = FALSE,
+    optional = TRUE
+  )
   check_type <-
     assert_character_scalar(
       check_type,
       values = c("none", "warning", "error"),
       case_sensitive = FALSE
     )
-  filter <- assert_filter_cond(enexpr(filter), optional = TRUE)
+  exist_flag <- assert_symbol(enexpr(exist_flag), optional = TRUE)
+  filter_add <- assert_filter_cond(enexpr(filter_add), optional = TRUE)
   assert_varval_list(set_values_to)
+  if (is.null(dataset) && is.null(dataset_add)) {
+    abort(paste(
+      "Neither `dataset` nor `dataset_add` is specified.",
+      "At least one of them must be specified.",
+      sep = "\n")
+    )
+  }
 
   # Create new observations
-  new_obs <- dataset %>%
-    filter_if(filter) %>%
-    filter_extreme(
+  if (is.null(dataset_add)) {
+    dataset_add <- dataset
+  }
+  new_add_obs <- filter_if(dataset_add, filter_add)
+
+  if (!is.null(order)) {
+    new_add_obs <- filter_extreme(
+      new_add_obs,
       by_vars = by_vars,
       order = order,
       mode = mode,
       check_type = check_type
-    ) %>%
-    mutate(!!!set_values_to)
+    )
+  }
+
+  if (!is.null(dataset_ref)) {
+    add_vars <- colnames(dataset_add)
+    ref_vars <- colnames(dataset_ref)
+
+    new_ref_obs <- anti_join(
+      select(dataset_ref, intersect(add_vars, ref_vars)),
+      select(new_add_obs, !!!by_vars),
+      by = sapply(by_vars, as_name) # nolint: undesirable_function_linter
+    )
+
+    if (!is.null(exist_flag)) {
+      new_add_obs <- mutate(new_add_obs, !!exist_flag := true_value)
+      new_ref_obs <- mutate(new_ref_obs, !!exist_flag := false_value)
+    }
+    new_obs <- bind_rows(new_add_obs, new_ref_obs)
+  } else {
+    new_obs <- new_add_obs
+  }
+
+  new_obs <- process_set_values_to(
+    new_obs,
+    set_values_to = set_values_to
+  )
 
   # Create output dataset
   bind_rows(dataset, new_obs)
