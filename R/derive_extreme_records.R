f079b4f29fadc563ded78c7f46a39107d83c764f
Bundfuss, Stefan {MDBB~Basel} <stefan.bundfuss@roche.com>
2023-04-27 14:09:28 +0200

    1725_enhance_derive_extreme_records: fix lintr check

--- R/derive_extreme_records.R
+++ R/derive_extreme_records.R
@@ -256,7 +256,11 @@ derive_extreme_records <- function(dataset = NULL,
                                    set_values_to,
                                    filter) {
   if (!missing(filter)) {
-    deprecate_warn("0.11.0", "derive_extreme_records(filter = )", "derive_extreme_records(filter_add = )")
+    deprecate_warn(
+      "0.11.0",
+      "derive_extreme_records(filter = )",
+      "derive_extreme_records(filter_add = )"
+    )
     filter_add <- enexpr(filter)
   }
 
