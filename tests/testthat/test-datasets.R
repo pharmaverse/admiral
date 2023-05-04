927c31e2c344288e5897e9a669ee6fbbdb0aacc0
Zelos Zhu <zdz2101@github.com>
2023-05-04 18:25:55 +0000

    chore: #104 styler fix

--- tests/testthat/test-datasets.R
+++ tests/testthat/test-datasets.R
@@ -8,6 +8,6 @@ test_that("get_dataset Test 1: get_dataset works", {
 test_that("get_dataset Test 2: get_dataset works", {
   expect_error(
     get_dataset("test"),
-   "`name` must be one of 'one_to_many' or 'many_to_one' but is 'test'"
+    "`name` must be one of 'one_to_many' or 'many_to_one' but is 'test'"
   )
 })
