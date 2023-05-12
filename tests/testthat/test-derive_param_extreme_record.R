a79bcbdec176054ae3dd32c0f878ba92ffe03170
Zelos Zhu <zdz2101@github.com>
2023-05-12 17:57:18 +0000

    test: #1822 add edge cases

--- tests/testthat/test-derive_param_extreme_record.R
+++ tests/testthat/test-derive_param_extreme_record.R
@@ -62,7 +62,7 @@ test_that("derive_param_extreme_record Test 1: Analysis date are derived correct
 })
 
 ## Test 2: Error given when order variable is not inside source datasets ----
-test_that("derive_param_extreme_record Test 2: Error given when order variable is not inside source datasets", {
+test_that("derive_param_extreme_record Test 2: Error given when order variable is not inside source datasets", { # nolint
   aevent <- tibble::tribble(
     ~STUDYID, ~USUBJID,     ~LBSTDTC, ~PARAMCD, ~PARAM,
     "1001",        "1", "2023-01-01",    "TST", "TEST",
@@ -150,6 +150,53 @@ test_that("derive_param_extreme_record Test 3: Error given when sources is not i
         PARAM = "First Anti-Cancer Therapy"
       )
     ),
-    regexp = "Each element of `sources` must be an object of class/type 'records_source' but the following are not:"
+    regexp = "Each element of `sources` must be an object of class/type 'records_source' but the following are not:" # nolint
   )
+})
+
+## Test 4: Non-existent/missing values are accounted for ----
+test_that("derive_param_extreme_record Test 4: Non-existent/missing values are accounted for", {
+  aevent <- tibble::tribble(
+    ~STUDYID, ~USUBJID,     ~LBSTDTC, ~PARAMCD, ~PARAM,
+    "1001",        "1", "2023-01-01",    "TST", "TEST",
+    "1001",        "2", "2023-01-01",    "TST", "TEST",
+    "1001",        "3", "2023-01-01",    "TST", "TEST"
+  )
+
+  pr <- tibble::tribble(
+    ~STUDYID, ~USUBJID, ~PRDECOD,     ~PRSTDTC,
+    "1001",        "2",    "ACS", "2021-12-25",
+    "1001",        "3",    "ACS", "2022-12-25",
+  )
+  expected_output <- tibble::tribble(
+    ~STUDYID, ~USUBJID,     ~LBSTDTC,   ~PARAMCD,                      ~PARAM,                         ~ADT, ~AVALC, # nolint
+    "1001",        "1", "2023-01-01",      "TST",                      "TEST",                           NA,     NA, # nolint
+    "1001",        "2", "2023-01-01",      "TST",                      "TEST",                           NA,     NA, # nolint
+    "1001",        "3", "2023-01-01",      "TST",                      "TEST",                           NA,     NA, # nolint
+    "1001",        "2",           NA, "FIRSTACT", "First Anti-Cancer Therapy", lubridate::ymd("2021-12-25"),  "ACS", # nolint
+    "1001",        "3",           NA, "FIRSTACT", "First Anti-Cancer Therapy", lubridate::ymd("2022-12-25"),  "ACS" # nolint
+  )
+  actual_output <- derive_param_extreme_record(
+    dataset = aevent,
+    sources = list(
+      records_source(
+        dataset_name = "pr",
+        filter = PRDECOD == "ACS",
+        new_vars = exprs(
+          ADT = convert_dtc_to_dt(PRSTDTC),
+          AVALC = PRDECOD
+        )
+      )
+    ),
+    source_datasets = list(pr = pr),
+    by_vars = exprs(STUDYID, USUBJID),
+    order = exprs(ADT),
+    mode = "first",
+    set_values_to = exprs(
+      PARAMCD = "FIRSTACT",
+      PARAM = "First Anti-Cancer Therapy"
+    )
+  )
+
+  expect_dfs_equal(expected_output, actual_output, keys = c("USUBJID", "PARAMCD", "PARAM", "ADT", "AVALC")) # nolint
 })
