99b9c14676d111ee597419f21ba846327fae79b2
DeclanHodges <113363343+DeclanHodges@users.noreply.github.com>
2023-05-16 10:39:55 +0100

    Add tibble package reference to tribble() call in tests/testthat/test-filter_exist.R

--- tests/testthat/test-filter_exist.R
+++ tests/testthat/test-filter_exist.R
@@ -9,7 +9,7 @@ test_that("filter_exist Test 1: filter_exist() works as expected", {
     "01-701-1444", 63, "M"
   )
 
-  input_dataset_add <- tribble(
+  input_dataset_add <- tibble::tribble(
     ~USUBJID, ~AEDECOD, ~AESTDTC,
     "01-701-1015", "DIARRHOEA", "2014-01-09",
     "01-701-1034", "APPLICATION SITE PRURITUS", "2014-08-27",
@@ -34,7 +34,7 @@ test_that("filter_exist Test 1: filter_exist() works as expected", {
 # filter_not_exist ----
 ## Test 2: filter_not_exist() works as expected ----
 test_that("filter_not_exist Test 2: filter_not_exist() works as expected", {
-  input_dataset <- tribble(
+  input_dataset <- tibble::tribble(
     ~USUBJID, ~AGE, ~SEX,
     "01-701-1015", 63, "F",
     "01-701-1034", 77, "F",
@@ -42,7 +42,7 @@ test_that("filter_not_exist Test 2: filter_not_exist() works as expected", {
     "01-701-1444", 63, "M"
   )
 
-  input_dataset_add <- tribble(
+  input_dataset_add <- tibble::tribble(
     ~USUBJID, ~AEDECOD, ~AESTDTC,
     "01-701-1015", "DIARRHOEA", "2014-01-09",
     "01-701-1034", "APPLICATION SITE PRURITUS", "2014-08-27",
