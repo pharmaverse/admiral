3cf8ec70c40fd456a2453123cff05e22f1e885b6
Declan Hodges <declanhodges2@hotmail.com>
2023-05-23 10:39:00 +0000

    1699_create_filter_exist@devel: Tribble code styling.

--- tests/testthat/test-filter_exist.R
+++ tests/testthat/test-filter_exist.R
@@ -2,19 +2,19 @@
 ## Test 1: filter_exist() works as expected ----
 test_that("filter_exist Test 1: filter_exist() works as expected", {
   input_dataset <- tibble::tribble(
-    ~USUBJID, ~AGE, ~SEX,
-    "01-701-1015", 63, "F",
-    "01-701-1034", 77, "F",
-    "01-701-1115", 84, "M",
-    "01-701-1444", 63, "M"
+    ~USUBJID,     ~AGE, ~SEX,
+    "01-701-1015",  63,  "F",
+    "01-701-1034",  77,  "F",
+    "01-701-1115",  84,  "M",
+    "01-701-1444",  63,  "M"
   )
 
   input_dataset_add <- tibble::tribble(
-    ~USUBJID, ~AEDECOD, ~AESTDTC,
-    "01-701-1015", "DIARRHOEA", "2014-01-09",
+    ~USUBJID,                         ~AEDECOD,     ~AESTDTC,
+    "01-701-1015",                 "DIARRHOEA", "2014-01-09",
     "01-701-1034", "APPLICATION SITE PRURITUS", "2014-08-27",
-    "01-701-1034", "FATIGUE", "2014-11-02",
-    "01-701-1115", "FATIGUE", "2013-01-14"
+    "01-701-1034",                   "FATIGUE", "2014-11-02",
+    "01-701-1115",                   "FATIGUE", "2013-01-14"
   )
 
   expected_output <- input_dataset %>%
@@ -35,19 +35,19 @@ test_that("filter_exist Test 1: filter_exist() works as expected", {
 ## Test 2: filter_not_exist() works as expected ----
 test_that("filter_not_exist Test 2: filter_not_exist() works as expected", {
   input_dataset <- tibble::tribble(
-    ~USUBJID, ~AGE, ~SEX,
-    "01-701-1015", 63, "F",
-    "01-701-1034", 77, "F",
-    "01-701-1115", 84, "M",
-    "01-701-1444", 63, "M"
+    ~USUBJID,     ~AGE, ~SEX,
+    "01-701-1015",  63,  "F",
+    "01-701-1034",  77,  "F",
+    "01-701-1115",  84,  "M",
+    "01-701-1444",  63,  "M"
   )
 
   input_dataset_add <- tibble::tribble(
-    ~USUBJID, ~AEDECOD, ~AESTDTC,
-    "01-701-1015", "DIARRHOEA", "2014-01-09",
+    ~USUBJID,                         ~AEDECOD,     ~AESTDTC,
+    "01-701-1015",                 "DIARRHOEA", "2014-01-09",
     "01-701-1034", "APPLICATION SITE PRURITUS", "2014-08-27",
-    "01-701-1034", "FATIGUE", "2014-11-02",
-    "01-701-1115", "FATIGUE", "2013-01-14"
+    "01-701-1034",                   "FATIGUE", "2014-11-02",
+    "01-701-1115",                   "FATIGUE", "2013-01-14"
   )
 
   expected_output <- input_dataset %>%
