2f7691ab00d425d6fa7d1341dcc99604eb3218c6
nicholas.eugenio <nicholas.eugenio@businesspartner.roche.com>
2023-07-04 23:37:52 +0000

    #1839 onboarding dummy function created

---
+++ R/my_first_fcn.R
@@ -0,0 +1,33 @@
+#' Dummy Function for Onboarding
+#'
+#' @description
+#' Returns "Welcome to the admiral family!"
+#'
+#' @details
+#' This is a function that returns "Welcome to the admiral family!"
+#' independent of its inputs.
+#'
+#' @param anything Element in any format the user wants.
+#'
+#' @return A "Welcome to the admiral family!" string.
+#'
+#' @family der_gen
+#'
+#' @keywords der_gen
+#'
+#' @export
+#'
+#' @examples
+#' welcome_fun(anything = "a")
+#' welcome_fun(anything = 2)
+#' welcome_fun(anything = NULL)
+#' welcome_fun(anything = data.frame(1:10, 2:11))
+welcome_fun <- function(anything = NULL){
+
+  if(is.null(anything)){
+
+    cat("Welcome to the admiral family!")
+
+  } else {cat("Welcome to the admiral family!")}
+
+}
---
+++ tests/testthat/test-my_first_fcn.R
@@ -0,0 +1,41 @@
+# anything param types ----
+## Test 1: param is NULL ----
+
+test_that("my_new_func Test 1: NULL works as parameter", {
+
+  input <- NULL
+
+  expected_output <- "Welcome to the admiral family!"
+
+  expect_dfs_equal(welcome_fun(input), expected_output)
+
+})
+
+
+## Test 2: param is double ----
+
+test_that("my_new_func Test 2: double works as parameter", {
+
+  input <- 3
+
+  expected_output <- "Welcome to the admiral family!"
+
+  expect_dfs_equal(welcome_fun(input), expected_output)
+
+})
+
+## Test 3: param is a dataframe ----
+
+test_that("my_new_func Test 3: dataframe works as parameter", {
+
+  input <- tibble::tribble(
+    ~x,  ~y,
+    "a", 1:3,
+    "b", 4:6
+  )
+
+  expected_output <- "Welcome to the admiral family!"
+
+  expect_dfs_equal(welcome_fun(input), expected_output)
+
+})
