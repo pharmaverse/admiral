cb56a8ea0bb076e9c6b38baa8882cef1ace5f7f4
asha-gsk <134052893+ashachakma@users.noreply.github.com>
2023-07-12 15:17:25 +0530

    Delete my_first_fcn.R

--- R/my_first_fcn.R
+++
@@ -1,6 +0,0 @@
-#' Created dummy function
-#'
-#' Outputs "Welcome to the admiral family!"
-#'
-#' @details returns a welcome greeting
-hello_admiral <- function() {{ message("Welcome to the admiral family!") }}
