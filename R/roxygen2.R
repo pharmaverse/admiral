c4ad78ab30b18e94ce54392ca2f6b9480326134b
Zelos Zhu <zdz2101@github.com>
2023-08-18 18:45:35 +0000

    feat: #2034 show how this can standardize our sub-descriptions

---
+++ R/roxygen2.R
@@ -0,0 +1,9 @@
+# THIS FILE INCLUDES FUNCTION THAT ARE RE-USED THROUGHOUT
+# roxygen2 COMMENTS TO GENERATE DOCUMENTATION TEXT
+
+roxygen_param_dataset <- function(expected_vars_args) {
+  paste0("Input DatasetThe variables specified by the ",
+         enumerate(expected_vars_args),
+         "argument(s) to be expected.")
+
+}
