b502ebb700068d79bd905e9eab8209f65e4944dc
Daphne Grasselly <daphne.grasselly@roche.com>
2023-09-04 17:41:03 +0200

    tmp fix for templates

--- inst/templates/ad_adeg.R
+++ inst/templates/ad_adeg.R
@@ -6,7 +6,7 @@
 #
 # Input: adsl, eg
 library(admiral)
-library(pharmaversesdtm) # Contains example datasets from the CDISC pilot project
+library(admiral.test) # Contains example datasets from the CDISC pilot project
 library(dplyr)
 library(lubridate)
 library(stringr)
--- inst/templates/ad_adpp.R
+++ inst/templates/ad_adpp.R
@@ -6,7 +6,7 @@
 #
 # Input: pp, adsl
 library(admiral)
-library(pharmaversesdtm) # Contains example datasets from the CDISC pilot project
+library(admiral.test) # Contains example datasets from the CDISC pilot project
 library(dplyr)
 library(lubridate)
 library(stringr)
