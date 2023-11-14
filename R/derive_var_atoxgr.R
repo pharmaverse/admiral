8a18e0e7332fb7d7d9fcb34a849b5c82829771eb
Gordon Miller <80953007+millerg23@users.noreply.github.com>
2023-11-14 13:34:49 +0000

    Closes #2134 add general option signif_digits (#2212)
    * 2134 add general option signif_digits
    
    * 2134 fix SPELLING and STYLER
    
    * 2134 address QC comments and update NEWS file
    
    * 2134 fix LINTR and STYLER
    
    * Apply suggestions from code review
    
    Minor updates from QC.
    
    Co-authored-by: Ben Straub <ben.x.straub@gsk.com>
    Co-authored-by: StefanThoma <40463122+StefanThoma@users.noreply.github.com>
    
    * Update vignettes/lab_grading.Rmd
    
    ---------
    
    Co-authored-by: Ben Straub <ben.x.straub@gsk.com>
    Co-authored-by: StefanThoma <40463122+StefanThoma@users.noreply.github.com>

--- R/derive_var_atoxgr.R
+++ R/derive_var_atoxgr.R
@@ -55,6 +55,8 @@
 #' value.
 #'
 #'   Significant digits used to avoid floating point discrepancies when comparing numeric values.
+#'   See blog: [How admiral handles floating
+#'   points](https://pharmaverse.github.io/blog/posts/2023-10-30_floating_point/floating_point.html)
 #'
 #' @details
 #' `new_var` is derived with values NA, "0", "1", "2", "3", "4", where "4" is the most
@@ -117,13 +119,16 @@ derive_var_atoxgr_dir <- function(dataset,
                                   meta_criteria,
                                   criteria_direction,
                                   get_unit_expr,
-                                  signif_dig = 15) {
+                                  signif_dig = get_admiral_option("signif_digits")) {
   new_var <- assert_symbol(enexpr(new_var))
   tox_description_var <- assert_symbol(enexpr(tox_description_var))
   get_unit_expr <- assert_expr(enexpr(get_unit_expr))
 
   # check input parameter has correct value
   assert_character_scalar(criteria_direction, values = c("L", "H"))
+
+  # check input parameter holding significant digits has correct value
+  assert_integer_scalar(signif_dig, subset = "positive")
 
   # Check Grade description variable exists on input data set
   assert_data_frame(dataset, required_vars = exprs(!!tox_description_var))
