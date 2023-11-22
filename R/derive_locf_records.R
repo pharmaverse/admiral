ac814d817f9f019997d208c744b0a17806cf17cb
StefanThoma <40463122+StefanThoma@users.noreply.github.com>
2023-11-22 15:05:47 +0100

    first few functions

--- R/derive_locf_records.R
+++ R/derive_locf_records.R
@@ -21,6 +21,8 @@
 #'   in the input dataset or for which `analysis_var` is `NA` for the corresponding observation
 #'   in the input dataset.
 #'
+#'   `r roxygen_param_by_vars(additional_dataset = "dataset_ref")`
+#'
 #' @param analysis_var Analysis variable.
 #'
 #'   *Default*: `AVAL`
--- R/roxygen2.R
+++ R/roxygen2.R
@@ -14,3 +14,43 @@ roxygen_param_dataset <- function(expected_vars = NULL) {
   }
   return(dataset_text)
 }
+
+
+# function to properly document the by_vars argument including specifying the use of exprs():
+
+roxygen_param_by_vars <- function(type = "group", additional_dataset = NULL) {
+
+  if (type == "group") {
+    by_vars_text <- "A list of quoted expressions specifying variables to group by. \n \n"
+  }
+
+
+  by_vars_text <- paste0(
+    by_vars_text,
+    # "The variables must be a unique key of the selected observations. \n \n",
+    #
+
+    "The expressions should be quoted using the `exprs()` function from ",
+    "the rlang package.",
+    "e.g. exprs(USUBJID, VISIT) \n \n"
+
+  )
+
+  if (!is.null(additional_dataset)) {
+    by_vars_text <- paste0(
+      by_vars_text,
+      "The by variables must be a unique key of the selected observations in ",
+      enumerate(additional_dataset), ". \n \n",
+      "Variables from ", enumerate(additional_dataset),
+      " can be renamed by naming the element, I.e. \n",
+      "by_vars = exprs(<name in input dataset› = ‹name in additional dataset>), similar to the dplyr joins.\n \n"
+    )
+  }
+
+  by_vars_text <- paste0(
+    by_vars_text,
+    "*Permitted Values*: list of variables created by exprs()"
+    )
+
+  return(by_vars_text)
+}
