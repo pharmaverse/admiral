363307cc5ba6340d05d17cda5ff2e61d933b78e5
Azra Khan <azra.8.khan@gsk.com>
2023-11-22 15:07:48 +0530

    #1839 Dummy function created

---
+++ R/my_first_fcn.R
@@ -0,0 +1,33 @@
+#' Drive Extension Example
+#'
+#' Says Welcome to admiral
+#'
+#' @param hw TRUE or FALSE
+#'
+#' @author Open Source
+#'
+#' @details Created function to output "Welciome to admiral family"
+#'
+#' @return Welcome Message
+#'
+#' @family der_adxx
+#'
+#' @keywords der_adxx
+#'
+#' @export
+#'
+#' @examples
+#' wlcm_admiral(hw = FALSE)
+
+wlcm_admiral <- function(hw = TRUE) {
+  if(hw) {
+    message("Welcome to the admiral family!")
+  }  else{
+    message("Welcome to the admiral family!")
+  }
+}
+
+
+
+
+
---
+++ tests/testthat/test-my_first_fcn.R
@@ -0,0 +1,19 @@
+test_that("hello admiral greets without hw", {
+  expect_message(
+    wlcm_admiral(),
+    "^Welcome to the admiral family! \\n"
+  )
+})
+test_that("hello admiral greets with hw", {
+  expect_message(
+    wlcm_admiral(TRUE),
+    "^Welcome to the admiral family! \\n"
+  )
+})
+
+
+
+
+
+
+
