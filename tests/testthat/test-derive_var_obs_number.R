207ee6f5e5cee685859aec34befd0fa6f9b868bc
Enki Wang <98389771+ynsec37@users.noreply.github.com>
2023-12-01 02:26:29 +0000

    Styler the file
    * removed the `L`
    * format code to solve the lintr error

--- tests/testthat/test-derive_var_obs_number.R
+++ tests/testthat/test-derive_var_obs_number.R
@@ -2,32 +2,32 @@
 test_that("derive_var_obs_number Test 1: create default new variable ASEQ", {
   input <- tibble::tribble(
     ~STUDYID, ~USUBJID, ~VSTESTCD, ~VISITNUM, ~VSTPTNUM,
-    "TEST01",  "PAT01",   "DIABP",        1L,        1L,
-    "TEST01",  "PAT01",   "PULSE",        1L,        1L,
-    "TEST01",  "PAT01",   "PULSE",        1L,        2L,
-    "TEST01",  "PAT01",   "PULSE",        2L,        1L,
-    "TEST01",  "PAT01",   "PULSE",        2L,        2L,
-    "TEST01",  "PAT01",   "SYSBP",        1L,        1L,
-    "TEST01",  "PAT01",   "SYSBP",        1L,        2L,
-    "TEST01",  "PAT01",   "SYSBP",        2L,        1L,
-    "TEST01",  "PAT02",   "DIABP",        1L,        1L,
-    "TEST01",  "PAT02",   "DIABP",        2L,        1L,
-    "TEST01",  "PAT02",   "DIABP",        2L,        2L
+    "TEST01",  "PAT01",   "DIABP",        1,        1,
+    "TEST01",  "PAT01",   "PULSE",        1,        1,
+    "TEST01",  "PAT01",   "PULSE",        1,        2,
+    "TEST01",  "PAT01",   "PULSE",        2,        1,
+    "TEST01",  "PAT01",   "PULSE",        2,        2,
+    "TEST01",  "PAT01",   "SYSBP",        1,        1,
+    "TEST01",  "PAT01",   "SYSBP",        1,        2,
+    "TEST01",  "PAT01",   "SYSBP",        2,        1,
+    "TEST01",  "PAT02",   "DIABP",        1,        1,
+    "TEST01",  "PAT02",   "DIABP",        2,        1,
+    "TEST01",  "PAT02",   "DIABP",        2,        2
   )
 
   expected_output <- tibble::tribble(
     ~STUDYID, ~USUBJID, ~VSTESTCD, ~VISITNUM, ~VSTPTNUM, ~ASEQ,
-    "TEST01",  "PAT01",   "DIABP",        1L,        1L,    1L,
-    "TEST01",  "PAT01",   "PULSE",        1L,        1L,    1L,
-    "TEST01",  "PAT01",   "PULSE",        1L,        2L,    2L,
-    "TEST01",  "PAT01",   "PULSE",        2L,        1L,    3L,
-    "TEST01",  "PAT01",   "PULSE",        2L,        2L,    4L,
-    "TEST01",  "PAT01",   "SYSBP",        1L,        1L,    1L,
-    "TEST01",  "PAT01",   "SYSBP",        1L,        2L,    2L,
-    "TEST01",  "PAT01",   "SYSBP",        2L,        1L,    3L,
-    "TEST01",  "PAT02",   "DIABP",        1L,        1L,    1L,
-    "TEST01",  "PAT02",   "DIABP",        2L,        1L,    2L,
-    "TEST01",  "PAT02",   "DIABP",        2L,        2L,    3L
+    "TEST01",  "PAT01",   "DIABP",        1,        1,    1L,
+    "TEST01",  "PAT01",   "PULSE",        1,        1,    1L,
+    "TEST01",  "PAT01",   "PULSE",        1,        2,    2L,
+    "TEST01",  "PAT01",   "PULSE",        2,        1,    3L,
+    "TEST01",  "PAT01",   "PULSE",        2,        2,    4L,
+    "TEST01",  "PAT01",   "SYSBP",        1,        1,    1L,
+    "TEST01",  "PAT01",   "SYSBP",        1,        2,    2L,
+    "TEST01",  "PAT01",   "SYSBP",        2,        1,    3L,
+    "TEST01",  "PAT02",   "DIABP",        1,        1,    1L,
+    "TEST01",  "PAT02",   "DIABP",        2,        1,    2L,
+    "TEST01",  "PAT02",   "DIABP",        2,        2,    3L
   )
 
   actual_output <-  derive_var_obs_number(
@@ -44,43 +44,43 @@ test_that("derive_var_obs_number Test 1: create default new variable ASEQ", {
 })
 
 ## Test 2: sorting missing value  is smallest ----
-test_that("derive_var_obs_number Test 2: sorting missing value  is smallest", {
+test_that("derive_var_obs_number Test 2: sorting missing value is smallest", {
   input <- tibble::tribble(
     ~STUDYID, ~USUBJID, ~VSTESTCD, ~VISITNUM, ~VSTPTNUM,
-    "TEST01",  "PAT01",   "DIABP",        1L,        1L,
-    "TEST01",  "PAT01",   "DIABP",        1L,        NA,
-    "TEST01",  "PAT01",   "PULSE",        1L,        1L,
-    "TEST01",  "PAT01",   "PULSE",        1L,        2L,
-    "TEST01",  "PAT01",   "PULSE",        2L,        1L,
-    "TEST01",  "PAT01",   "PULSE",        2L,        2L,
-    "TEST01",  "PAT01",   "SYSBP",        1L,        1L,
-    "TEST01",  "PAT01",   "SYSBP",        1L,        2L,
-    "TEST01",  "PAT01",   "SYSBP",        2L,        1L,
-    "TEST01",  "PAT02",   "DIABP",        1L,        1L,
-    "TEST01",  "PAT02",   "DIABP",        2L,        1L,
-    "TEST01",  "PAT02",   "DIABP",        2L,        2L
+    "TEST01",  "PAT01",   "DIABP",        1,        1,
+    "TEST01",  "PAT01",   "DIABP",        1,        NA,
+    "TEST01",  "PAT01",   "PULSE",        1,        1,
+    "TEST01",  "PAT01",   "PULSE",        1,        2,
+    "TEST01",  "PAT01",   "PULSE",        2,        1,
+    "TEST01",  "PAT01",   "PULSE",        2,        2,
+    "TEST01",  "PAT01",   "SYSBP",        1,        1,
+    "TEST01",  "PAT01",   "SYSBP",        1,        2,
+    "TEST01",  "PAT01",   "SYSBP",        2,        1,
+    "TEST01",  "PAT02",   "DIABP",        1,        1,
+    "TEST01",  "PAT02",   "DIABP",        2,        1,
+    "TEST01",  "PAT02",   "DIABP",        2,        2
   )
 
   expected_output <- tibble::tribble(
     ~STUDYID, ~USUBJID, ~VSTESTCD, ~VISITNUM, ~VSTPTNUM, ~ASEQ,
-    "TEST01",  "PAT01",   "DIABP",        1L,        NA,    1L,
-    "TEST01",  "PAT01",   "DIABP",        1L,        1L,    2L,
-    "TEST01",  "PAT01",   "PULSE",        1L,        1L,    1L,
-    "TEST01",  "PAT01",   "PULSE",        1L,        2L,    2L,
-    "TEST01",  "PAT01",   "PULSE",        2L,        1L,    3L,
-    "TEST01",  "PAT01",   "PULSE",        2L,        2L,    4L,
-    "TEST01",  "PAT01",   "SYSBP",        1L,        1L,    1L,
-    "TEST01",  "PAT01",   "SYSBP",        1L,        2L,    2L,
-    "TEST01",  "PAT01",   "SYSBP",        2L,        1L,    3L,
-    "TEST01",  "PAT02",   "DIABP",        1L,        1L,    1L,
-    "TEST01",  "PAT02",   "DIABP",        2L,        1L,    2L,
-    "TEST01",  "PAT02",   "DIABP",        2L,        2L,    3L
+    "TEST01",  "PAT01",   "DIABP",        1,        NA,   1L,
+    "TEST01",  "PAT01",   "DIABP",        1,        1,    2L,
+    "TEST01",  "PAT01",   "PULSE",        1,        1,    1L,
+    "TEST01",  "PAT01",   "PULSE",        1,        2,    2L,
+    "TEST01",  "PAT01",   "PULSE",        2,        1,    3L,
+    "TEST01",  "PAT01",   "PULSE",        2,        2,    4L,
+    "TEST01",  "PAT01",   "SYSBP",        1,        1,    1L,
+    "TEST01",  "PAT01",   "SYSBP",        1,        2,    2L,
+    "TEST01",  "PAT01",   "SYSBP",        2,        1,    3L,
+    "TEST01",  "PAT02",   "DIABP",        1,        1,    1L,
+    "TEST01",  "PAT02",   "DIABP",        2,        1,    2L,
+    "TEST01",  "PAT02",   "DIABP",        2,        2,    3L
   )
 
   actual_output <-  derive_var_obs_number(
     input,
     by_vars = exprs(USUBJID, VSTESTCD),
-    order = exprs(VISITNUM,!is.na(VSTPTNUM), VSTPTNUM)
+    order = exprs(VISITNUM, !is.na(VSTPTNUM), VSTPTNUM)
   )
 
   expect_dfs_equal(
@@ -95,32 +95,32 @@ test_that("derive_var_obs_number Test 2: sorting missing value  is smallest", {
 test_that("derive_var_obs_number Test 3: create customized new variable ASEQ1", {
   input <- tibble::tribble(
     ~STUDYID, ~USUBJID, ~VSTESTCD, ~VISITNUM, ~VSTPTNUM,
-    "TEST01",  "PAT01",   "DIABP",        1L,        1L,
-    "TEST01",  "PAT01",   "PULSE",        1L,        1L,
-    "TEST01",  "PAT01",   "PULSE",        1L,        2L,
-    "TEST01",  "PAT01",   "PULSE",        2L,        1L,
-    "TEST01",  "PAT01",   "PULSE",        2L,        2L,
-    "TEST01",  "PAT01",   "SYSBP",        1L,        1L,
-    "TEST01",  "PAT01",   "SYSBP",        1L,        2L,
-    "TEST01",  "PAT01",   "SYSBP",        2L,        1L,
-    "TEST01",  "PAT02",   "DIABP",        1L,        1L,
-    "TEST01",  "PAT02",   "DIABP",        2L,        1L,
-    "TEST01",  "PAT02",   "DIABP",        2L,        2L
+    "TEST01",  "PAT01",   "DIABP",        1,        1,
+    "TEST01",  "PAT01",   "PULSE",        1,        1,
+    "TEST01",  "PAT01",   "PULSE",        1,        2,
+    "TEST01",  "PAT01",   "PULSE",        2,        1,
+    "TEST01",  "PAT01",   "PULSE",        2,        2,
+    "TEST01",  "PAT01",   "SYSBP",        1,        1,
+    "TEST01",  "PAT01",   "SYSBP",        1,        2,
+    "TEST01",  "PAT01",   "SYSBP",        2,        1,
+    "TEST01",  "PAT02",   "DIABP",        1,        1,
+    "TEST01",  "PAT02",   "DIABP",        2,        1,
+    "TEST01",  "PAT02",   "DIABP",        2,        2
   )
 
   expected_output <- tibble::tribble(
     ~STUDYID, ~USUBJID, ~VSTESTCD, ~VISITNUM, ~VSTPTNUM, ~ASEQ1,
-    "TEST01",  "PAT01",   "DIABP",        1L,        1L,    1L,
-    "TEST01",  "PAT01",   "PULSE",        1L,        1L,    1L,
-    "TEST01",  "PAT01",   "PULSE",        1L,        2L,    2L,
-    "TEST01",  "PAT01",   "PULSE",        2L,        1L,    3L,
-    "TEST01",  "PAT01",   "PULSE",        2L,        2L,    4L,
-    "TEST01",  "PAT01",   "SYSBP",        1L,        1L,    1L,
-    "TEST01",  "PAT01",   "SYSBP",        1L,        2L,    2L,
-    "TEST01",  "PAT01",   "SYSBP",        2L,        1L,    3L,
-    "TEST01",  "PAT02",   "DIABP",        1L,        1L,    1L,
-    "TEST01",  "PAT02",   "DIABP",        2L,        1L,    2L,
-    "TEST01",  "PAT02",   "DIABP",        2L,        2L,    3L
+    "TEST01",  "PAT01",   "DIABP",        1,        1,    1L,
+    "TEST01",  "PAT01",   "PULSE",        1,        1,    1L,
+    "TEST01",  "PAT01",   "PULSE",        1,        2,    2L,
+    "TEST01",  "PAT01",   "PULSE",        2,        1,    3L,
+    "TEST01",  "PAT01",   "PULSE",        2,        2,    4L,
+    "TEST01",  "PAT01",   "SYSBP",        1,        1,    1L,
+    "TEST01",  "PAT01",   "SYSBP",        1,        2,    2L,
+    "TEST01",  "PAT01",   "SYSBP",        2,        1,    3L,
+    "TEST01",  "PAT02",   "DIABP",        1,        1,    1L,
+    "TEST01",  "PAT02",   "DIABP",        2,        1,    2L,
+    "TEST01",  "PAT02",   "DIABP",        2,        2,    3L
   )
 
   actual_output <-  derive_var_obs_number(input,
@@ -139,10 +139,10 @@ test_that("derive_var_obs_number Test 3: create customized new variable ASEQ1", 
 test_that("derive_var_obs_number Test 4: expected condition for `check_type` ", {
   input <- tibble::tribble(
     ~STUDYID, ~USUBJID, ~VSTESTCD, ~VISITNUM, ~VSTPTNUM,
-    "TEST01",  "PAT01",   "DIABP",        1L,        1L,
-    "TEST01",  "PAT01",   "DIABP",        1L,        1L,
-    "TEST01",  "PAT01",   "DIABP",        2L,        1L,
-    "TEST01",  "PAT01",   "DIABP",        2L,        2L
+    "TEST01",  "PAT01",   "DIABP",        1,        1,
+    "TEST01",  "PAT01",   "DIABP",        1,        1,
+    "TEST01",  "PAT01",   "DIABP",        2,        1,
+    "TEST01",  "PAT01",   "DIABP",        2,        2
   )
 
   expect_warning(
