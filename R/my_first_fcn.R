8515ac49ba07b4fad5017613adc5b1bd874aad89
Lina2689 <lina.patil@cytel.com>
2024-02-08 10:52:23 +0000

    #1839 Created a dummy function my_first_fcn for an on boarding process.

--- R/my_first_fcn.R
+++ R/my_first_fcn.R
@@ -2,7 +2,7 @@
 #'
 #' @author Lina Patil
 #' @param input - required input information in any format
-#' @details create a function to output a text message Welcome to the admiral family!
+#' @details create a function to output a text message 'Welcome to the admiral family!'
 #' @return a welcome message
 #' @export
 #' @examples
