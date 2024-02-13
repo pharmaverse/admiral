0fb1c558cc05a556dac3dad7b4cb6591935d007a
Lina2689 <lina.patil@cytel.com>
2024-02-13 10:07:19 +0000

    added keyword and family to function header.

--- R/my_first_fcn.R
+++ R/my_first_fcn.R
@@ -4,12 +4,13 @@
 #' @param input - required input information in any format
 #' @details create a function to output a text message 'Welcome to the admiral family!'
 #' @return a welcome message
+#' @keywords der_gen
+#' @family der_gen
 #' @export
 #' @examples
 #' my_first_fcn("Y")
 #' my_first_fcn(NULL)
-
-my_first_fcn <- function(input){
+my_first_fcn <- function(input) {
   if (!is.null(input)) {
     print("Welcome to the admiral family!")
   } else {
