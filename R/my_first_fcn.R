4e1a538b4ca251899f86756b27cdce9358c7e1b8
Fanny-Gautier <fanny.gautier@cytel.com>
2024-02-13 11:10:15 +0000

    my_first_fcn: remove the space

--- R/my_first_fcn.R
+++ R/my_first_fcn.R
@@ -12,7 +12,6 @@
 #  keywords  der_message is not a known tag, so not included for this function
 #  family    der_message is not a known tag, so not included for this function
 #' @examples my_first_fcn(first_time = FALSE)
-
 my_first_fcn <- function(first_time = TRUE) {
   # Check that first_time is Logical
   expect(is.logical(first_time), "Error: first_time must be logical")
