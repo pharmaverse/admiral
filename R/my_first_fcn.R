2de635332205e6ee82fac7ddcc33098188f7e41c
Fanny-Gautier <fanny.gautier@cytel.com>
2024-02-13 15:41:59 +0000

    my_first_fcn: update header and replace expect by stopifnot

--- R/my_first_fcn.R
+++ R/my_first_fcn.R
@@ -1,22 +1,16 @@
 #' Derive on boarding dummy function
 #'
-#' @author   Fanny Gautier
-#'
-# Mandatory fields in header:
 #' @param    first_time TRUE or FALSE
 #' @details  outputs "Welcome to the admiral family!".
 #' @return   a Welcome message
 #' @export
 #' @keywords utils_print
 #' @family   utils_print
-#  keywords  der_message is not a known tag, so not included for this function
-#  family    der_message is not a known tag, so not included for this function
 #' @examples
-#' library(testthat)
 #' my_first_fcn(first_time = FALSE)
 my_first_fcn <- function(first_time = TRUE) {
   # Check that first_time is Logical
-  expect(is.logical(first_time), "Error: first_time must be logical")
+  stopifnot(is.logical(first_time))
 
   if (first_time) {
     message("Welcome to the admiral family!")
