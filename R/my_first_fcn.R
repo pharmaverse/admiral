92a02310ff065e2ab4c67c9fa5a3b97dccbb6d7a
Fanny-Gautier <fanny.gautier@cytel.com>
2024-02-13 11:15:02 +0000

    my_first_fcn : add library(testthat)

--- R/my_first_fcn.R
+++ R/my_first_fcn.R
@@ -12,6 +12,7 @@
 #  keywords  der_message is not a known tag, so not included for this function
 #  family    der_message is not a known tag, so not included for this function
 #' @examples my_first_fcn(first_time = FALSE)
+library(testthat)
 my_first_fcn <- function(first_time = TRUE) {
   # Check that first_time is Logical
   expect(is.logical(first_time), "Error: first_time must be logical")
