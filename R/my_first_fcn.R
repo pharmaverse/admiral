9a94879c61e50f1f5972dc0aa33df3e065cf3be8
Lina2689 <lina.patil@cytel.com>
2024-02-14 04:54:05 +0000

    Updated to remove author

--- R/my_first_fcn.R
+++ R/my_first_fcn.R
@@ -1,6 +1,5 @@
 #' Dummy function for on boarding
 #'
-#' @author Lina Patil
 #' @param input - required input information in any format
 #' @details create a function to output a text message 'Welcome to the admiral family!'
 #' @return a welcome message
