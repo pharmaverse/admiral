e8d110085a142f3782972052302c86706ac182e0
Nick Giangreco <nick.giangreco@gmail.com>
2024-02-27 21:24:20 -0500

    addedd family and keywords to my first function

--- R/my_first_function.R
+++ R/my_first_function.R
@@ -3,6 +3,9 @@
 #' @return character: "Welcome to the admiral family!"
 #' @export
 #'
+#' @family my_first_function
+#' @keywords my_first_function
+#'
 #' @examples
 #' my_first_function()
 my_first_function <- function() {
