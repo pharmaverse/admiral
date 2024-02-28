e644facefd789c0ca1c3aec992e1e1e7c92a7249
Nick Giangreco <nick.giangreco@gmail.com>
2024-02-27 21:11:29 -0500

    create my first function

---
+++ R/my_first_function.R
@@ -0,0 +1,10 @@
+#' My First Function
+#'
+#' @return character: "Welcome to the admiral family!"
+#' @export
+#'
+#' @examples
+#' my_first_function()
+my_first_function <- function(){
+  return("Welcome to the admiral family!")
+}
