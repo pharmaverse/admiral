7efa201123bade96eaf21af91ba48f46d8d8ba94
Jasmine Daly <jasmine.dumas@gmail.com>
2024-03-02 22:10:43 -0500

    #1839 onboarding ticket

---
+++ R/my_first_fcn.R
@@ -0,0 +1,10 @@
+#' My first function
+#' Admiral on boarding Ticket
+#' @return a string
+#' @export
+#'
+#' @examples
+#' my_first_fcn()
+my_first_fcn <- function() {
+  print("Welcome to the admiral family!")
+}
