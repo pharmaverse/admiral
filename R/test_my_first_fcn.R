4e9650fd7ed9c3ef2eb965aa519f760e11166d3b
Abeer Mahmoud <abeer.mahmoud@DataClin.local>
2024-03-07 20:18:25 +0200

    # 1839 dummy issue

---
+++ R/test_my_first_fcn.R
@@ -0,0 +1,13 @@
+test_that("hello admiral without hw",{
+  expect_message(
+    my_first_fcn(),
+    "^welcome to the admiral family!\\n"
+  )
+})
+
+test_that("hello admiral with hw",{
+  expect_message(
+    my_first_fcn(TRUE),
+    "^welcome to the admiral family!\\n"
+  )
+})
