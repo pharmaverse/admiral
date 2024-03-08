816d29917281516b1e6bc7639e0e70dc6ec8be1b
Abeer Mahmoud <abeer.mahmoud@DataClin.local>
2024-03-08 10:42:09 +0200

    # 1839 dummy function and test created

---
+++ R/My_first_Fcn_AZ.R
@@ -0,0 +1,10 @@
+#' @desc Prints a welcome message to the Admiral family.
+#' @param hw true or false
+#' @return character vector containing the welcome message.
+my_first_fcn <- function(hw=TRUE) {
+  if (hw) {
+    message("Welcome to the admiral family!")
+  } else {
+    message("Welcome to the admiral family!")
+  }
+}
