2527436dc7b87040f7a75b64bc0a92a6facab43f
Zelos Zhu <zelos.zhu@atorusresearch.com>
2024-03-18 20:23:51 +0000

    get past lintr

--- tests/testthat/test-derive_vars_dt_dtm_utils.R
+++ tests/testthat/test-derive_vars_dt_dtm_utils.R
@@ -1,30 +1,30 @@
 # convert_date_to_dtm ----
-## Test 1: convert_date_to_dtm  Convert a complete -- DTC into a date time object ----
-test_that("convert_date_to_dtm Test 1: convert_date_to_dtm  Convert a complete -- DTC into a date time object", {
+## Test 1: Convert a complete -- DTC into a date time object ----
+test_that("convert_date_to_dtm Test 1: Convert a complete -- DTC into a date time object", {
   expect_equal(
     convert_date_to_dtm("2019-07-18T15:25:52"),
     ymd_hms("2019-07-18T15:25:52")
   )
 })
 
-## Test 2: convert_date_to_dtm  Impute incomplete -- DTC into a date time object ----
-test_that("convert_date_to_dtm Test 2: convert_date_to_dtm  Impute incomplete -- DTC into a date time object", {
+## Test 2: Impute incomplete -- DTC into a date time object ----
+test_that("convert_date_to_dtm Test 2: Impute incomplete -- DTC into a date time object", {
   expect_equal(
     convert_date_to_dtm("2019-07-18", time_imputation = "23:59:59"),
     ymd_hms("2019-07-18T23:59:59")
   )
 })
 
-## Test 3: convert_date_to_dtm  Convert -- DT into a date time object ----
-test_that("convert_date_to_dtm Test 3: convert_date_to_dtm  Convert -- DT into a date time object", {
+## Test 3: Convert -- DT into a date time object ----
+test_that("convert_date_to_dtm Test 3: Convert -- DT into a date time object", {
   expect_equal(
     convert_date_to_dtm(as.Date("2019-07-18"), time_imputation = "23:59:59"),
     ymd_hms("2019-07-18T23:59:59")
   )
 })
 
-## Test 4: convert_date_to_dtm  Keep -- DTM as the original date time object ----
-test_that("convert_date_to_dtm Test 4: convert_date_to_dtm  Keep -- DTM as the original date time object", {
+## Test 4: Keep -- DTM as the original date time object ----
+test_that("convert_date_to_dtm Test 4: Keep -- DTM as the original date time object", {
   expect_equal(
     convert_date_to_dtm(ymd_hms("2019-07-18T15:25:52"), time_imputation = "23:59:59"),
     ymd_hms("2019-07-18T15:25:52")
