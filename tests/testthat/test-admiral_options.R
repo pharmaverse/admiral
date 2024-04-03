0fda1e1d8ad90587ccdae627e5217f78812c1754
Zelos Zhu <zelos.zhu@atorusresearch.com>
2024-04-03 15:52:51 +0000

    bundle error fixes in one go

--- tests/testthat/test-admiral_options.R
+++ tests/testthat/test-admiral_options.R
@@ -13,7 +13,7 @@ test_that("get_admiral_option Test 2: common typo gives error to select availabl
 test_that("get_admiral_option Test 3: non-character argument triggers assertion error", {
   subject_keys <- 1
   expect_error(get_admiral_option(subject_keys),
-    class = "assert_character_scalar"
+               class = "assert_character_scalar"
   )
 })
 
@@ -28,12 +28,11 @@ test_that("set_admiral_options Test 4: set works", {
 test_that("set_admiral_options Test 5: unexpected function input for set gives error", {
   expect_error(
     set_admiral_options(subject_keys = rlang::quos(STUDYID, USUBJID2)),
-    class = "assert_list_of"
+    class = "assert-admiraldev"
   )
   expect_error(
     set_admiral_options(subject_keys = STUDYID),
-    regexp = "`subject_keys` must be a list of symbols,",
-    fixed = TRUE
+    class = "assert-admiraldev"
   )
 })
 set_admiral_options(subject_keys = exprs(STUDYID, USUBJID))
