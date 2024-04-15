f0929d91b93e2600a1ad23926a2315edd83b252f
ProfessorP <pwebster26@gmail.com>
2024-04-15 11:47:31 -0400

    Corrected documentation of the dummy function for submission and generated the .Rd file

--- R/1839_dummy_my_first_fcn.R
+++ R/1839_dummy_my_first_fcn.R
@@ -1,15 +1,35 @@
-#install package dependencies
-if(!require('devtools')){
-   install.packages('devtools')
-}
+#' @name professorp 1839 Dummy Function
+#'
+#' @title professorp 1839 Dummy Function
+#'
+#' @description
+#' This function prints "Welcome to the admiral family!"
+#'
+#' @param x any input for the function
+#'
+#' @details
+#' This function will print the description message regardless of the input or an absence of input
+#' for the 1839 dummy example.
+#'
+#' @return "Welcome to the admiral family!"
+#'
+#' @keywords deprecated, internal
+#'
+#' @family deprecated,internal
+#'
+#' @export
+#'
+#' @examples
+#' if(!require('devtools')){
+#' install.packages('devtools')
+#' }
+#'
+#' devtools::dev_package_deps(dependencies = T)
+#'
+#' my_first_fcn()
 
-#Check that dependencies have been installed
-devtools::dev_package_deps(dependencies = T)
-
-#This is a function created for the admiral pharmaverse package
-# dummy example to print when run
 my_first_fcn <-  function(x){
   print("Welcome to the admiral family!")
+
 }
 
-my_first_fcn()
