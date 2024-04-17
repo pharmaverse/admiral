af235b0f40b23e8fd6a1560034acf4ba4ec20c0b
Daniel Sjoberg <danield.sjoberg@gmail.com>
2024-04-17 11:45:46 -0700

    Update R/compute_scale.R
    Co-authored-by: Gordon Miller <80953007+millerg23@users.noreply.github.com>

--- R/compute_scale.R
+++ R/compute_scale.R
@@ -80,7 +80,7 @@ compute_scale <- function(source,
     cli_abort(
       c("Argument {.arg source_range} is missing with no default
          and {.arg target_range} is not missing.",
-        "i" = "Either both or neither argument should by specified."
+        "i" = "Either both or neither argument should be specified."
       )
     )
   }
