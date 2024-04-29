65f0d0244e9a6cbbb85538808e6c18867f4c2403
Stefan Bundfuss <stefan.bundfuss@roche.com>
2024-04-29 17:30:02 +0000

    #2413 abort_query_data: style files

--- tests/testthat/test-derive_extreme_event.R
+++ tests/testthat/test-derive_extreme_event.R
@@ -619,31 +619,31 @@ test_that("derive_extreme_event Test 8: error if source dataset not available", 
 
   expect_snapshot(
     derive_extreme_event(
-    adhy,
-    by_vars = exprs(USUBJID),
-    events = list(
-      event(
-        dataset_name = "adyh",
-        condition = is.na(CRIT1FL),
-        set_values_to = exprs(AVALC = "N")
+      adhy,
+      by_vars = exprs(USUBJID),
+      events = list(
+        event(
+          dataset_name = "adyh",
+          condition = is.na(CRIT1FL),
+          set_values_to = exprs(AVALC = "N")
+        ),
+        event(
+          condition = CRIT1FL == "Y",
+          mode = "last",
+          set_values_to = exprs(AVALC = "Y")
+        )
       ),
-      event(
-        condition = CRIT1FL == "Y",
-        mode = "last",
-        set_values_to = exprs(AVALC = "Y")
+      source_datasets = list(adhy = adhy),
+      tmp_event_nr_var = event_nr,
+      order = exprs(event_nr, AVISITN),
+      mode = "first",
+      keep_source_vars = exprs(AVISITN),
+      set_values_to = exprs(
+        PARAMCD = "ALK2",
+        PARAM = "ALKPH <= 2 times ULN"
       )
     ),
-    source_datasets = list(adhy = adhy),
-    tmp_event_nr_var = event_nr,
-    order = exprs(event_nr, AVISITN),
-    mode = "first",
-    keep_source_vars = exprs(AVISITN),
-    set_values_to = exprs(
-      PARAMCD = "ALK2",
-      PARAM = "ALKPH <= 2 times ULN"
-    )
-  ),
-  error = TRUE
+    error = TRUE
   )
 })
 
