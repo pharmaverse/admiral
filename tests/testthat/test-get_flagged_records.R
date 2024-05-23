03f8b27377dbf97ebdeebb0a53a6e89019358164
Iain McCay <iain.x.mccay@gsk.com>
2024-05-23 10:59:08 -0700

    Added NA_character_ tests
    Pushed from Domino: https://domino-uat.gsk.com/workspace/im631011/admiral?executionId=66339539d022324fe623e848

--- tests/testthat/test-get_flagged_records.R
+++ tests/testthat/test-get_flagged_records.R
@@ -26,7 +26,7 @@ test_that("get_flagged_records Test 1: generate existence flag", {
 })
 
 ## Test 2: test filter argument ----
-test_that("get_flagged_records Test 1: generate existence flag", {
+test_that("get_flagged_records Test 2: test filter argument", {
   advs <- tibble::tribble(
     ~USUBJID, ~PARAMCD, ~AVISIT,    ~AVAL,
     "ST42-1", "WEIGHT", "BASELINE", 66,
@@ -46,6 +46,32 @@ test_that("get_flagged_records Test 1: generate existence flag", {
   expected <- advs %>%
     slice(-5) %>%
     mutate(VSEVALFL = c(1, 0, 1, 0))
+  
+  expect_dfs_equal(
+    base = expected,
+    compare = actual,
+    keys = c("USUBJID", "AVISIT")
+  )
+})
+
+## Test 3: test missing values ----
+test_that("get_flagged_records Test 3: test missing values", {
+  advs <- tibble::tribble(
+    ~USUBJID, ~PARAMCD, ~AVISIT,    ~AVAL,
+    "ST42-1", "WEIGHT", "BASELINE", 66,
+    "ST42-1", "WEIGHT", "Week 2",   68,
+    "ST42-2", "WEIGHT", NA_character_, 88,
+    "ST42-3", "WEIGHT", "Week 2",   55,
+    "ST42-3", "WEIGHT", NA_character_,   50
+  ) %>% mutate(STUDYID = "ST42")
+  
+  actual <- get_flagged_records(
+    dataset = advs,
+    new_var = VSEVALFL,
+    condition = AVISIT == "BASELINE"
+  )
+  
+  expected <- mutate(advs, VSEVALFL = c(1, 0, 0, 0, 0))
   
   expect_dfs_equal(
     base = expected,
