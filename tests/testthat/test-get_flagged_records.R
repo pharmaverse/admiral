79af5b055579ee6c37ee5c03016ee81c1ea53c0c
Iain McCay <iain.x.mccay@gsk.com>
2024-05-23 10:54:59 -0700

    Added test for filter condition
    Pushed from Domino: https://domino-uat.gsk.com/workspace/im631011/admiral?executionId=66339539d022324fe623e848

--- tests/testthat/test-get_flagged_records.R
+++ tests/testthat/test-get_flagged_records.R
@@ -24,3 +24,32 @@ test_that("get_flagged_records Test 1: generate existence flag", {
     keys = c("USUBJID", "AVISIT")
   )
 })
+
+## Test 2: test filter argument ----
+test_that("get_flagged_records Test 1: generate existence flag", {
+  advs <- tibble::tribble(
+    ~USUBJID, ~PARAMCD, ~AVISIT,    ~AVAL,
+    "ST42-1", "WEIGHT", "BASELINE", 66,
+    "ST42-1", "WEIGHT", "Week 2",   68,
+    "ST42-2", "WEIGHT", "BASELINE", 88,
+    "ST42-3", "WEIGHT", "Week 2",   55,
+    "ST42-3", "WEIGHT", "Week 4",   50
+  ) %>% mutate(STUDYID = "ST42") 
+  
+  actual <- get_flagged_records(
+    dataset = advs,
+    new_var = VSEVALFL,
+    condition = AVISIT == "BASELINE",
+    filter = AVAL > 51
+  )
+  
+  expected <- advs %>%
+    slice(-5) %>%
+    mutate(VSEVALFL = c(1, 0, 1, 0))
+  
+  expect_dfs_equal(
+    base = expected,
+    compare = actual,
+    keys = c("USUBJID", "AVISIT")
+  )
+})
