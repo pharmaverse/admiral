9c964cea3162a8ef9fa014be2478687d48f02c3e
Jeffrey Dickinson <dickinson.jeffrey@gene.com>
2024-05-30 22:29:39 +0000

    #2405 update messaging for duplicates in derive_extreme_event()

--- R/derive_extreme_event.R
+++ R/derive_extreme_event.R
@@ -440,6 +440,15 @@ derive_extreme_event <- function(dataset = NULL,
           filter_if(event$condition) %>%
           ungroup()
         if (!is.null(event$mode)) {
+          # Check for duplicates
+          signal_duplicate_records(
+            dataset = data_events,
+            by_vars = append(by_vars, event_order),
+            msg = paste("Check duplicates: ", event$dataset_name, " dataset contains duplicate records
+                        with respect to {.var {replace_values_by_names(by_vars)}}"),
+            cnd_type = "message"
+          )
+
           data_events <- filter_extreme(
             data_events,
             by_vars = by_vars,
@@ -448,6 +457,15 @@ derive_extreme_event <- function(dataset = NULL,
           )
         }
       } else {
+        # Check for duplicates
+        signal_duplicate_records(
+          dataset = data_source,
+          by_vars = append(by_vars, event_order),
+          msg = paste("Check duplicates: ", event$dataset_name, " dataset contains duplicate records
+                      with respect to {.var {replace_values_by_names(by_vars)}}"),
+          cnd_type = "message"
+        )
+
         data_events <- filter_joined(
           data_source,
           dataset_add = data_source,
@@ -474,6 +492,15 @@ derive_extreme_event <- function(dataset = NULL,
     }
   )
   selected_records <- bind_rows(selected_records_ls)
+
+  # Check for duplicates
+  signal_duplicate_records(
+    dataset = selected_records,
+    by_vars = append(by_vars, order),
+    msg = paste("Check duplicates: selected_records dataset contains duplicate records
+                with respect to {.var {replace_values_by_names(by_vars)}}"),
+    cnd_type = "message"
+  )
 
   ## filter_extreme
   new_obs <- selected_records %>%
