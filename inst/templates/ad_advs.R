f126ce8db60e33a04f368b1854fd6375262a76ef
steventing12 <steven.ting@cytel.com>
2024-09-03 20:56:03 +0800

    Closes #2484 Used restrict_derivation() with AVISITN > 0 to restrict CHG and PCHG to post-baseline records (#2488)
    * Used restrict_derivation() with AVISITN > 0 to restrict CHG and PCHG derivations to post-baseline observations. Note that NA visit changes are also not derived.
    
    * feat: #2484 updated comments for CHG and PCHG
    
    * feat: #2484 Manually added "Rothstein" to inst/WORDLIST
    
    * feat: #2484 Updated CHG/PCHG comments and restrict_derivation call to notify that as per ADaM IG, the decision on how to populate pre-baseline and baseline values of CHG/PCHG is left to producer choice.
    
    * feat: #2484 Changed 'sponsor' to 'producer'
    
    * feat: #2484 Use restrict_derivation() with filter AVISITN > 0 to derive CHG and PCHG for post-baseline records only
    
    * feat: #2484 Run style_file()
    
    * feat: #2484 Add piping back to derive_var_obs_number() for adgdssf dataframe

--- inst/templates/ad_advs.R
+++ inst/templates/ad_advs.R
@@ -227,11 +227,18 @@ advs <- advs %>%
     source_var = ANRIND,
     new_var = BNRIND
   ) %>%
-  # Calculate CHG
-  derive_var_chg() %>%
-  # Calculate PCHG
-  derive_var_pchg()
-
+  # Calculate CHG for post-baseline records
+  # The decision on how to populate pre-baseline and baseline values of CHG is left to producer choice
+  restrict_derivation(
+    derivation = derive_var_chg,
+    filter = AVISITN > 0
+  ) %>%
+  # Calculate PCHG for post-baseline records
+  # The decision on how to populate pre-baseline and baseline values of PCHG is left to producer choice
+  restrict_derivation(
+    derivation = derive_var_pchg,
+    filter = AVISITN > 0
+  )
 
 ## ANL01FL: Flag last result within an AVISIT and ATPT for post-baseline records ----
 advs <- advs %>%
