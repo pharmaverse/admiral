b79a5585081e10db0740ecc1fa2a02fba73827e4
Stefan Pascal Thoma <stefan.thoma@roche.com>
2024-09-05 12:40:30 +0000

    now using exprs

--- R/derive_vars_cat.R
+++ R/derive_vars_cat.R
@@ -2,42 +2,64 @@
 #'
 #'
 #' @param dataset
-#' @param definition
+#' @param definition of condition and values defined as `exprs()` object.
+#' e.g. exprs(~condition, ~AVALCAT1, ~AVALCA1N
+#'            AVAL >= 140, ">=140 cm", 1,
+#'            AVAL < 140, "<140 cm", 2)
 #'
 #' @return
 #' @export
 #'
 #' @examples
 #'
-#' advs <- tibble::tribble(
-#'   ~USUBJID, ~PARAMCD, ~AVAL,
-#'   "01",     "HEIGHT", 150,
-#'   "02",     "HEIGHT", 135,
-#'   "03",     "HEIGHT", 145
-#' )
-#'
-#' define_tibble <- tribble(
-#'   ~condition,  ~AVALCAT1,  ~AVALCA1N, ~NEWCOL,
-#'   expr(AVAL > 140),  ">140 cm",          1,  "extra1",
-#'   expr(AVAL <= 140), "<=140 cm",         2,  "extra2"
-#' )
-#'
-#'
-#' derive_vars_cat(dataset = advs, definition = define_tibble) %>% print()
+# advs <- tibble::tribble(
+#   ~USUBJID, ~PARAMCD, ~AVAL,
+#   "01",     "HEIGHT", 150,
+#   "02",     "HEIGHT", 135,
+#   "03",     "HEIGHT", 145
+# )
+
+# advs <- pharmaverseadam::advs
+#
+# definition <- exprs(
+#   ~condition,  ~AVALCAT1,  ~AVALCA1N, ~NEWCOL,
+#   VSTESTCD == "HEIGHT" & AVAL > 140,  ">140 cm",          1,  "extra1",
+#   VSTESTCD == "HEIGHT" & AVAL <= 140, "<=140 cm",         2,  "extra2"
+# )
+#
+#
+# definition <- exprs(
+#   ~condition,                                              ~AVALCAT1,  ~AVALCA1N,   ~NEWCOL,
+#   VSTESTCD == "HEIGHT" & AVAL >= 150,                     ">=150 cm",          1,  "extra1",
+#   VSTESTCD == "HEIGHT" & AVAL > 140 & AVAL < 150, "[140 cm, 150 cm]",          2,  "extra2",
+#   VSTESTCD == "HEIGHT" & AVAL <= 140,                     "<=140 cm",          3,  "extra3"
+# )
+#
+
+# derive_vars_cat(dataset = advs, definition = definition) %>% select(USUBJID, VSTESTCD, AVAL, AVALCA1N, AVALCAT1) %>% filter(VSTESTCD == "HEIGHT")
 
 derive_vars_cat <- function(dataset,
                             definition) {
-  # assertions (waiting for feedback first)
+  # assertions
+  assert_data_frame(dataset)
+  assert_expr_list(definition)
+  assert_data_frame(dataset, required_vars = admiraldev::extract_vars(definition) %>% unique())
 
-  # extract new variable names
-  new_col_names <- names(expr_list)[!names(expr_list)=="condition"]
+  # transform definition to tibble
+  names(definition) <- NULL
+  definition <- tibble::tribble(!!!definition)
+  assert_data_frame(definition, required_vars = exprs(condition))
+
+
+  # extract new variable names and conditions
+  new_col_names <- names(definition)[!names(definition)=="condition"]
+  condition <- definition[["condition"]] # could also be outside of the function
 
   # (re)apply the function for each new variable name and iteratively derive the categories
   new_dataset <- reduce(new_col_names, function(.data, col_name) {
     # extract conditions
-    condition <- definition[["condition"]] # could also be outside of the function
-    # extract values
     values <- definition[[col_name]]
+    # extract values
 
     .data %>%
       mutate(!!sym(col_name) := eval(rlang::call2(
