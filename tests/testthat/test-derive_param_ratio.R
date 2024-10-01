6e6cd49af1db1d23d7febb3e052800c694a276d2
Andrii Yurovskyi <a.yurovskyi@icloud.com>
2024-10-01 15:29:28 +0200

    Update unit tests as per styler

--- tests/testthat/test-derive_param_ratio.R
+++ tests/testthat/test-derive_param_ratio.R
@@ -1,15 +1,15 @@
 test_that("derive_param_ratio Test 1: Cross-check with derive_param_computed()", {
   input <- tribble(
     ~USUBJID, ~PARAMCD, ~PARAM, ~AVAL, ~AVALU, ~VISIT,
-    "01-101-1001", "HIPCIR", "Hip Circumference (cm)",   125, "cm", "SCREENING",
-    "01-101-1001", "HIPCIR", "Hip Circumference (cm)",   124, "cm", "WEEK 2",
-    "01-101-1001", "HIPCIR", "Hip Circumference (cm)",   123, "cm", "WEEK 3",
+    "01-101-1001", "HIPCIR", "Hip Circumference (cm)", 125, "cm", "SCREENING",
+    "01-101-1001", "HIPCIR", "Hip Circumference (cm)", 124, "cm", "WEEK 2",
+    "01-101-1001", "HIPCIR", "Hip Circumference (cm)", 123, "cm", "WEEK 3",
     "01-101-1001", "WSTCIR", "Waist Circumference (cm)", 110, "cm", "SCREENING",
     "01-101-1001", "WSTCIR", "Waist Circumference (cm)", 108, "cm", "WEEK 2",
     "01-101-1001", "WSTCIR", "Waist Circumference (cm)", 107, "cm", "WEEK 3",
-    "01-101-1002", "HIPCIR", "Hip Circumference (cm)",   135, "cm", "SCREENING",
-    "01-101-1002", "HIPCIR", "Hip Circumference (cm)",   133, "cm", "WEEK 2",
-    "01-101-1002", "HIPCIR", "Hip Circumference (cm)",   132, "cm", "WEEK 3",
+    "01-101-1002", "HIPCIR", "Hip Circumference (cm)", 135, "cm", "SCREENING",
+    "01-101-1002", "HIPCIR", "Hip Circumference (cm)", 133, "cm", "WEEK 2",
+    "01-101-1002", "HIPCIR", "Hip Circumference (cm)", 132, "cm", "WEEK 3",
     "01-101-1002", "WSTCIR", "Waist Circumference (cm)", 120, "cm", "SCREENING",
     "01-101-1002", "WSTCIR", "Waist Circumference (cm)", 118, "cm", "WEEK 2",
     "01-101-1002", "WSTCIR", "Waist Circumference (cm)", 117, "cm", "WEEK 3"
@@ -48,11 +48,11 @@ test_that("derive_param_computed Test 2: Cross-check with derive_param_computed(
           new observations with constant parameters", {
   input <- tribble(
     ~USUBJID, ~PARAMCD, ~PARAM, ~AVAL, ~AVALU, ~VISIT,
-    "01-101-1001", "HEIGHT", "Height (cm)",              147, "cm", "SCREENING",
+    "01-101-1001", "HEIGHT", "Height (cm)", 147, "cm", "SCREENING",
     "01-101-1001", "WSTCIR", "Waist Circumference (cm)", 110, "cm", "SCREENING",
     "01-101-1001", "WSTCIR", "Waist Circumference (cm)", 108, "cm", "WEEK 2",
     "01-101-1001", "WSTCIR", "Waist Circumference (cm)", 107, "cm", "WEEK 3",
-    "01-101-1002", "HEIGHT", "Height (cm)",              163, "cm", "SCREENING",
+    "01-101-1002", "HEIGHT", "Height (cm)", 163, "cm", "SCREENING",
     "01-101-1002", "WSTCIR", "Waist Circumference (cm)", 120, "cm", "SCREENING",
     "01-101-1002", "WSTCIR", "Waist Circumference (cm)", 118, "cm", "WEEK 2",
     "01-101-1002", "WSTCIR", "Waist Circumference (cm)", 117, "cm", "WEEK 3",
