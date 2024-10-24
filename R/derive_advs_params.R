4595cf6e9695703de671eecaf0b37c503a400255
jimrothstein <jimrothstein@gmail.com>
2024-10-24 13:08:39 -0700

    run styler::styler_pkg()

--- R/derive_advs_params.R
+++ R/derive_advs_params.R
@@ -521,7 +521,7 @@ compute_bsa <- function(height = height,
   } else if (method == "DuBois-DuBois") {
     # The DuBois & DuBois formula expects the value of height in meters
     # We need to convert from cm
-    bsa <- 0.007184 * height ^ 0.725 * weight ^ 0.425
+    bsa <- 0.007184 * height^0.725 * weight^0.425
   } else if (method == "Haycock") {
     bsa <- 0.024265 * height^0.3964 * weight^0.5378
   } else if (method == "Gehan-George") {
