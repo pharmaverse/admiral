3f9f388f12d9a08a3855e11384df8ab438271f4f
jimrothstein <jimrothstein@gmail.com>
2024-11-27 14:54:55 -0800

    2529 add words to `derive_param_computed()`  When OK, will do same with `derive_vars_computed()`

--- R/derive_param_computed.R
+++ R/derive_param_computed.R
@@ -2,9 +2,10 @@
 #'
 #' Adds a parameter computed from the analysis value of other parameters. It is
 #' expected that the analysis value of the new parameter is defined by an
-#' expression using the analysis values of other parameters. addition/sum,
+#' expression using the analysis values of other parameters, such as addition/sum,
 #' subtraction/difference, multiply/product, division/ratio,
-#' exponentiate/logarithm
+#' exponentiate/logarithm, or by formula.
+#' <br/><br/>
 #' For example mean arterial pressure (MAP) can be derived from systolic (SYSBP)
 #' and diastolic blood pressure (DIABP) with the formula
 #' \deqn{MAP = \frac{SYSBP + 2 DIABP}{3}}{MAP = (SYSBP + 2 DIABP) / 3}
