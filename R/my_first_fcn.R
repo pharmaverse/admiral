73a0636bec92fd00b55934f66ffc338a094f3bb8
Vladyslav <shuliarv@roche.com>
2024-12-02 15:09:41 +0000

    #1839 Fix bugs

--- R/my_first_fcn.R
+++ R/my_first_fcn.R
@@ -1,3 +1,8 @@
-my_first_fcn <- function(hw = TRUE) {
+#' Send welcoming message to the user
+#'
+#' @details
+#' This is a function that prints a welcoming message to the terminal that
+#' greets the user.
+my_first_fcn <- function() {
   "Welcome to the admiral family!"
 }
--- tests/testthat/test-my_first_fcn.R
+++ tests/testthat/test-my_first_fcn.R
@@ -1,8 +1,6 @@
-## Test 1: Veryfying the message function works fine and prints out the message ----
-test_that("multiplication works", {
+## Test 1: Verifying the message function works fine and prints out the message ----
+test_that("Message printing works", {
   expected_output <- "Welcome to the admiral family!"
-
   actual_output <- my_first_fcn()
-
   expect_equal(expected_output, actual_output)
 })
