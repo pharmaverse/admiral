7c2ddc80cd18ede9566c554da3b841ce65638c6f
Lun-Hsien Chang <luenhchang@gmail.com>
2024-12-04 17:40:56 +1000

    feat: Add my_first_fcn to welcome new users - Create new function my_first_fcn() - Add corresponding test - Resolves #1839

---
+++ R/my_first_fcn.R
@@ -0,0 +1,11 @@
+#' My First Function
+#'
+#' This function outputs a welcome message for onboarding purposes.
+#'
+#' @return A string with a welcome message.
+#' @export
+#'
+my_first_fcn <- function() {
+  "Welcome to the admiral family!"
+}
+
---
+++ tests/testthat/test-my_first_fcn.R
@@ -0,0 +1,5 @@
+test_that("my_first_fcn works correctly", {
+  result <- my_first_fcn()
+  expect_type(result, "character")  # Test that the output is a string
+  expect_equal(result, "Welcome to the admiral family!")  # Test the exact content
+})
