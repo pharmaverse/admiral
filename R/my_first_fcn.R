1f9937c192a002c50e98fb2f76b25ecb7925c5fd
Lun-Hsien Chang <luenhchang@gmail.com>
2024-12-18 15:00:48 +1000

    Updated R/my_first_fcn.R changing message from "Welcome to admiral\!" to "Welcome to the admiral family\!"

--- R/my_first_fcn.R
+++ R/my_first_fcn.R
@@ -2,9 +2,9 @@
 #'
 #' This function outputs a welcome message for onboarding purposes.
 #'
-#' @return A string with a welcome message "Welcome to admiral!" 
+#' @return A string with a welcome message
 #' @export
 #'
 my_first_fcn <- function() {
-  "Welcome to admiral!"
+  "Welcome to the admiral family!"
 }
