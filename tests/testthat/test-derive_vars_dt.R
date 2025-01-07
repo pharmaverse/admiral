cdb25275d49771138a914f910e2b81bfb9719739
esimms999 <esimms@yahoo.com>
2025-01-07 20:31:03 +0000

    Issue #2626: Call to derive_vars_dt is now inside a suppressMessages function to keep message from cluttering up the test summary.

--- tests/testthat/test-derive_vars_dt.R
+++ tests/testthat/test-derive_vars_dt.R
@@ -647,12 +647,12 @@ test_that("derive_vars_dt Test 26: no date imputation, DTF present", {
     "2019---07",           as.Date(NA),           NA_character_
   )
   date <- select(expected_output, -ASTDT)
-  actual_output <- derive_vars_dt(
+  actual_output <- suppressMessages(derive_vars_dt(
     date,
     new_vars_prefix = "AST",
     flag_imputation = "date",
     dtc = XXSTDTC
-  )
+  ))
 
   expect_dfs_equal(
     expected_output,
