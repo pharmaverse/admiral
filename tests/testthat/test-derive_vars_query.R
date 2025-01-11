c1938bfb383511bbd69a2fa43abeadaf96a478a7
jimrothstein <jimrothstein@gmail.com>
2025-01-11 12:43:27 -0800

    # 2579 Run lintr

--- tests/testthat/test-derive_vars_query.R
+++ tests/testthat/test-derive_vars_query.R
@@ -351,7 +351,8 @@ test_that("assert_valid_queries Test 10: assert_valid_queries checks", {
 
 # assert_db_requirements ----
 ## Test 11: assert_db_requirements_
-test_that("assert_db_requirements 11: assert_db_requirements() throws error and increases code coverage when called with no `fun` arugment provided", {
+test_that("assert_db_requirements 11: assert_db_requirements() throws error and
+          increases code coverage when called with no `fun` arugment provided", {
   expect_error(
     assert_db_requirements()
   )
