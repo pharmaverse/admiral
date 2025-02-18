822cf8fabebc08ad79df7de48238fbd5c1e8d034
Stefan Bundfuss <stefan.bundfuss@roche.com>
2025-02-18 16:58:26 +0000

    #2585 roclet: update tests

--- R/roxygen2.R
+++ R/roxygen2.R
@@ -28,7 +28,7 @@ roxygen_param_by_vars <- function(rename = FALSE) {
       by_vars_text,
       "Variables can be renamed by naming the element, i.e. \n",
       "`by_vars = exprs(<name in input dataset> = <name in additional dataset>)`, ",
-      "similar to the `dplyr` joins.\n \n"
+      "similar to the `dplyr` joins."
     )
   }
 
