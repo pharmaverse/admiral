05fcc033a22c10e9d77020a7ec030e381ad20271
jimrothstein <jimrothstein@gmail.com>
2025-02-19 22:27:15 -0800

    # 2642 NOT FINISHED
    Multiple templates

--- R/admiral_verify_templates.R
+++ R/admiral_verify_templates.R
@@ -5,13 +5,56 @@ source("inst/templates/ad_adae.R")
 
 # Store adam data in cache.
 theDir = tools::R_user_dir("admiral_templates_data", which = "cache")
-load(paste0(theDir, "/adae.rda"))
 
-# compare this adam to
+# -------   N ----
+# Step 1, gather 23 datasets from pharamaversadam ---
+
+#  This makes all ds in pkg available
 library(pharmaverseadam )
-adae2 = pharmaverseadam::adae
-adae2
+#
+# need ?
+original_datasets = data(package = "pharmaverseadam")
+original_datasets = original_datasets$results
+original_datasets = original_datasets[, "Title"]
+original_datasets
 
+adae2 = pharmaverseadam::adae
+adcm2 = pharmaverseadam::adcm
+`::`(pharmaverseadam, "adae")
+`::`(pharmaverseadam, sym(original_datasets[[1]]))
+
+
+do.call(`::`, list("pharmaverseadam", "adae"))
+x="adae"
+do.call(`::`, list("pharmaverseadam", x))
+
+
+z=lapply(original_datasets, function(x)  do.call(`::`, list("pharmaverseadam", x)))
+z
+
+# find all templsates in `admiral` (when did admiral get loaded?)
+templates = list.files("inst/templates", full.names=TRUE)
+templates
+
+# run the templates, store results tables in .cache (~23 tables)
+lapply(templates, source)
+
+# retrieve from cache
+f = function(x = theDir){
+unlist(lapply(list.files(theDir, full.names=TRUE), load))
+}
+theDir
+
+ls()
+
+
+original_datasets
+templates
+
+
+# compare ----
 library(diffdf)
 
+
 diffdf(adae, adae2 )
+diffdf(adcm, adcm2 )
