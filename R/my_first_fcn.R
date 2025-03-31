507e877cf3d410537c2469acc5e0fb883e18cbe5
1mgroot <zjzsmail@gmail.com>
2025-03-31 01:58:14 -0400

    code restyle with styler

--- R/my_first_fcn.R
+++ R/my_first_fcn.R
@@ -1,7 +1,7 @@
-hello_admiral <- function(hw=TRUE){
-  if(hw){
+hello_admiral <- function(hw = TRUE) {
+  if (hw) {
     message("Welcome to the admiral family with hw")
-  }else{
+  } else {
     message("Welcome to the admiral family without hw")
   }
 }
