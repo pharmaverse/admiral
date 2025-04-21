d48d6eca7fac58b3c3ab43022f286cccfb99e521
Arianna Cascone <arianna.cascone@atorusresearch.com>
2025-04-21 14:28:13 +0000

    feat: #1839 fixing fails in lint

--- tests/testthat/test-my_first_fcn.R
+++ tests/testthat/test-my_first_fcn.R
@@ -8,7 +8,7 @@ test_that("my_first_fcn Test 1: Default argument returns 'Welcome to the admiral
 })
 
 ## Test 2: Setting argument `exclamation` to `TRUE` returns 'Welcome to the admiral family!' ----
-test_that("my_first_fcn Test 2: Argument `exclamation=TRUE` returns 'Welcome to the admiral family!'", {
+test_that("my_first_fcn Test 2: `exclamation=TRUE` returns 'Welcome to the admiral family!'", {
   expect_message(
     hello_admiral(exclamation = TRUE),
     "^Welcome to the admiral family!\\n"
@@ -16,7 +16,7 @@ test_that("my_first_fcn Test 2: Argument `exclamation=TRUE` returns 'Welcome to 
 })
 
 ## Test 3: Setting argument `exclamation` to `FALSE` returns 'Welcome to the admiral family!' ----
-test_that("my_first_fcn Test 3: Argument `exclamation=FALSE` returns 'Welcome to the admiral family!'", {
+test_that("my_first_fcn Test 3: `exclamation=FALSE` returns 'Welcome to the admiral family!'", {
   expect_message(
     hello_admiral(exclamation = FALSE),
     "^Welcome to the admiral family.\\n"
