650daaad2f755c6582f125c045b52880dad0ab28
Arianna Cascone <arianna.cascone@atorusresearch.com>
2025-04-21 14:22:54 +0000

    feat: #1839 styling test-my_first_fcn.R file

--- tests/testthat/test-my_first_fcn.R
+++ tests/testthat/test-my_first_fcn.R
@@ -1,30 +1,24 @@
 # my_first_fcn ----
 ## Test 1: Using the default `exclamation` argument returns 'Welcome to the admiral family!' ----
 test_that("my_first_fcn Test 1: Default argument returns 'Welcome to the admiral family!'", {
-
   expect_message(
     hello_admiral(),
     "^Welcome to the admiral family!\\n"
-    )
-
+  )
 })
 
 ## Test 2: Setting argument `exclamation` to `TRUE` returns 'Welcome to the admiral family!' ----
 test_that("my_first_fcn Test 2: Argument `exclamation=TRUE` returns 'Welcome to the admiral family!'", {
-
   expect_message(
     hello_admiral(exclamation = TRUE),
     "^Welcome to the admiral family!\\n"
   )
-
 })
 
 ## Test 3: Setting argument `exclamation` to `FALSE` returns 'Welcome to the admiral family!' ----
 test_that("my_first_fcn Test 3: Argument `exclamation=FALSE` returns 'Welcome to the admiral family!'", {
-
   expect_message(
     hello_admiral(exclamation = FALSE),
     "^Welcome to the admiral family.\\n"
   )
-
 })
