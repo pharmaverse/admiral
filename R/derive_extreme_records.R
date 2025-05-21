3037977a090214a41ba432ff7d690af03d18dc21
Edoardo Mancini <edoardo.mancini@roche.com>
2025-05-21 11:55:48 +0000

    #2664 use all_of

--- R/derive_extreme_records.R
+++ R/derive_extreme_records.R
@@ -575,7 +575,7 @@ derive_extreme_records <- function(dataset = NULL,
       intersect(by_vars_chr)
 
     new_ref_obs <- anti_join(
-      select(dataset_ref, common_vars),
+      select(dataset_ref, all_of(common_vars)),
       select(new_add_obs, !!!by_vars),
       by = by_vars_chr
     )
