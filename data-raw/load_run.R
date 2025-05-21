96904989a098313c7a4347d1f958c8a997612c79
jimrothstein <jimrothstein@gmail.com>
2025-05-20 22:19:16 -0700

    need devtools

---
+++ data-raw/load_run.R
@@ -0,0 +1,28 @@
+# This script:  data-raw/load_all.R
+
+#
+# install all needed  packages
+# load_all()
+# source("admiral_verify_templates.R")
+# run the function:  verify_templates():w
+
+install.package("pak")
+pak::pak("devtools")
+pak::pak("diffdf")
+
+
+devtools::load_all()
+
+source("data-raw/admiral_verify_templates.R")
+
+verify_templates()
+
+
+
+
+
+
+
+
+
+
