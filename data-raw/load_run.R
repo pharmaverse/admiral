c97234b9784d733de47995aa4f8c9a138ac31fd1
jimrothstein <jimrothstein@gmail.com>
2025-05-23 10:52:08 -0700

    lint data-raw/load_run.R

--- data-raw/load_run.R
+++ data-raw/load_run.R
@@ -1,10 +1,11 @@
 # This script:  data-raw/load_all.R
 
-#
+# nolint start
 # install all needed  packages
 # load_all()
 # source("admiral_verify_templates.R")
 # run the function:  verify_templates():w
+# nolint end
 
 install.packages("pak")
 pak::pak("devtools")
@@ -14,6 +15,6 @@ pak::pak("teal.data")
 
 devtools::load_all()
 
-source("data-raw/admiral_verify_templates.R")
+source("data-raw/admiral_verify_templates.R")  # nolint
 
 verify_templates()
