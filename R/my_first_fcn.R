c015e38a68065f90fb9689c60bdc6f72072d73aa
GSGH (Solveig Holmgaard) <gsgh@novonordisk.com>
2025-06-06 15:43:35 +0200

    #1839 updated spelling

--- R/my_first_fcn.R
+++ R/my_first_fcn.R
@@ -4,14 +4,14 @@
 #'
 #' @details In the roxygen documentation you will find tags for family and keywords.
 #' This is to create organized sections for the Reference tab on the pkgdown website.
-#' You can modify the `_pkgdoqn.yml` as necessary to create appropriate sections as necessary.
+#' You can modify the `_pkgdown.yml` as necessary to create appropriate sections as necessary.
 #' Under `./man/roxygen/meta.R`, you will find where to store these family/keywords.
 #'
 #' @return Happy Message
 #'
-#' family der_adxx
+#' @family example
 #'
-#' @keywords der_adxx
+#' @keywords example
 #'
 #' @export
 #'
