e1f0d6ad507310e55e6af322eb2818b789b0dda2
jimrothstein <jimrothstein@gmail.com>
2025-07-11 17:33:29 -0700

    run ALL TEMPLATES

--- data-raw/load_run.R
+++ data-raw/load_run.R
@@ -10,14 +10,19 @@
 install.packages("pak")
 pak::pak("devtools")
 pak::pak("diffdf")
-pak::pak("teal.data")
+# pak::pak("teal.data")
 
 
 devtools::load_all()
 
 source("data-raw/admiral_verify_templates.R") # nolint
 
+# select templates to run
 templates <- list_all_templates() |> paste()
 templates <- templates[templates!="ADLBHY"]
 
-verify_templates(ds = templates |> tolower() |> head(2))
+# run only 2 templates
+# verify_templates(ds = templates |> tolower() |> head(2))
+
+# run all templates, default is all
+verify_templates()
