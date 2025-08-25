17b0556225a3916f72c9705e41f233b739cd7454
jimrothstein <jimrothstein@gmail.com>
2025-08-25 15:32:32 -0700

    2506_verify_templates lint

--- R/create_query_data.R
+++ R/create_query_data.R
@@ -915,7 +915,7 @@ format.basket_select <- function(x, ...) {
 
   formvar <- list()
 
-  for (i in seq_len(length(all_arg_names))) {
+  for (i in seq_len(length(all_arg_names))) {   # nolint
     is_numeric_class <- map_lgl(x[i], inherits, "numeric") | map_chr(x[i], typeof) == "numeric"
 
     if (is.character(x[[i]]) && length(x[[i]]) <= 1 && !is.na(x[[i]])) {
