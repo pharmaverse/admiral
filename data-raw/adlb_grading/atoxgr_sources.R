eb3c2cdefe3ae8eaba4efe711935d7abfe1b9249
Gordon Miller <gordon.miller@roche.com>
2025-09-29 15:46:00 +0000

    1858 add NCICTCAEv4 JSIN file

--- data-raw/adlb_grading/atoxgr_sources.R
+++ data-raw/adlb_grading/atoxgr_sources.R
@@ -4,13 +4,6 @@ atoxgr_criteria <- system.file("adlb_grading/adlb_grading_spec.xlsx", package = 
 # listing it in "Depends".
 
 
-# create NCICTCAEv4 metadata for SI units
-atoxgr_criteria_ctcv4 <- atoxgr_criteria %>%
-  readxl::read_excel(sheet = "NCICTCAEv4") %>%
-  dplyr::mutate(GRADE_CRITERIA_CODE = gsub("[\r\n]", " ", GRADE_CRITERIA_CODE))
-
-save(atoxgr_criteria_ctcv4, file = "data/atoxgr_criteria_ctcv4.rda")
-
 # create DAIDs metadata for SI units
 atoxgr_criteria_daids <- atoxgr_criteria %>%
   readxl::read_excel(sheet = "DAIDS") %>%
@@ -32,77 +25,61 @@ atoxgr_criteria_daids_uscv <- atoxgr_criteria %>%
 
 save(atoxgr_criteria_daids_uscv, file = "data/atoxgr_criteria_daids_uscv.rda")
 
-# Read in JSON file - for NCICTCAEv5 for CV units
-json_file_path <- "data-raw/adlb_grading/ncictcaev5_cv.json"
 
-atoxgr_criteria_ctcv5_uscv <- jsonlite::fromJSON(json_file_path) %>%
-  filter(!is.na(GRADE_NA_CODE)) %>%
-  dplyr::mutate(
-    NEW_GRADE_CODE = "TRUE ~ \"0\"",
-    NEW_GRADE_CODE = if_else(
-      !is.na(GRADE_1_CODE),
-      paste(GRADE_1_CODE, NEW_GRADE_CODE, sep = ", "),
-      NEW_GRADE_CODE
-    ),
-    NEW_GRADE_CODE = if_else(
-      !is.na(GRADE_2_CODE),
-      paste(GRADE_2_CODE, NEW_GRADE_CODE, sep = ", "),
-      NEW_GRADE_CODE
-    ),
-    NEW_GRADE_CODE = if_else(
-      !is.na(GRADE_3_CODE),
-      paste(GRADE_3_CODE, NEW_GRADE_CODE, sep = ", "),
-      NEW_GRADE_CODE
-    ),
-    NEW_GRADE_CODE = if_else(
-      !is.na(GRADE_4_CODE),
-      paste(GRADE_4_CODE, NEW_GRADE_CODE, sep = ", "),
-      NEW_GRADE_CODE
-    ),
-    NEW_GRADE_CODE = if_else(
-      !is.na(GRADE_NA_CODE),
-      paste(GRADE_NA_CODE, NEW_GRADE_CODE, sep = ", "),
-      NEW_GRADE_CODE
-    ),
-    GRADE_CRITERIA_CODE = paste0("case_when(", NEW_GRADE_CODE, ")")
-  )
+atoxgr_json_to_dataframe <- function(dataset, json_file) {
+  dataset <- jsonlite::fromJSON(json_file) %>%
+    filter(!is.na(GRADE_NA_CODE)) %>%
+    dplyr::mutate(
+      NEW_GRADE_CODE = "TRUE ~ \"0\"",
+      NEW_GRADE_CODE = if_else(
+        !is.na(GRADE_1_CODE),
+        paste(GRADE_1_CODE, NEW_GRADE_CODE, sep = ", "),
+        NEW_GRADE_CODE
+      ),
+      NEW_GRADE_CODE = if_else(
+        !is.na(GRADE_2_CODE),
+        paste(GRADE_2_CODE, NEW_GRADE_CODE, sep = ", "),
+        NEW_GRADE_CODE
+      ),
+      NEW_GRADE_CODE = if_else(
+        !is.na(GRADE_3_CODE),
+        paste(GRADE_3_CODE, NEW_GRADE_CODE, sep = ", "),
+        NEW_GRADE_CODE
+      ),
+      NEW_GRADE_CODE = if_else(
+        !is.na(GRADE_4_CODE),
+        paste(GRADE_4_CODE, NEW_GRADE_CODE, sep = ", "),
+        NEW_GRADE_CODE
+      ),
+      NEW_GRADE_CODE = if_else(
+        !is.na(GRADE_NA_CODE),
+        paste(GRADE_NA_CODE, NEW_GRADE_CODE, sep = ", "),
+        NEW_GRADE_CODE
+      ),
+      GRADE_CRITERIA_CODE = paste0("case_when(", NEW_GRADE_CODE, ")")
+    ) %>%
+    select(-NEW_GRADE_CODE)
+}
 
-save(atoxgr_criteria_ctcv5_uscv, file = "data/atoxgr_criteria_ctcv5_uscv.rda")
+library(dplyr)
 
-# Read in JSON file - for NCICTCAEv5 for CV units
-json_file_path <- "data-raw/adlb_grading/ncictcaev5.json"
+# Read in JSON file - for NCICTCAEv4 for SI units
+json_file_path_v4 <- "data-raw/adlb_grading/ncictcaev4.json"
 
-atoxgr_criteria_ctcv5 <- jsonlite::fromJSON(json_file_path) %>%
-  filter(!is.na(GRADE_NA_CODE)) %>%
-  dplyr::mutate(
-    NEW_GRADE_CODE = "TRUE ~ \"0\"",
-    NEW_GRADE_CODE = if_else(
-      !is.na(GRADE_1_CODE),
-      paste(GRADE_1_CODE, NEW_GRADE_CODE, sep = ", "),
-      NEW_GRADE_CODE
-    ),
-    NEW_GRADE_CODE = if_else(
-      !is.na(GRADE_2_CODE),
-      paste(GRADE_2_CODE, NEW_GRADE_CODE, sep = ", "),
-      NEW_GRADE_CODE
-    ),
-    NEW_GRADE_CODE = if_else(
-      !is.na(GRADE_3_CODE),
-      paste(GRADE_3_CODE, NEW_GRADE_CODE, sep = ", "),
-      NEW_GRADE_CODE
-    ),
-    NEW_GRADE_CODE = if_else(
-      !is.na(GRADE_4_CODE),
-      paste(GRADE_4_CODE, NEW_GRADE_CODE, sep = ", "),
-      NEW_GRADE_CODE
-    ),
-    NEW_GRADE_CODE = if_else(
-      !is.na(GRADE_NA_CODE),
-      paste(GRADE_NA_CODE, NEW_GRADE_CODE, sep = ", "),
-      NEW_GRADE_CODE
-    ),
-    GRADE_CRITERIA_CODE = paste0("case_when(", NEW_GRADE_CODE, ")")
-  )
+atoxgr_criteria_ctcv4 <- atoxgr_json_to_dataframe(json_file = json_file_path_v4)
+
+save(atoxgr_criteria_ctcv4, file = "data/atoxgr_criteria_ctcv4.rda")
+
+# Read in JSON file - for NCICTCAEv5 for SI units
+json_file_path_v5 <- "data-raw/adlb_grading/ncictcaev5.json"
+
+atoxgr_criteria_ctcv5 <- atoxgr_json_to_dataframe(json_file = json_file_path_v5)
 
 save(atoxgr_criteria_ctcv5, file = "data/atoxgr_criteria_ctcv5.rda")
 
+# Read in JSON file - for NCICTCAEv5 for CV units
+json_file_path_v5_cv <- "data-raw/adlb_grading/ncictcaev5_cv.json"
+
+atoxgr_criteria_ctcv5_uscv <- atoxgr_json_to_dataframe(json_file = json_file_path_v5_cv)
+
+save(atoxgr_criteria_ctcv5_uscv, file = "data/atoxgr_criteria_ctcv5_uscv.rda")
