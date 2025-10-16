c52fc14f09b8a430e6e2db0907170cc7a46d095a
Laurent Dassencourt <Laurent.Dassencourt@sanofi.com>
2025-10-16 14:38:57 +0200

    function added + documentation

--- R/my_first_fcn_ld.R
+++ R/my_first_fcn_ld.R
@@ -4,6 +4,9 @@
 #'
 #' @return No return value, called for side effects.
 #' @export
+#'
+#' @examples
+#' my_first_fcn()
 my_first_fcn <- function() {
   message("Welcome to the admiral family!")
 }
